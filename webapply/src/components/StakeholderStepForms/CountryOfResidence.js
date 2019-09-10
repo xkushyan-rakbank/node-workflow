a936086d4396c525d6f61fd0ff60b4741243705a
Oleksiy B <oleksii.boiko@designit.com>
2019-09-10 18:03:07 +0300

    Country Of Residence – validation fix validation issues

--- webapply/src/components/StakeholderStepForms/CountryOfResidence.js
+++ webapply/src/components/StakeholderStepForms/CountryOfResidence.js
@@ -1,32 +1,56 @@
 import React from "react";
 import { connect } from "react-redux";
 import Grid from "@material-ui/core/Grid";
-import Select from "../InputField/PureSelect";
-import Input from "../InputField/TextInput";
-import { getInputValueById } from "../../store/selectors/appConfig";
+import PureSelect from "../InputField/PureSelect";
+import TextInput from "../InputField/TextInput";
+import {
+  getInputNameById,
+  getInputValueById
+} from "../../store/selectors/appConfig";
+import { updateField } from "../../store/actions/appConfig";
 
 class CountryOfResidence extends React.Component {
   static defaultProps = {
     index: 0
   };
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.residenceCountry !== this.props.residenceCountry) {
+      this.setEidNumberValue(this.isSelectedOAE() ? "784" : "");
+    }
+  }
+
+  setEidNumberValue(value) {
+    this.props.updateField({
+      value,
+      name: this.props.eidNumberInputName
+    });
+  }
+
+  isSelectedOAE() {
+    return this.props.residenceCountry === "AE";
+  }
+
   render() {
-    const { index, isOAE, isSignatory } = this.props;
+    const { index, isSignatory } = this.props;
+    const isOAE = this.isSelectedOAE();
 
     return (
       <Grid container spacing={3}>
         <Grid item md={6} sm={12}>
-          <Select
+          <PureSelect
             disabled={isSignatory}
-            defaultValue="AE"
+            defaultValue={isSignatory ? "AE" : undefined}
             id="SigKycd.residenceCountry"
             indexes={[index]}
           />
         </Grid>
         <Grid item md={6} sm={12}>
-          <Input
-            defaultValue="784-XXXX-XXXXXXX-X"
+          <TextInput
+            placeholder="784-XXXX-XXXXXXX-X"
+            required={isOAE}
             disabled={!isOAE}
+            defaultValue={isOAE ? "784" : undefined}
             id="SigKycdEmid.eidNumber"
             indexes={[index]}
           />
@@ -37,9 +61,19 @@ class CountryOfResidence extends React.Component {
 }
 
 const mapStateToProps = (state, { index }) => ({
-  isOAE: getInputValueById(state, "SigKycd.residenceCountry", [index]) === "AE",
+  residenceCountry: getInputValueById(state, "SigKycd.residenceCountry", [
+    index
+  ]),
   isSignatory:
-    getInputValueById(state, "SigKycd.isSignatory", [index]) === "true"
+    getInputValueById(state, "SigKycd.isSignatory", [index]) === "true",
+  eidNumberInputName: getInputNameById(state, "SigKycdEmid.eidNumber", [index])
 });
 
-export default connect(mapStateToProps)(CountryOfResidence);
+const mapDispatchToProps = {
+  updateField
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CountryOfResidence);
