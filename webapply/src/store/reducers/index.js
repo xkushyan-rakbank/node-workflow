e60dcced46261f75bd0b5fc51c50ccdc4d965284
Oleksiy B <oleksii.boiko@designit.com>
2019-09-12 12:48:28 +0300

    add jsDoc typedef models

--- webapply/src/store/reducers/index.js
+++ webapply/src/store/reducers/index.js
@@ -2,6 +2,11 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import appConfig from "./appConfig";
 
+/**
+ * @typedef {Object} Store
+ * @property {AppConfig} appConfig
+ * @property {Object} router
+ */
 const reducers = history =>
   combineReducers({
     router: connectRouter(history),
--- webapply/src/store/selectors/appConfig.js
+++ webapply/src/store/selectors/appConfig.js
@@ -2,6 +2,10 @@ import get from "lodash/get";
 import combineNestingName from "../../utils/combineNestingName";
 
 export const getUiConfig = state => state.appConfig.uiConfig || {};
+/**
+ * @param {Store} state
+ * @return {Prospect}
+ */
 export const geProspect = state => state.appConfig.prospect || {};
 
 export const getFieldConfigById = (state, id) => {
@@ -19,4 +23,13 @@ export const getInputValueById = (state, id, indexes = []) => {
   return get(state.appConfig, getInputNameById(state, id, indexes));
 };
 
-export const getSignatories = state => geProspect(state).signatoryInfo;
+/**
+ * @param {Store} state
+ * @return {Signatory[]|[]}
+ */
+export const getSignatories = state => geProspect(state).signatoryInfo || [];
+/**
+ * @param {Store} state
+ * @return {OrgKYCDetails|{}}
+ */
+export const getOrgKYCDetails = state => geProspect(state).orgKYCDetails || {};
