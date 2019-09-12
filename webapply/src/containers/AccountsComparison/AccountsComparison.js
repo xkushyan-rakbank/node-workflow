a5bade4451a36f691791ca57b60d236a49114159
VjacheslavZ <vjacheslav.zemlyaniy@designit.com>
2019-09-12 14:27:07 +0300

    add Table compare component

--- webapply/src/containers/AccountsComparison/AccountsComparison.js
+++ webapply/src/containers/AccountsComparison/AccountsComparison.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import AccountsComparisonContainer from "./AccountsComparisonContainer";
-import AccountCardContainer from "./AccountCardContainer";
 
 const style = {
   container: {
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
+    maxWidth: "790px"
   }
 };
 
