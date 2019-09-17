198dc8e22e771010354886db7e120d4230b5061d
Oleksiy B <oleksii.boiko@designit.com>
2019-09-17 15:06:36 +0300

    update BasicForm reCaptcha onVerify/onExpired/onError handlers

--- webapply/src/containers/BasicsForm.js
+++ webapply/src/containers/BasicsForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import { withStyles } from "@material-ui/core/styles";
 import ErrorBoundary from "../components/ErrorBoundary";
 import TextInput from "../components/InputField/TextInput";
@@ -6,7 +7,9 @@ import ReCaptcha from "../components/ReCaptcha/ReCaptcha";
 import RefactoredCheckbox from "../components/InputField/RefactoredCheckbox";
 import SubmitButton from "../components/Buttons/SubmitButton";
 import PureSelect from "../components/InputField/PureSelect";
+import { setToken, verifyToken, setVerified } from "../store/actions/reCaptcha";
 import validateForm from "../utils/validate";
+import * as reCaptchaSelectors from "../store/selectors/reCaptcha";
 
 const styles = {
   reCaptchaContainer: {
@@ -17,6 +20,19 @@ const styles = {
 };
 
 class BasicsForm extends React.Component {
+  static defaultProps = {
+    setToken: () => {}
+  };
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (
+      prevProps.reCaptchaToken !== this.props.reCaptchaToken &&
+      this.props.reCaptchaToken
+    ) {
+      this.props.verifyToken();
+    }
+  }
+
   submitForm = event => {
     event.preventDefault();
     const errorList = validateForm(event);
@@ -24,6 +40,19 @@ class BasicsForm extends React.Component {
     if (!errorList.length) {
       this.props.history.push("/VerifyOTP");
     }
+  };
+
+  handleReCaptchaVerify = token => {
+    this.props.setToken(token);
+  };
+
+  handleReCaptchaExpired = () => {
+    this.props.setVerified(false);
+  };
+
+  handleReCaptchaError = error => {
+    console.error(error);
+    this.props.setVerified(false);
   };
 
   render() {
@@ -60,21 +89,37 @@ class BasicsForm extends React.Component {
 
           <ErrorBoundary className={classes.reCaptchaContainer}>
             <ReCaptcha
-              onVerify={token =>
-                console.log("ReCaptcha onVerify callback:", token)
-              }
-              onExpired={() =>
-                console.log("ReCaptcha onExpired callback (2 min)")
-              }
-              onError={() => console.log("ReCaptcha onError callback")}
+              onVerify={this.handleReCaptchaVerify}
+              onExpired={this.handleReCaptchaExpired}
+              onError={this.handleReCaptchaError}
             />
           </ErrorBoundary>
 
-          <SubmitButton label="Next Step" justify="flex-end" />
+          <SubmitButton
+            label="Next Step"
+            justify="flex-end"
+            disabled={!this.props.isReCaptchaVerified}
+          />
         </form>
       </>
     );
   }
 }
 
-export default withStyles(styles)(BasicsForm);
+const mapStateToProps = state => ({
+  reCaptchaToken: reCaptchaSelectors.getReCaptchaToken(state),
+  isReCaptchaVerified: reCaptchaSelectors.getReCaptchaVerified(state)
+});
+
+const mapDispatchToProps = {
+  setToken,
+  setVerified,
+  verifyToken
+};
+
+export default withStyles(styles)(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(BasicsForm)
+);
