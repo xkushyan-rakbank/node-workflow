273d66107c23df5d177f35f9ffdde6ffabb54df7
Oleksiy B <oleksii.boiko@designit.com>
2019-09-18 17:12:18 +0300

    configure WireMock: POST recaptcha/verify success request

--- webapply/src/api/axiosConfig.js
+++ webapply/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_PATH || "http://localhost:9090/api/v1"
+  baseURL: process.env.REACT_APP_API_PATH || "http://localhost:8080"
 });
 
 export default instance;
