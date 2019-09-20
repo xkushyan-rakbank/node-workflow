f30d17d238b63fe51dfc32c6bb9b776882bf0ff8
VjacheslavZ <vjacheslav.zemlyaniy@designit.com>
2019-09-20 15:15:35 +0300

    update styles for <MyApplicationsGrid/>and <MyApplicationsList/>, add new svg icons, update styles for <AccountTypeCard/> update styles for <AccountCardContainer>

--- webapply/src/containers/AccountsComparison/AccountCardContainer.js
+++ webapply/src/containers/AccountsComparison/AccountCardContainer.js
@@ -1,36 +1,39 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
+
 import AccountTypeCard from "../../components/AccountTypeCard";
-import doneIcon from "../../assets/images/done-icon.png";
+import startups_ic from "../../assets/icons/startups_ic.svg";
+import businesses_ic from "../../assets/icons/growing_businesses_ic.svg";
+import established_businesses_ic from "../../assets/icons/established_businesses_ic.svg";
 
 const style = {
   cardsContainer: {
     display: "flex",
-    marginLeft: "-20px",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
+    marginTop: 20
   }
 };
 
 const AccountCardContainer = ({ classes }) => (
   <div className={classes.cardsContainer}>
     <AccountTypeCard
-      iconSrc={doneIcon}
+      iconSrc={startups_ic}
       title="For Startups or Entrepreneurs"
       differences={[
         "Zero balance account",
-        "Atractive interest",
-        "Multiple currencies"
+        "Preferential transaction fees",
+        "Dedicated Relationship Manager"
       ]}
       buttonText="RAKstarter"
     />
     <AccountTypeCard
-      iconSrc={doneIcon}
+      iconSrc={businesses_ic}
       title="For Established Businesses"
       differences={["Low balance account", "Low fees", "Dedicated service"]}
       buttonText="Current Account"
     />
     <AccountTypeCard
-      iconSrc={doneIcon}
+      iconSrc={established_businesses_ic}
       title="For Exclusive Banking Privileges"
       differences={["Preferential pricing", "No fees", "Fast-tracked services"]}
       buttonText="RAKelite"
--- webapply/src/containers/AccountsComparison/AccountsComparisonContainer.js
+++ webapply/src/containers/AccountsComparison/AccountsComparisonContainer.js
@@ -1,49 +1,37 @@
 import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+
+import VerticalPaginationWrapper from "../../components/VerticalPaginationWrapper";
 import SectionTitleWithInfo from "../../components/SectionTitleWithInfo";
 import AccountCardContainer from "./AccountCardContainer";
 import InfoNote from "../../components/InfoNote";
 import TableCompare from "../../components/TableCompare";
 
-const style = {
-  container: {
-    display: "inline-block",
-    "& + &": {
-      marginTop: "150px"
-    }
-  },
-  cardsContainer: {
-    display: "flex",
-    flexDirection: "column",
-    marginTop: "40px"
-  }
-};
-
 class AccountsComparisonContainer extends React.Component {
   render() {
-    const { classes } = this.props;
     return (
       <React.Fragment>
-        <div className={classes.container}>
-          <SectionTitleWithInfo
-            title="One business account for every business stage"
-            info="Wherever your business is, our accounts will meet you there"
-          />
-          <AccountCardContainer />
-          <InfoNote text="Note: Companies older than 12 months are not eligible for the RAKstarter account" />
-        </div>
+        <VerticalPaginationWrapper>
+          <div>
+            <SectionTitleWithInfo
+              title="One business account for every business stage"
+              info="Wherever your business is, our accounts will meet you there"
+            />
+            <AccountCardContainer />
+            <InfoNote text="Note: Companies older than 12 months are not eligible for the RAKstarter account" />
+          </div>
 
-        <div className={classes.container}>
-          <SectionTitleWithInfo
-            title="Compare the accounts"
-            info="Our three business accounts, side by side"
-          />
-          <TableCompare />
-          <InfoNote text="Note: 5% VAT will be levied on all charges applicable to business customers as published on the Service & Price guide. For all other charges related to your accounts please visit www.rakbank.ae " />
-        </div>
+          <div>
+            <SectionTitleWithInfo
+              title="Compare the accounts"
+              info="Our three business accounts, side by side"
+            />
+            <TableCompare />
+            <InfoNote text="Note: 5% VAT will be levied on all charges applicable to business customers as published on the Service & Price guide. For all other charges related to your accounts please visit www.rakbank.ae " />
+          </div>
+        </VerticalPaginationWrapper>
       </React.Fragment>
     );
   }
 }
 
-export default withStyles(style)(AccountsComparisonContainer);
+export default AccountsComparisonContainer;
