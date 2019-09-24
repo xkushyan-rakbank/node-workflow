fa2dc36bdf59d212f9b6b71a795eee62dc820da7
Shailesh Kumar (Digital) <SH390569@wipro.com>
2019-09-24 15:31:12 +0530

    Committing files for document-uploader api development

--- document-uploader/src/main/java/ae/rakbank/documentuploader/controllers/DocumentUploadController.java
+++ document-uploader/src/main/java/ae/rakbank/documentuploader/controllers/DocumentUploadController.java
@@ -1,27 +1,23 @@
 package ae.rakbank.documentuploader.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import ae.rakbank.documentuploader.commons.DocumentUploadException;
 import ae.rakbank.documentuploader.services.DocumentUploadService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 @Controller
+@CrossOrigin
 public class DocumentUploadController {
 
 	private static final Logger logger = LoggerFactory.getLogger(DocumentUploadController.class);
@@ -33,30 +29,20 @@ public class DocumentUploadController {
 		this.docUploadService = docUploadService;
 	}
 
-	@GetMapping("/files/{filename:.+}")
-	@ResponseBody
-	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-		
-		Resource file = docUploadService.loadAsResource(filename);
-		return ResponseEntity.ok()
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
-				.body(file);
-	}
-
+	@CrossOrigin
 	@PostMapping("/banks/RAK/prospects/{prospectId}/documents")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes,
-			@PathVariable String prospectId) {
-		logger.info("Uploading document for "+prospectId);
-		docUploadService.store(file);
-		redirectAttributes.addFlashAttribute("message",
-				"You successfully uploaded " + file.getOriginalFilename() + "!");
-
-		return "redirect:/";
-	}
-
-	@ExceptionHandler(DocumentUploadException.class)
-	public ResponseEntity<?> handleStorageFileNotFound(DocumentUploadException exc) {
-		return ResponseEntity.notFound().build();
+	public ResponseEntity<Object> handleFileUpload(@RequestParam("file") MultipartFile[] file, 
+			RedirectAttributes redirectAttributes, @PathVariable String prospectId) {
+			HttpHeaders headers = new HttpHeaders();
+		if(file.length > 0) {
+			logger.info("Uploading document for "+prospectId);
+			docUploadService.store(file[0]);
+			redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file[0].getOriginalFilename() + "!");
+			return new ResponseEntity<Object>(redirectAttributes.getFlashAttributes(), headers, HttpStatus.OK);
+		} 
+		else {
+			return new ResponseEntity<Object>(redirectAttributes.getFlashAttributes(), headers, HttpStatus.NOT_FOUND);
+		}
 	}
 
 }
