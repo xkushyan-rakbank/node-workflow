48afc012a27ac85f74511336e7219756c0984782
Dhanya Kumar KV <dhanya.kv3@wipro.com>
2019-09-27 21:30:02 +0400

    web apply server changes

--- webapplyserver/src/main/java/ae/rakbank/webapply/controllers/ApiRequestForwarder.java
+++ webapplyserver/src/main/java/ae/rakbank/webapply/controllers/ApiRequestForwarder.java
@@ -1,10 +1,15 @@
 package ae.rakbank.webapply.controllers;
 
+import java.util.Collections;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +23,10 @@ import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import ae.rakbank.webapply.commons.ApiError;
 import ae.rakbank.webapply.commons.EnvUtil;
 import ae.rakbank.webapply.commons.FileHelper;
+import ae.rakbank.webapply.services.OAuthClient;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -27,6 +34,9 @@ public class ApiRequestForwarder {
 
 	@Autowired
 	FileHelper fileHelper;
+
+	@Autowired
+	OAuthClient oauthClient;
 
 	private JsonNode dehURIs = null;
 
@@ -40,97 +50,222 @@ public class ApiRequestForwarder {
 
 	}
 
-	@PostMapping(value = "/banks/RAK/usertypes/sme/prospects/", produces = "application/json", consumes = "application/json")
+	@PostMapping(value = "/usertypes/sme/prospects/", produces = "application/json", consumes = "application/json")
 	@ResponseBody
 	public ResponseEntity<Object> createSMEProspect(@RequestBody JsonNode jsonNode) {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+		ResponseEntity<JsonNode> oauthResponse = oauthClient.getOAuthToken();
 
-		String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
-		ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
-		return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		HttpHeaders headers = new HttpHeaders();
+
+		if (oauthResponse != null && oauthResponse.getStatusCode().is2xxSuccessful()) {
+
+			headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.set("Authorization", "Bearer " + oauthResponse.getBody().get("access_token").asText());
+
+			RestTemplate restTemplate = new RestTemplate();
+			HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+
+			String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
+			ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
+			return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		} else {
+			ApiError error = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error",
+					"oauth error, check logs for more info.");
+
+			return new ResponseEntity<Object>(error, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
 	}
 
-	@PutMapping(value = "/banks/RAK/usertypes/sme/prospects/", produces = "application/json", consumes = "application/json")
+	@PutMapping(value = "/usertypes/sme/prospects/{prospectId}", produces = "application/json", consumes = "application/json")
 	@ResponseBody
-	public ResponseEntity<Object> updateSMEProspect(@RequestBody JsonNode jsonNode) {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+	public ResponseEntity<Object> updateSMEProspect(@RequestBody JsonNode jsonNode, @PathVariable String prospectId) {
+		ResponseEntity<JsonNode> oauthResponse = oauthClient.getOAuthToken();
 
-		String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
-		ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
-		return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		HttpHeaders headers = new HttpHeaders();
+
+		if (oauthResponse != null && oauthResponse.getStatusCode().is2xxSuccessful()) {
+
+			headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.set("Authorization", "Bearer " + oauthResponse.getBody().get("access_token").asText());
+
+			RestTemplate restTemplate = new RestTemplate();
+			HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+
+			String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
+			ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
+			return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		} else {
+			ApiError error = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error",
+					"oauth error, check logs for more info.");
+
+			return new ResponseEntity<Object>(error, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
-	@GetMapping(value = "/banks/RAK/usertypes/{userType}/prospects/search", produces = "application/json", consumes = "application/json")
+	@PostMapping(value = "/usertypes/{userType}/prospects/search", produces = "application/json", consumes = "application/json")
 	@ResponseBody
 	public ResponseEntity<Object> searchProspect(@RequestBody JsonNode jsonNode, @PathVariable String userType) {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+		ResponseEntity<JsonNode> oauthResponse = oauthClient.getOAuthToken();
 
-		String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
-		ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
-		return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		HttpHeaders headers = new HttpHeaders();
+
+		if (oauthResponse != null && oauthResponse.getStatusCode().is2xxSuccessful()) {
+
+			headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.set("Authorization", "Bearer " + oauthResponse.getBody().get("access_token").asText());
+
+			RestTemplate restTemplate = new RestTemplate();
+			HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+
+			String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
+			ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
+			return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		} else {
+			ApiError error = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error",
+					"oauth error, check logs for more info.");
+
+			return new ResponseEntity<Object>(error, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
-	@GetMapping(value = "/banks/RAK/usertypes/sme/prospects/{prospectId}", produces = "application/json", consumes = "application/json")
+	@GetMapping(value = "/usertypes/sme/prospects/{prospectId}", produces = "application/json")
 	@ResponseBody
 	public ResponseEntity<Object> getProspectById(@RequestBody JsonNode jsonNode, @PathVariable String prospectId) {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+		ResponseEntity<JsonNode> oauthResponse = oauthClient.getOAuthToken();
 
-		String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
-		ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
-		return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		HttpHeaders headers = new HttpHeaders();
+
+		if (oauthResponse != null && oauthResponse.getStatusCode().is2xxSuccessful()) {
+
+			headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.set("Authorization", "Bearer " + oauthResponse.getBody().get("access_token").asText());
+
+			RestTemplate restTemplate = new RestTemplate();
+			HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+
+			String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
+			ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
+			return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		} else {
+			ApiError error = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error",
+					"oauth error, check logs for more info.");
+
+			return new ResponseEntity<Object>(error, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
-	@GetMapping(value = "/banks/RAK/prospects/{prospectId}/documents/{documentId}", produces = "application/json", consumes = "application/json")
+	@GetMapping(value = "/prospects/{prospectId}/documents/{documentId}", produces = "application/json")
 	@ResponseBody
 	public ResponseEntity<Object> getDocumentById(@RequestBody JsonNode jsonNode, @PathVariable String prospectId,
 			@PathVariable String documentId) {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+		ResponseEntity<JsonNode> oauthResponse = oauthClient.getOAuthToken();
 
-		String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
-		ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
-		return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		HttpHeaders headers = new HttpHeaders();
+
+		if (oauthResponse != null && oauthResponse.getStatusCode().is2xxSuccessful()) {
+
+			headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.set("Authorization", "Bearer " + oauthResponse.getBody().get("access_token").asText());
+			RestTemplate restTemplate = new RestTemplate();
+			HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+
+			String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
+			ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
+			return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		} else {
+			ApiError error = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error",
+					"oauth error, check logs for more info.");
+
+			return new ResponseEntity<Object>(error, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
-	@GetMapping(value = "/banks/RAK/prospects/{prospectId}/documents", produces = "application/json", consumes = "application/json")
+	@GetMapping(value = "/prospects/{prospectId}/documents", produces = "application/json")
 	@ResponseBody
-	public ResponseEntity<Object> getDocumentsByProspectId(@RequestBody JsonNode jsonNode,
+	public ResponseEntity<Object> getProspectDocuments(@RequestBody JsonNode jsonNode,
 			@PathVariable String prospectId) {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+		ResponseEntity<JsonNode> oauthResponse = oauthClient.getOAuthToken();
 
-		String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
-		ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
-		return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		HttpHeaders headers = new HttpHeaders();
+
+		if (oauthResponse != null && oauthResponse.getStatusCode().is2xxSuccessful()) {
+
+			headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.set("Authorization", "Bearer " + oauthResponse.getBody().get("access_token").asText());
+
+			RestTemplate restTemplate = new RestTemplate();
+			HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+
+			String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
+			ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
+			return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		} else {
+			ApiError error = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error",
+					"oauth error, check logs for more info.");
+
+			return new ResponseEntity<Object>(error, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
-	@PostMapping(value = "/banks/RAK/users/authenticate", produces = "application/json", consumes = "application/json")
+	@PostMapping(value = "/users/authenticate", produces = "application/json", consumes = "application/json")
 	@ResponseBody
-	public ResponseEntity<Object> login(@RequestBody JsonNode jsonNode, @PathVariable String prospectId,
-			@PathVariable String documentId) {
+	public ResponseEntity<Object> login(@RequestBody JsonNode jsonNode) {
+		ResponseEntity<JsonNode> oauthResponse = oauthClient.getOAuthToken();
 
-		RestTemplate restTemplate = new RestTemplate();
-		HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+		HttpHeaders headers = new HttpHeaders();
 
-		String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
-		ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
-		return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		if (oauthResponse != null && oauthResponse.getStatusCode().is2xxSuccessful()) {
+
+			headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.set("Authorization", "Bearer " + oauthResponse.getBody().get("access_token").asText());
+
+			RestTemplate restTemplate = new RestTemplate();
+			HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+
+			String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
+			ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
+			return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		} else {
+			ApiError error = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error",
+					"oauth error, check logs for more info.");
+
+			return new ResponseEntity<Object>(error, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
-	@PostMapping(value = "/banks/RAK/otp", produces = "application/json", consumes = "application/json")
+	@PostMapping(value = "/otp", produces = "application/json", consumes = "application/json")
 	@ResponseBody
-	public ResponseEntity<Object> generateVerifyOTP(@RequestBody JsonNode jsonNode, @PathVariable String prospectId,
-			@PathVariable String documentId) {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+	public ResponseEntity<Object> generateVerifyOTP(@RequestBody JsonNode jsonNode) {
+		ResponseEntity<JsonNode> oauthResponse = oauthClient.getOAuthToken();
 
-		String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
-		ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
-		return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		HttpHeaders headers = new HttpHeaders();
+
+		if (oauthResponse != null && oauthResponse.getStatusCode().is2xxSuccessful()) {
+
+			headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.set("Authorization", "Bearer " + oauthResponse.getBody().get("access_token").asText());
+
+			RestTemplate restTemplate = new RestTemplate();
+			HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+
+			String url = dehBaseUrl + dehURIs.get("createProspectPath").asText();
+			ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, request, JsonNode.class);
+			return new ResponseEntity<Object>(response.getBody(), response.getHeaders(), response.getStatusCode());
+		} else {
+			ApiError error = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error",
+					"oauth error, check logs for more info.");
+
+			return new ResponseEntity<Object>(error, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 }
