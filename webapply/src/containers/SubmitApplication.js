2784e8d43372e8b7ec67616cf2c22571db2f870f
OlegSuvorov <oleg.suvorov@designit.com>
2019-09-27 12:25:23 +0300

    remove unused code

--- webapply/src/containers/SubmitApplication.js
+++ webapply/src/containers/SubmitApplication.js
@@ -20,10 +20,9 @@ const style = {
     display: "block"
   },
   card: {
-    width: "271px",
-    height: "236px",
+    minHeight: "236px",
     margin: "auto auto 40px",
-    padding: "32px 54.5px 30px",
+    padding: "40px 20px 42px 20px",
     textAlign: "center",
     borderRadius: "8px",
     boxShadow: "0 1px 16px 0 rgba(0, 0, 0, 0.1)",
@@ -31,8 +30,8 @@ const style = {
     backgroundImage: "linear-gradient(to bottom, #ffffff, rgba(255, 255, 255, 0))"
   },
   icon: {
-    width: "40px",
-    height: "40px",
+    width: "37px",
+    height: "37px",
     border: "solid 1.5px #e9e9ed",
     borderRadius: "50%",
     margin: "auto",
@@ -62,54 +61,55 @@ const style = {
   }
 };
 
-const SubmitApplication = props => {
-  const { classes } = props;
-  return (
-    <>
-      <FormTitle
-        title="Submit application"
-        info="And just like that, we have reached the end! Here’s the overview of what you’re applying for."
-      />
-      <div className={classes.card}>
-        <div className={classes.icon}>
-          <img src={brief} alt="brief" width={24} height={24} />
+class SubmitApplication extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <>
+        <FormTitle
+          title="Submit application"
+          info="And just like that, we have reached the end! Here’s the overview of what you’re applying for."
+        />
+        <div className={classes.card}>
+          <div className={classes.icon}>
+            <img src={brief} alt="brief" width={24} height={24} />
+          </div>
+          <div className={classes.mainTitle}>Designit Arabia</div>
+          <div className={classes.grayText}>RAKstarter account</div>
+          <div className={classes.divider} />
+          <div className={classes.secondaryTitle}>Services selected</div>
+          <div className={classes.grayText}>
+            AED & USD
+            <br />
+            Any of you can sign
+            <br />
+            Debit cards for all signatories
+            <br />
+            Online bank statements
+          </div>
         </div>
-        <div className={classes.mainTitle}>Designit Arabia</div>
-        <div className={classes.grayText}>RAKstarter account</div>
-        <div className={classes.divider} />
-        <div className={classes.secondaryTitle}>Services selected</div>
-        <div className={classes.grayText}>
-          AED & USD
-          <br />
-          Any of you can sign
-          <br />
-          Debit cards for all signatories
-          <br />
-          Online bank statements
+        <div className={classes.checkboxesWrapper}>
+          <Checkbox
+            label="I confirm that the information provided is true and complete"
+            className={classes.listItem}
+          />
+          <Checkbox
+            label={
+              <span>
+                I agree with RakBank’s <a href="/">terms and conditions</a>
+              </span>
+            }
+            className={classes.listItem}
+          />
+          <Checkbox
+            label="I want to receive marketing and promotional communication from RakBank"
+            className={classes.listItem}
+          />
         </div>
-      </div>
-      <div className={classes.checkboxesWrapper}>
-        <Checkbox
-          label="I confirm that the information provided is true and complete"
-          className={classes.listItem}
-        />
-        <Checkbox
-          label={
-            <span>
-              I agree with RakBank’s <a href="/">terms and conditions</a>
-            </span>
-          }
-          className={classes.listItem}
-        />
-        <Checkbox
-          label="I want to receive marketing and promotional communication from RakBank"
-          className={classes.listItem}
-        />
-      </div>
-
-      <Button label="Submit" />
-    </>
-  );
-};
+        <Button label="Submit" />
+      </>
+    );
+  }
+}
 
 export default withStyles(style)(SubmitApplication);
