56f48c8bf612dc417f2f677cc1e83abb84dc0cda
OlegSuvorov <oleg.suvorov@designit.com>
2019-10-02 16:53:47 +0300

    add remove button, submit disable rules fix

--- webapply/src/components/FinalQuestions/CompanyBackgroundForm.js
+++ webapply/src/components/FinalQuestions/CompanyBackgroundForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import get from "lodash/get";
+import cx from "classnames";
 import SectionTitle from "../SectionTitle";
 import Checkbox from "../InputField/Checkbox";
 import Grid from "@material-ui/core/Grid";
@@ -9,6 +10,7 @@ import ContinueButton from "../Buttons/ContinueButton";
 import TextInput from "../InputField/TextInput";
 import PureSelect from "../InputField/PureSelect";
 import AddButton from "../Buttons/AddButton";
+import RemoveButton from "../Buttons/RemoveButton";
 import { updateField } from "../../store/actions/appConfig";
 import { getOrgKYCDetails } from "../../store/selectors/appConfig";
 
@@ -36,6 +38,28 @@ const styles = {
     display: "flex",
     justifyContent: "flex-end",
     margin: "20px 0 0"
+  },
+  relative: {
+    position: "relative"
+  },
+  tablet: {
+    "@media only screen and (max-width: 959px)": {
+      marginBottom: "20px"
+    }
+  },
+  container: {
+    top: "18px",
+    right: "-100px",
+    "@media only screen and (max-width: 959px)": {
+      top: "70px",
+      right: "auto",
+      left: 0
+    }
+  },
+  marginBottom: {
+    "@media only screen and (max-width: 959px)": {
+      marginBottom: "45px"
+    }
   }
 };
 
@@ -152,6 +176,10 @@ class CompanyBackgroundForm extends Component {
       name: "prospect.orgKYCDetails.otherBankingRelationshipsInfo.otherBankDetails",
       value: [{ bankName: "" }]
     });
+    this.props.updateField({
+      name: "prospect.orgKYCDetails.otherBankingRelationshipsInfo.otherBankingRelationshipsExist",
+      value: false
+    });
   }
 
   filterAndUpdateTopCustomersValues() {
@@ -194,6 +222,15 @@ class CompanyBackgroundForm extends Component {
     }
   };
 
+  handleRemoveTopCustomer = index => {
+    const dataList = this.getTopCustomerData();
+    dataList.splice(index, 1);
+    this.props.updateField({
+      name: "prospect.orgKYCDetails.topCustomers",
+      value: [...dataList]
+    });
+  };
+
   handleAddSupplierClick = () => {
     const dataList = this.getTopSupplierData();
     if (dataList.length < this.limits.supplierCount) {
@@ -202,6 +239,15 @@ class CompanyBackgroundForm extends Component {
         value: [...dataList, this.getEmptyCustomerSupplierItem()]
       });
     }
+  };
+
+  handleRemoveSupplier = index => {
+    const dataList = this.getTopSupplierData();
+    dataList.splice(index, 1);
+    this.props.updateField({
+      name: "prospect.orgKYCDetails.topSuppliers",
+      value: [...dataList]
+    });
   };
 
   handleAddCountryOfOriginClick = () => {
@@ -214,6 +260,15 @@ class CompanyBackgroundForm extends Component {
     }
   };
 
+  handleRemoveTopGood = index => {
+    const dataList = this.getTopOriginGoodsCountries();
+    dataList.splice(index, 1);
+    this.props.updateField({
+      name: "prospect.orgKYCDetails.topOriginGoodsCountries",
+      value: [...dataList]
+    });
+  };
+
   handleAddAnotherBank = () => {
     const dataList = this.getOtherBankingRelationshipsInfo();
     if (dataList.length < this.limits.anotherBankCount) {
@@ -224,12 +279,29 @@ class CompanyBackgroundForm extends Component {
     }
   };
 
+  handleRemoveBankInfo = index => {
+    const dataList = this.getOtherBankingRelationshipsInfo();
+    dataList.splice(index, 1);
+    this.props.updateField({
+      name: "prospect.orgKYCDetails.otherBankingRelationshipsInfo.otherBankDetails",
+      value: [...dataList]
+    });
+  };
+
   handleSubmit = event => {
     event.preventDefault();
     this.filterAndUpdateTopCustomersValues();
     this.filterAndUpdateTopSuppliersValues();
 
     this.props.handleContinue(event);
+  };
+
+  handleSwitchOtherBankAccounts = e => {
+    this.props.updateField({
+      name: "prospect.orgKYCDetails.otherBankingRelationshipsInfo.otherBankingRelationshipsExist",
+      value: e.target.checked
+    });
+    this.setState({ isDontHaveOtherBankAccounts: !e.target.checked });
   };
 
   isTopCustomerNameRequired(index) {
@@ -272,7 +344,12 @@ class CompanyBackgroundForm extends Component {
     const { supplierCount } = this.limits;
     const suppliers = this.getTopSupplierData();
     const lastAddedSupplier = suppliers[suppliers.length - 1];
-    return isDontHaveSuppliers || suppliers.length >= supplierCount || !lastAddedSupplier.country;
+    return (
+      isDontHaveSuppliers ||
+      suppliers.length >= supplierCount ||
+      !lastAddedSupplier.name ||
+      !lastAddedSupplier.country
+    );
   };
 
   isAddTopOriginGoodsDisabled = () => {
@@ -290,8 +367,30 @@ class CompanyBackgroundForm extends Component {
     return banks.length >= anotherBankCount || !lastAddedBank.bankName;
   };
 
+  isContinueDisabled = () => {
+    const { isDontHaveOtherBankAccounts, isDontTradingGoods, isDontHaveSuppliers } = this.state;
+    const customers = this.getTopCustomerData();
+    const isTopCustomersFilled =
+      customers.length > 1 || !!(customers[0].name && customers[0].country);
+    const suppliers = this.getTopSupplierData();
+    const isTopSuppliersFilled =
+      isDontHaveSuppliers || suppliers.length > 1 || !!(suppliers[0].name && suppliers[0].country);
+    const goods = this.getTopOriginGoodsCountries();
+    const isOriginGoodsFilled = isDontTradingGoods || goods.length > 1 || !!goods[0];
+    const banks = this.getOtherBankingRelationshipsInfo();
+    const isAnotherBanksFilled =
+      isDontHaveOtherBankAccounts || banks.length > 1 || !!banks[0].bankName;
+    return !(
+      isTopCustomersFilled &&
+      isTopSuppliersFilled &&
+      isOriginGoodsFilled &&
+      isAnotherBanksFilled
+    );
+  };
+
   render() {
     const { isDontHaveSuppliers, isDontTradingGoods, isDontHaveOtherBankAccounts } = this.state;
+    const { classes } = this.props;
     return (
       <form noValidate onSubmit={this.handleSubmit}>
         <SectionTitle title="Business relationships" className={this.props.classes.title} />
@@ -301,20 +400,31 @@ class CompanyBackgroundForm extends Component {
           {this.getTopCustomerData().map((_, index) => {
             return (
               <React.Fragment key={index}>
-                <Grid item md={6} sm={12}>
+                <Grid item md={index === 0 ? 6 : 5} sm={12}>
                   <TextInput
                     id="OkycTopc.name"
                     indexes={[index]}
                     required={this.isTopCustomerNameRequired(index)}
                   />
                 </Grid>
-                <Grid item md={6} sm={12}>
+                <Grid
+                  item
+                  md={index === 0 ? 6 : 5}
+                  sm={12}
+                  className={cx(classes.relative, { [classes.tablet]: index !== 0 })}
+                >
                   <PureSelect
                     id="OkycTopc.country"
                     indexes={[index]}
                     resetValue={""}
                     required={this.isTopCustomerCountyRequired(index)}
                   />
+                  {index !== 0 && (
+                    <RemoveButton
+                      onClick={() => this.handleRemoveTopCustomer(index)}
+                      title="Remove"
+                    />
+                  )}
                 </Grid>
               </React.Fragment>
             );
@@ -338,7 +448,7 @@ class CompanyBackgroundForm extends Component {
           {this.getTopSupplierData().map((_, index) => {
             return (
               <React.Fragment key={index}>
-                <Grid item md={6} sm={12}>
+                <Grid item md={index === 0 ? 6 : 5} sm={12}>
                   <TextInput
                     id="OkycTops.name"
                     indexes={[index]}
@@ -346,7 +456,12 @@ class CompanyBackgroundForm extends Component {
                     disabled={isDontHaveSuppliers}
                   />
                 </Grid>
-                <Grid item md={6} sm={12}>
+                <Grid
+                  item
+                  md={index === 0 ? 6 : 5}
+                  sm={12}
+                  className={cx(classes.relative, { [classes.tablet]: index !== 0 })}
+                >
                   <PureSelect
                     id="OkycTops.country"
                     indexes={[index]}
@@ -354,6 +469,9 @@ class CompanyBackgroundForm extends Component {
                     required={this.isTopSupplierCountryRequired(index)}
                     disabled={isDontHaveSuppliers}
                   />
+                  {index !== 0 && (
+                    <RemoveButton onClick={() => this.handleRemoveSupplier(index)} title="Remove" />
+                  )}
                 </Grid>
               </React.Fragment>
             );
@@ -373,10 +491,16 @@ class CompanyBackgroundForm extends Component {
           onChange={event => this.setState({ isDontTradingGoods: event.target.checked })}
           label="I don't trade with goods yet"
         />
-        <Grid container spacing={3} className={this.props.classes.flexContainer}>
-          <Grid item md={6} sm={12}>
-            {this.getTopOriginGoodsCountries().map((_, index) => {
-              return (
+        <Grid container direction="column" spacing={3} className={this.props.classes.flexContainer}>
+          {this.getTopOriginGoodsCountries().map((_, index) => {
+            return (
+              <Grid
+                key={index}
+                item
+                md={6}
+                sm={12}
+                className={cx(classes.relative, { [classes.tablet]: index !== 0 })}
+              >
                 <PureSelect
                   key={index}
                   id="Okyc.topOriginGoodsCountries"
@@ -388,9 +512,12 @@ class CompanyBackgroundForm extends Component {
                   required={this.isCountryOriginGoodsRequired(index)}
                   disabled={isDontTradingGoods}
                 />
-              );
-            })}
-          </Grid>
+                {index !== 0 && (
+                  <RemoveButton onClick={() => this.handleRemoveTopGood(index)} title="Remove" />
+                )}
+              </Grid>
+            );
+          })}
         </Grid>
         <AddButton
           onClick={this.handleAddCountryOfOriginClick}
@@ -404,7 +531,7 @@ class CompanyBackgroundForm extends Component {
         <Checkbox
           label="The company has accounts with other banks, inside or outside the UAE"
           value={!isDontHaveOtherBankAccounts}
-          onChange={event => this.setState({ isDontHaveOtherBankAccounts: !event.target.checked })}
+          onChange={e => this.handleSwitchOtherBankAccounts(e)}
         />
         {!isDontHaveOtherBankAccounts && (
           <>
@@ -413,13 +540,25 @@ class CompanyBackgroundForm extends Component {
                 {this.getOtherBankingRelationshipsInfo().map((_, index) => {
                   return (
                     <React.Fragment key={index}>
-                      <Grid item sm={12}>
+                      <Grid
+                        item
+                        sm={12}
+                        md={index === 0 ? 12 : 10}
+                        className={cx(classes.relative, { [classes.marginBottom]: index !== 0 })}
+                      >
                         <TextInput
                           id="OkycObriObd.bankName"
                           indexes={[index]}
                           required={index === 0 && !isDontHaveOtherBankAccounts}
                           disabled={isDontHaveOtherBankAccounts}
                         />
+                        {index !== 0 && (
+                          <RemoveButton
+                            onClick={() => this.handleRemoveBankInfo(index)}
+                            title="Remove"
+                            classes={{ container: classes.container }}
+                          />
+                        )}
                       </Grid>
                     </React.Fragment>
                   );
@@ -435,7 +574,7 @@ class CompanyBackgroundForm extends Component {
         )}
 
         <div className={this.props.classes.controlsWrapper}>
-          <ContinueButton type="submit" />
+          <ContinueButton disabled={this.isContinueDisabled()} type="submit" />
         </div>
       </form>
     );
