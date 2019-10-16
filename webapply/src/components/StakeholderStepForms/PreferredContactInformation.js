efebf07c5cc6c277bf650f068294ebf774804c1f
Anna Korzhavykh <anna.korzhavykh@designit.com>
2019-10-16 18:35:36 +0300

    Refactor PreferredContactInformation

--- webapply/src/components/StakeholderStepForms/PreferredContactInformation.js
+++ webapply/src/components/StakeholderStepForms/PreferredContactInformation.js
@@ -1,99 +1,59 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import { withStyles } from "@material-ui/core";
 import { connect } from "react-redux";
 import TextInput from "../InputField/TextInput";
 import PureSelect from "../InputField/PureSelect";
-import AddButton from "../Buttons/AddButton";
-import RemoveButton from "../Buttons/RemoveButton";
 import { getInputValueById } from "../../store/selectors/input";
 import { updateProspect } from "../../store/actions/appConfig";
+import InfoTitle from "../InfoTitle";
 
-const styles = {
-  relative: {
-    position: "relative"
-  },
-  removeButton: { right: "-45px" }
-};
-
-class PreferredContactInformation extends React.Component {
-  state = {
-    isShownLandlineNumber: false
-  };
-
-  removeLandlineNumber = () => {
-    this.setState({ isShownLandlineNumber: false });
-    const { index } = this.props;
-    this.props.updateProspect({
-      [`prospect.signatoryInfo[${index}].contactDetails.primaryPhoneNo`]: ""
-    });
-  };
-
-  addLandlineNumber = () => {
-    this.setState({ isShownLandlineNumber: true });
-  };
-
-  render() {
-    const { classes, isSignatory } = this.props;
-    return (
-      <>
-        <Grid container spacing={3}>
-          <Grid item md={6} sm={12}>
-            <TextInput
-              id="SigCont.primaryMobileNo"
-              indexes={[this.props.index]}
-              disabled={!isSignatory}
-              select={
-                <PureSelect
-                  id="SigCont.primaryMobCountryCode"
-                  indexes={[this.props.index]}
-                  defaultValue="UAE"
-                  combinedSelect
-                  disabled={!isSignatory}
-                />
-              }
-            />
-          </Grid>
-          <Grid item md={6} sm={12}>
-            <TextInput
-              id="SigCont.primaryEmail"
-              indexes={[this.props.index]}
-              disabled={!isSignatory}
-            />
-          </Grid>
+const PreferredContactInformation = props => {
+  const { isSignatory, index } = props;
+  return (
+    <>
+      <Grid container spacing={3}>
+        <Grid item sm={12}>
+          <TextInput id="SigCont.primaryEmail" indexes={[index]} disabled={!isSignatory} />
         </Grid>
-
-        {this.state.isShownLandlineNumber ? (
-          <Grid container spacing={3}>
-            <Grid item md={6} sm={12} className={classes.relative}>
-              <TextInput
-                id="SigCont.primaryPhoneNo"
-                indexes={[this.props.index]}
+      </Grid>
+      <Grid item container spacing={3}>
+        <Grid item md={6} sm={12}>
+          <TextInput
+            id="SigCont.primaryMobileNo"
+            indexes={[index]}
+            disabled={!isSignatory}
+            select={
+              <PureSelect
+                id="SigCont.primaryMobCountryCode"
+                indexes={[index]}
+                defaultValue="UAE"
+                combinedSelect
                 disabled={!isSignatory}
-                select={
-                  <PureSelect
-                    id="SigCont.primaryPhoneCountryCode"
-                    indexes={[this.props.index]}
-                    defaultValue="UAE"
-                    combinedSelect
-                    disabled={!isSignatory}
-                  />
-                }
               />
-              <RemoveButton
-                onClick={this.removeLandlineNumber}
-                className={classes.removeButton}
-                title=""
+            }
+          />
+        </Grid>
+        <Grid item md={6} sm={12}>
+          <TextInput
+            id="SigCont.primaryPhoneNo"
+            indexes={[index]}
+            disabled={!isSignatory}
+            select={
+              <PureSelect
+                id="SigCont.primaryPhoneCountryCode"
+                indexes={[index]}
+                defaultValue="UAE"
+                combinedSelect
+                disabled={!isSignatory}
               />
-            </Grid>
-          </Grid>
-        ) : (
-          <AddButton title="Add a landline number" onClick={this.addLandlineNumber} />
-        )}
-      </>
-    );
-  }
-}
+            }
+          />
+        </Grid>
+      </Grid>
+      <InfoTitle title="Heads up! We can only issue chequebooks if you use a phone number from the UAE." />
+    </>
+  );
+};
 
 const mapStateToProps = (state, { index }) => ({
   isSignatory: getInputValueById(state, "SigKycd.isSignatory", [index])
@@ -101,9 +61,7 @@ const mapStateToProps = (state, { index }) => ({
 
 const mapDispatchToProps = { updateProspect };
 
-export default withStyles(styles)(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(PreferredContactInformation)
-);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PreferredContactInformation);
