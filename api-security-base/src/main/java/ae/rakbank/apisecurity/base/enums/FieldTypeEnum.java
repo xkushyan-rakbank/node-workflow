e8ee4497fd05a6dbda80439109165ff64a1f5cae
shailesh kumar <shailesh.kumar16@wipro.com>
2019-10-18 13:07:44 +0000

    Updated FieldTypeEnum.java

---
+++ api-security-base/src/main/java/ae/rakbank/apisecurity/base/enums/FieldTypeEnum.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package ae.rakbank.apisecurity.base.enums;
+
+/**
+ * @author Sumanth V
+ *
+ *         Apr 1, 2019
+ */
+public enum FieldTypeEnum {
+	NUMBER, TEXT, DATE, TIMESTAMP, BOOLEAN
+}
