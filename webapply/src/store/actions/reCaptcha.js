b381fdc8ae2fd1d3727ea38c74c34fe784fb1182
Ivan Shinkarenko <ivan.shinkarenko@designit.com>
2019-10-22 11:48:03 +0300

    update autoSaveProspect

--- webapply/src/store/actions/reCaptcha.js
+++ webapply/src/store/actions/reCaptcha.js
@@ -4,34 +4,18 @@ export const SET_TOKEN = "RECAPTCHA/SET_TOKEN";
 export const SET_ERROR = "RECAPTCHA/SET_ERROR";
 export const SET_VERIFIED = "RECAPTCHA/SET_VERIFIED";
 
-/**
- * @param {Boolean} payload
- * @return {{payload: Boolean, type: String}}
- */
 export const setPending = payload => {
   return { type: SET_PENDING, payload };
 };
 
-/**
- * @param {String} payload
- * @return {{payload: String, type: String}}
- */
 export const setToken = payload => {
   return { type: SET_TOKEN, payload };
 };
 
-/**
- * @param {String} payload
- * @return {{payload: String, type: String}}
- */
 export const setError = payload => {
   return { type: SET_ERROR, payload };
 };
 
-/**
- * @param {Boolean} payload
- * @return {{payload: Boolean, type: String}}
- */
 export const setVerified = payload => {
   return { type: SET_VERIFIED, payload };
 };
--- webapply/src/store/sagas/sendProspectToAPI.js
+++ webapply/src/store/sagas/sendProspectToAPI.js
@@ -1,4 +1,15 @@
-import { all, put, call, delay, select, takeLatest, take } from "redux-saga/effects";
+import {
+  all,
+  put,
+  call,
+  delay,
+  select,
+  takeLatest,
+  take,
+  cancel,
+  cancelled,
+  fork
+} from "redux-saga/effects";
 import {
   SEND_PROSPECT_TO_API,
   sendProspectToAPISuccess,
@@ -28,21 +39,37 @@ function* sendProspectToAPISaga() {
   }
 }
 
-function* prospectAutoSaveSaga() {
+function* prospectAutoSave() {
   try {
-    const { prospectId } = yield take("UPDATE_PROSPECT_ID");
-    while (prospectId) {
+    while (true) {
       const state = yield select();
       const prospect = getProspect(state);
+      const prospectId = getProspectId(state);
+
       yield call(apiClient.prospect.update, prospectId, prospect);
       yield delay(40000);
     }
   } finally {
-    console.log("sws");
+    if (yield cancelled()) {
+      console.log("cancel");
+    }
+  }
+}
+
+function* prospectAutoSaveFlow() {
+  while (yield take("UPDATE_PROSPECT_ID")) {
+    const bgSyncAutoSave = yield fork(prospectAutoSave);
+
+    // wait for the user stop action
+    const { actionType } = yield take("UPDATE_ACTION_TYPE"); // need some action
+    if (actionType === "submit") {
+      // this will cause the forked bgSyncAutoSave task to jump into its finally block
+      yield cancel(bgSyncAutoSave);
+    }
   }
 }
 
 export default function* sendProspectToAPI() {
   yield all([takeLatest(SEND_PROSPECT_TO_API, sendProspectToAPISaga)]);
-  yield all([takeLatest(PROSPECT_AUTO_SAVE, prospectAutoSaveSaga)]);
+  yield all([takeLatest(PROSPECT_AUTO_SAVE, prospectAutoSaveFlow)]);
 }
