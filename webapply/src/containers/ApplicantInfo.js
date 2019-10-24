dcfebffe9bbb4d78e7b7be151db7082ad42b6108
Ivan Shinkarenko <ivan.shinkarenko@designit.com>
2019-10-24 11:41:40 +0300

    update Aplicant info - revert prev changes

--- webapply/src/containers/ApplicantInfo.js
+++ webapply/src/containers/ApplicantInfo.js
@@ -13,7 +13,7 @@ import { applicantInfoForm } from "../store/actions/applicantInfoForm";
 import { setToken, setVerified, verifyToken } from "../store/actions/reCaptcha";
 import { generateOtpCode } from "../store/actions/otp";
 import { receiveAppConfig, updateActionType } from "./../store/actions/appConfig";
-// import * as reCaptchaSelectors from "../store/selectors/reCaptcha";
+import * as reCaptchaSelectors from "../store/selectors/reCaptcha";
 import * as appConfigSelectors from "../store/selectors/appConfig";
 import * as otpSelectors from "../store/selectors/otp";
 import * as inputSelectors from "../store/selectors/input";
@@ -64,7 +64,7 @@ class BasicsForm extends React.Component {
   };
 
   render() {
-    const { classes, lastInputValue } = this.props;
+    const { classes, lastInputValue, reCaptchaToken } = this.props;
 
     return (
       <>
@@ -96,7 +96,11 @@ class BasicsForm extends React.Component {
             <div className="linkContainer">
               <BackLink path={routes.detailedAccount} />
 
-              <SubmitButton label="Next Step" justify="flex-end" disabled={!lastInputValue} />
+              <SubmitButton
+                label="Next Step"
+                justify="flex-end"
+                disabled={!lastInputValue || !reCaptchaToken}
+              />
             </div>
           </Grid>
         </form>
@@ -108,7 +112,7 @@ class BasicsForm extends React.Component {
 const mapStateToProps = state => ({
   prospectId: appConfigSelectors.getProspectId(state),
   otp: otpSelectors.getOtp(state),
-  // reCaptchaToken: reCaptchaSelectors.getReCaptchaToken(state),
+  reCaptchaToken: reCaptchaSelectors.getReCaptchaToken(state),
   lastInputValue: inputSelectors.getInputValueById(state, "Aplnt.mobileNo"),
   isProceed: appConfigSelectors.getProceedStatus(state),
   serverError: appConfigSelectors.getServerErrorStatus(state),
