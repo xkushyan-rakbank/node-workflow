c146eb73b485e19f11309aa11f9fdf9b130032fd
bhakta <bh20107429@wipro.com>
2019-11-10 18:00:10 +0400

    extra document upload functionality

--- webapply/src/store/actions/getProspectDocuments.js
+++ webapply/src/store/actions/getProspectDocuments.js
@@ -1,5 +1,7 @@
 export const RETRIEVE_DOC_UPLOADER = "RETRIEVE_DOC_UPLOADER";
 export const UPLOAD_SUCCESS = "UPLOAD_SUCCESS";
+export const EXTRA_DOC_UPLOAD_SUCCESS = "EXTRA_DOC_UPLOAD_SUCCESS";
+export const DELETE_EXTRA_DOC_UPLOAD_SUCCESS = "DELETE_EXTRA_DOC_UPLOAD_SUCCESS";
 
 export const retrieveDocDetails = () => {
   return { type: RETRIEVE_DOC_UPLOADER };
@@ -7,4 +9,13 @@ export const retrieveDocDetails = () => {
 
 export const docUploadSuccess = (payload, docDetails) => {
   return { type: "UPLOAD_SUCCESS", payload, docDetails };
+};
+
+export const extraDocUploadSuccess = payload => {
+  return { type: "EXTRA_DOC_UPLOAD_SUCCESS", payload };
+};
+
+export const deleteExtraDocUploadSuccess = payload => {
+  console.log(payload);
+  return { type: "DELETE_EXTRA_DOC_UPLOAD_SUCCESS", payload };
 };
