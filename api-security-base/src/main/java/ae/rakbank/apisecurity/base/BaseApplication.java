a11c630f8c4e831aedd28503ee6812b3225d17dd
shailesh kumar <shailesh.kumar16@wipro.com>
2019-11-11 10:10:29 +0000

    Updated BaseApplication.java

---
+++ api-security-base/src/main/java/ae/rakbank/apisecurity/base/BaseApplication.java
@@ -0,0 +1,13 @@
+package ae.rakbank.apisecurity.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class BaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseApplication.class, args);
+	}
+}
