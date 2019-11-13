11585b19eed002b2d28c37b712985cab5d3520ef
VjacheslavZ <vjacheslav.zemlyaniy@designit.com>
2019-11-13 16:30:20 +0200

    refactoring AccountDetails component

--- webapply/src/containers/SelectServices/components/AccountDetails/AccountDetails.js
+++ webapply/src/containers/SelectServices/components/AccountDetails/AccountDetails.js
@@ -1,74 +1,62 @@
 import React from "react";
-import cx from "classnames";
 import get from "lodash/get";
 import isEmpty from "lodash/isEmpty";
-import { compose } from "recompose";
+import { compose } from "redux";
 import { connect } from "react-redux";
 import { withStyles } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
-import { getInputValueById, getFieldConfigById } from "../../../../store/selectors/input";
-import Checkbox from "../../../../components/InputField/RefactoredCheckbox";
-import CheckboxGroup from "../../../../components/InputField/CheckboxGroup";
-import FormWrapper from "../../../../components/StakeholderStepForms/FormWrapper";
-import PureSelect from "../../../../components/InputField/PureSelect";
+
 import { getOptionsForSubId } from "../../../../utils/getInputSubOptions";
+import { getInputValueById, getFieldConfigById } from "../../../../store/selectors/input";
 
-const style = {
-  formWrapper: {
-    margin: 0
-  },
-  checkboxesWrapper: {
-    marginBottom: "30px"
-  },
-  checkboxWrapper: {
-    marginTop: "10px"
-  },
-  sectionLabel: {
-    padding: "30px 0 20px"
-  },
-  topDivider: {
-    borderTop: "1px solid rgba(230, 230, 230, 0.5)",
-    marginTop: "30px"
-  }
-};
+import CheckboxGroup from "../../../../components/InputField/CheckboxGroup";
+import Checkbox from "../../../../components/InputField/RefactoredCheckbox";
+import PureSelect from "../../../../components/InputField/PureSelect";
+import FormWrapper from "../../../../components/StakeholderStepForms/FormWrapper";
+import Subtitle from "../../../../components/Subtitle";
+import Divider from "../../../../components/Divider";
 
-class AccountDetails extends React.Component {
-  render() {
-    const { classes, islamicBanking, branchCityValue, branchCityConfig } = this.props;
-    const subOptions = getOptionsForSubId(branchCityValue, branchCityConfig, true);
+import { styled } from "./styled";
 
-    return (
-      <FormWrapper className={classes.formWrapper} handleContinue={this.props.goToNext}>
-        <div className={cx("text", classes.sectionLabel)}>Select currencies</div>
-        <CheckboxGroup id="Acnt.accountCurrencies" indexes={[0]} />
+const AccountDetails = props => {
+  const { classes, islamicBanking, branchCityValue, branchCityConfig, goToNext } = props;
+  const subOptions = getOptionsForSubId(branchCityValue, branchCityConfig, true);
 
-        <div className={cx("text", classes.topDivider, classes.sectionLabel)}>Select branch</div>
-        <Grid container spacing={3}>
-          <Grid item md={6} sm={12}>
-            <PureSelect id="Org.branchCity" indexes={[0, 0]} />
-          </Grid>
-          <Grid item md={6} sm={12}>
-            <PureSelect
-              id="Org.subCategory"
-              indexes={[0, 0]}
-              subOptions={subOptions}
-              disabled={isEmpty(branchCityValue)}
-            />
-          </Grid>
+  const index = [0];
+  const indexes = [0, 0];
+
+  return (
+    <FormWrapper className={classes.formWrapper} handleContinue={goToNext}>
+      <Subtitle title="Select currencies" />
+      <CheckboxGroup id="Acnt.accountCurrencies" indexes={index} />
+
+      <Divider />
+
+      <Subtitle title="Select branch" />
+      <Grid container spacing={3}>
+        <Grid item md={6} sm={12}>
+          <PureSelect id="Org.branchCity" indexes={indexes} />
         </Grid>
+        <Grid item md={6} sm={12}>
+          <PureSelect
+            id="Org.subCategory"
+            indexes={indexes}
+            subOptions={subOptions}
+            disabled={isEmpty(branchCityValue)}
+          />
+        </Grid>
+      </Grid>
 
-        {!islamicBanking && (
-          <>
-            <div className={cx("text", classes.topDivider, classes.sectionLabel)}>
-              Select interest
-            </div>
-            <Checkbox id="Acnt.receiveInterest" indexes={[0]} />
-          </>
-        )}
-      </FormWrapper>
-    );
-  }
-}
+      {!islamicBanking && (
+        <>
+          <Divider />
+          <Subtitle title="Select interest" />
+          <Checkbox id="Acnt.receiveInterest" indexes={index} />
+        </>
+      )}
+    </FormWrapper>
+  );
+};
 
 const mapStateToProps = state => ({
   islamicBanking: get(state.appConfig, "prospect.applicationInfo.islamicBanking"),
@@ -77,9 +65,6 @@ const mapStateToProps = state => ({
 });
 
 export default compose(
-  withStyles(style),
-  connect(
-    mapStateToProps,
-    {}
-  )
+  withStyles(styled),
+  connect(mapStateToProps)
 )(AccountDetails);
---
+++ webapply/src/containers/SelectServices/components/AccountDetails/styled.js
@@ -0,0 +1,5 @@
+export const styled = {
+  formWrapper: {
+    margin: 0
+  }
+};
