be973cae828859b3d7ed47b51db37a8ec64db944
jarick <yaroslav.zolotarev@designit.com>
2019-11-16 21:24:14 +0400

    fix: refactoring `SignatoriesList` component

--- webapply/src/containers/SelectServices/components/Channels/SignatoriesList/index.js
+++ webapply/src/containers/SelectServices/components/Channels/SignatoriesList/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import get from "lodash/get";
 
 import InfoTitle from "../../../../../components/InfoTitle";
 import TextInput from "../../../../../components/InputField/TextInput";
@@ -18,21 +17,21 @@ export const SignatoriesList = ({ stakeholders }) => {
       />
 
       <div className={classes.signatoryNamesContainer}>
-        {stakeholders.map((stakeholder, index) => {
-          const { firstName, lastName } = stakeholder;
-          const isSignatory = get(stakeholder, "kycDetails.isSignatory");
-
-          return isSignatory ? (
-            <div className={classes.signatoryName} key={index}>
-              <span>{`${firstName} ${lastName}`}</span>
-              <TextInput
-                id="SigDbtcAuths.nameOnDebitCard"
-                indexes={[index]}
-                classes={{ regularWrapper: classes.selectCombined }}
-              />
-            </div>
-          ) : null;
-        })}
+        {stakeholders.map(
+          ({ firstName, lastName, kycDetails = {} }, index) =>
+            kycDetails.isSignatory && (
+              <div className={classes.signatoryName} key={index}>
+                <span>
+                  {firstName} {lastName}
+                </span>
+                <TextInput
+                  id="SigDbtcAuths.nameOnDebitCard"
+                  indexes={[index]}
+                  classes={{ regularWrapper: classes.selectCombined }}
+                />
+              </div>
+            )
+        )}
       </div>
     </>
   );
