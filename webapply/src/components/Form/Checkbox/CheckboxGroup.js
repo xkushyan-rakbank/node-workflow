311a699d6389c718095aaf5bc37ddda3d675b396
jarick <yaroslav.zolotarev@designit.com>
2019-11-20 16:06:43 +0400

    fix: code style issues

--- webapply/src/components/Form/Checkbox/CheckboxGroup.js
+++ webapply/src/components/Form/Checkbox/CheckboxGroup.js
@@ -1,30 +1,26 @@
 import React from "react";
 import FormControl from "@material-ui/core/FormControl";
+import { styled } from "@material-ui/styles";
 
 import { CustomCheckbox } from "./CustomCheckbox";
 import { InfoTitle } from "../../Notifications/index";
 import { ErrorMessage } from "../../Notifications/index";
 
-import { useStyles } from "./styled";
+const CheckboxesWrapper = styled("div")({
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr"
+});
 
-export const CheckboxGroup = ({
-  id,
-  options,
-  infoTitle,
-  field,
-  form,
-  form: { errors, touched }
-}) => {
-  const classes = useStyles();
+export const CheckboxGroup = ({ options, infoTitle, field, form: { errors, touched } }) => {
   const isError = errors[field.name] && touched[field.name];
 
   return (
-    <FormControl className={classes.formControl}>
-      <div className={classes.checkboxesWrapper}>
+    <FormControl className="formControl">
+      <CheckboxesWrapper>
         {options.map(({ key, value, displayText }) => (
           <CustomCheckbox key={key} value={value} label={displayText} {...field} />
         ))}
-      </div>
+      </CheckboxesWrapper>
 
       {isError && <ErrorMessage error={errors[field.name]} />}
       {infoTitle && <InfoTitle title={infoTitle} />}
