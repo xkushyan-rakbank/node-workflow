05b6225950dace7bf80b46d778daa27accf8f3bb
jarick <yaroslav.zolotarev@designit.com>
2019-11-24 18:09:50 +0400

    fix: Append question icon

--- webapply/src/components/Icons/icons.js
+++ webapply/src/components/Icons/icons.js
@@ -1,12 +1,15 @@
 import { ReactComponent as Done } from "../../assets/icons/add.svg";
 import { ReactComponent as Plus } from "../../assets/icons/add-icon.svg";
+import { ReactComponent as Question } from "../../assets/icons/question-mark.svg";
 
 export const iconComponents = {
   done: Done,
-  plus: Plus
+  plus: Plus,
+  question: Question
 };
 
 export const ICONS = {
   done: "done",
-  plus: "plus"
+  plus: "plus",
+  question: "question"
 };
