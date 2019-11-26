7868c96462514ec923dd3058860c8f7e68720b46
jarick <yaroslav.zolotarev@designit.com>
2019-11-26 14:17:51 +0400

    fix: `SigningPreferencesComponent` component

--- webapply/src/containers/SelectServices/SigningPreferences.js
+++ webapply/src/containers/SelectServices/SigningPreferences.js
@@ -1,16 +1,12 @@
 import { connect } from "react-redux";
 
-import { getSignatories } from "../../store/selectors/appConfig";
-import { getGeneralInputProps } from "../../store/selectors/input";
+import { getOrganizationInfo } from "../../store/selectors/appConfig";
 import { updateProspect } from "../../store/actions/appConfig";
 
 import { SigningPreferencesComponent } from "./components/SigningPreferences/SigningPreferences";
 
 const mapStateToProps = state => ({
-  signatoryInfo: getSignatories(state),
-  signInfoFullNameInput: getGeneralInputProps(state, "Sig.fullName", [0]),
-  accountSigningType: getGeneralInputProps(state, "SigAcntSig.accountSigningType", [0]),
-  accountSigningInstn: getGeneralInputProps(state, "SigAcntSig.accountSigningInstn", [0])
+  organizationInfo: getOrganizationInfo(state)
 });
 
 const mapDispatchToProps = {
