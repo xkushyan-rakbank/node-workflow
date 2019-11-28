55480ddecd75f4f91e2035e1bcb11dc4ce229d35
Anjali Tandon <anjali.tandon@wipro.com>
2019-11-28 16:36:28 +0400

    resolved review issue

--- webapply/src/containers/MyApplications/Grid.js
+++ webapply/src/containers/MyApplications/Grid.js
@@ -14,64 +14,72 @@ const ApplicationGrid = ({ getProspectInfo, applicantInfo = [] }) => {
     getProspectInfo(prospectId);
   };
 
-  let info;
-  applicantInfo.length &&
-    (info = applicantInfo.map(applications => {
-      return (
-        <div className={classes.gridContainer} key={applications.prospectId}>
-          <div className={classes.application}>
-            <img src={waves_background} className={classes.containerBg} alt="waves background" />
-            {applications.organizationInfo.companyName ? (
-              <>
-                <Typography variant="h6" component="span" classes={{ root: classes.title }}>
-                  {applications.organizationInfo.companyName}
-                </Typography>
-                <Typography
-                  variant="subtitle2"
-                  component="span"
-                  classes={{ root: classes.account }}
-                >
-                  {`${applications.applicationInfo.accountType} ${
-                    applications.applicationInfo.islamicBanking ? "islamic" : ""
-                  }`}
-                </Typography>
-              </>
-            ) : (
-              <>
-                <Typography variant="h6" component="span" classes={{ root: classes.title }}>
-                  {applications.applicantInfo.fullName}
-                </Typography>
-                <Typography
-                  variant="subtitle2"
-                  component="span"
-                  classes={{ root: classes.account }}
-                >
-                  {applications.applicantInfo.email}
-                </Typography>
-              </>
-            )}
-            <div className={classes.status}>{applications.status.statusNotes}</div>
-            <div className={classes.blockAction}>
-              {ctaStatus.includes(applications.status.statusNotes) ? (
-                <StyledWhiteContainedButton
-                  label={ctaWaterText[ctaStatus.indexOf(applications.status.statusNotes)]}
-                  handleClick={() => getProspectDetails(applications.prospectId)}
-                  disabled={
-                    applications.applicationInfo.lastModifiedBy.trim().length ? true : false
-                  }
+  return (
+    <>
+      {applicantInfo.length &&
+        applicantInfo.map(applications => {
+          return (
+            <div className={classes.gridContainer} key={applications.prospectId}>
+              <div className={classes.application}>
+                <img
+                  src={waves_background}
+                  className={classes.containerBg}
+                  alt="waves background"
                 />
-              ) : (
-                <span>
-                  {withoutCtaStatus.includes(applications.status.statusNotes) &&
-                    withoutCtaWaterText[withoutCtaStatus.indexOf(applications.status.statusNotes)]}
-                </span>
-              )}
+                {applications.organizationInfo.companyName ? (
+                  <>
+                    <Typography variant="h6" component="span" classes={{ root: classes.title }}>
+                      {applications.organizationInfo.companyName}
+                    </Typography>
+                    <Typography
+                      variant="subtitle2"
+                      component="span"
+                      classes={{ root: classes.account }}
+                    >
+                      {`${applications.applicationInfo.accountType} ${
+                        applications.applicationInfo.islamicBanking ? "islamic" : ""
+                      }`}
+                    </Typography>
+                  </>
+                ) : (
+                  <>
+                    <Typography variant="h6" component="span" classes={{ root: classes.title }}>
+                      {applications.applicantInfo.fullName}
+                    </Typography>
+                    <Typography
+                      variant="subtitle2"
+                      component="span"
+                      classes={{ root: classes.account }}
+                    >
+                      {applications.applicantInfo.email}
+                    </Typography>
+                  </>
+                )}
+                <div className={classes.status}>{applications.status.statusNotes}</div>
+                <div className={classes.blockAction}>
+                  {ctaStatus.includes(applications.status.statusNotes) ? (
+                    <StyledWhiteContainedButton
+                      label={ctaWaterText[ctaStatus.indexOf(applications.status.statusNotes)]}
+                      handleClick={() => getProspectDetails(applications.prospectId)}
+                      disabled={
+                        applications.applicationInfo.lastModifiedBy.trim().length ? true : false
+                      }
+                    />
+                  ) : (
+                    <span>
+                      {withoutCtaStatus.includes(applications.status.statusNotes) &&
+                        withoutCtaWaterText[
+                          withoutCtaStatus.indexOf(applications.status.statusNotes)
+                        ]}
+                    </span>
+                  )}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      );
-    }));
-  return <>{info}</>;
+          );
+        })}
+    </>
+  );
 };
 
 const mapDispatchToProps = {
--- webapply/src/containers/MyApplications/List.js
+++ webapply/src/containers/MyApplications/List.js
@@ -6,37 +6,33 @@ import { getProspectInfo } from "./../../store/actions/retrieveApplicantInfo";
 import { useStyles } from "./styled";
 import { ctaStatus, ctaWaterText, withoutCtaStatus, withoutCtaWaterText } from "./constants";
 
-export const StyledWhiteContainedButton = props => {
-  const Button = withStyles(() => ({
-    buttonStyle: {
-      boxShadow: "none",
-      border: "solid 1px #373737",
-      backgroundColor: "#fff",
-      width: 160,
-      height: 32,
-      padding: "0 10px",
-      "@media only screen and (max-width: 991px)": {
-        width: "130px"
-      },
-      "&:hover": {
-        backgroundColor: "#000",
-        "& span": {
-          color: "#fff"
-        }
-      }
+export const StyledWhiteContainedButton = withStyles(() => ({
+  buttonStyle: {
+    boxShadow: "none",
+    border: "solid 1px #373737",
+    backgroundColor: "#fff",
+    width: 160,
+    height: 32,
+    padding: "0 10px",
+    "@media only screen and (max-width: 991px)": {
+      width: "130px"
     },
-    labelStyle: {
-      color: "#373737",
-      fontSize: 14,
-      justifyContent: "center",
-      "@media only screen and (max-width: 991px)": {
-        fontSize: 12
+    "&:hover": {
+      backgroundColor: "#000",
+      "& span": {
+        color: "#fff"
       }
     }
-  }))(ContainedButton);
-
-  return <Button {...props} />;
-};
+  },
+  labelStyle: {
+    color: "#373737",
+    fontSize: 14,
+    justifyContent: "center",
+    "@media only screen and (max-width: 991px)": {
+      fontSize: 12
+    }
+  }
+}))(ContainedButton);
 
 const ApplicationList = ({ getProspectInfo, applicantInfo = [] }) => {
   const classes = useStyles();
@@ -45,54 +41,57 @@ const ApplicationList = ({ getProspectInfo, applicantInfo = [] }) => {
     getProspectInfo(prospectId);
   };
 
-  let info;
-  applicantInfo.length &&
-    (info = applicantInfo.map(applications => {
-      return (
-        <div className={classes.wrapper} key={applications.prospectId}>
-          <div className={classes.applicationRow}>
-            {applications.organizationInfo.companyName ? (
-              <div>
-                <div className={classes.companyName}>
-                  {applications.organizationInfo.companyName}
+  return (
+    <>
+      {applicantInfo.length &&
+        applicantInfo.map(applications => {
+          return (
+            <div className={classes.wrapper} key={applications.prospectId}>
+              <div className={classes.applicationRow}>
+                {applications.organizationInfo.companyName ? (
+                  <div>
+                    <div className={classes.companyName}>
+                      {applications.organizationInfo.companyName}
+                    </div>
+                    <div className={classes.listAccount}>
+                      {`${applications.applicationInfo.accountType} ${
+                        applications.applicationInfo.islamicBanking ? "islamic" : ""
+                      }`}
+                    </div>
+                  </div>
+                ) : (
+                  <div>
+                    <div className={classes.companyName}>{applications.applicantInfo.fullName}</div>
+                    <div className={classes.listAccount}>{applications.applicantInfo.email}</div>
+                  </div>
+                )}
+                <div>
+                  <span className={classes.listStatus}>{applications.status.statusNotes}</span>
                 </div>
-                <div className={classes.listAccount}>
-                  {`${applications.applicationInfo.accountType} ${
-                    applications.applicationInfo.islamicBanking ? "islamic" : ""
-                  }`}
+                <div className={classes.action}>
+                  {ctaStatus.includes(applications.status.statusNotes) ? (
+                    <StyledWhiteContainedButton
+                      label={ctaWaterText[ctaStatus.indexOf(applications.status.statusNotes)]}
+                      handleClick={() => getProspectDetails(applications.prospectId)}
+                      disabled={
+                        applications.applicationInfo.lastModifiedBy.trim().length ? true : false
+                      }
+                    />
+                  ) : (
+                    <span>
+                      {withoutCtaStatus.includes(applications.status.statusNotes) &&
+                        withoutCtaWaterText[
+                          withoutCtaStatus.indexOf(applications.status.statusNotes)
+                        ]}
+                    </span>
+                  )}
                 </div>
               </div>
-            ) : (
-              <div>
-                <div className={classes.companyName}>{applications.applicantInfo.fullName}</div>
-                <div className={classes.listAccount}>{applications.applicantInfo.email}</div>
-              </div>
-            )}
-            <div>
-              <span className={classes.listStatus}>{applications.status.statusNotes}</span>
             </div>
-            <div className={classes.action}>
-              {ctaStatus.includes(applications.status.statusNotes) ? (
-                <StyledWhiteContainedButton
-                  label={ctaWaterText[ctaStatus.indexOf(applications.status.statusNotes)]}
-                  handleClick={() => getProspectDetails(applications.prospectId)}
-                  disabled={
-                    applications.applicationInfo.lastModifiedBy.trim().length ? true : false
-                  }
-                />
-              ) : (
-                <span>
-                  {withoutCtaStatus.includes(applications.status.statusNotes) &&
-                    withoutCtaWaterText[withoutCtaStatus.indexOf(applications.status.statusNotes)]}
-                </span>
-              )}
-            </div>
-          </div>
-        </div>
-      );
-    }));
-
-  return <>{info}</>;
+          );
+        })}
+    </>
+  );
 };
 
 const mapDispatchToProps = {
