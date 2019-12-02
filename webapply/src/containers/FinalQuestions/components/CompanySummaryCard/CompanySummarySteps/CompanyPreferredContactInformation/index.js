a23b106734a205b0d269fab6ea63e73491c16ed4
OlegSuvorov <oleg.suvorov@designit.com>
2019-12-02 11:19:02 +0200

    minor code fixes

--- webapply/src/containers/FinalQuestions/components/CompanySummaryCard/CompanySummarySteps/CompanyPreferredContactInformation/index.js
+++ webapply/src/containers/FinalQuestions/components/CompanySummaryCard/CompanySummarySteps/CompanyPreferredContactInformation/index.js
@@ -1,9 +1,10 @@
 import { connect } from "react-redux";
+import get from "lodash/get";
 import { updateProspect } from "../../../../../../store/actions/appConfig";
 import { CompanyPreferredContactInformationComponent } from "./CompanyPreferredContactInformation";
 import { getOrganizationInfo, getAccountInfo } from "../../../../../../store/selectors/appConfig";
-import get from "lodash/get";
 import { prospect } from "../../../../../../constants/config";
+import { FIRST_ARRAY_INDEX } from "./constants";
 
 const mapStateToProps = state => ({
   primaryPhoneNo: get(
@@ -12,9 +13,9 @@ const mapStateToProps = state => ({
     prospect.organizationInfo.contactDetails.primaryPhoneNo
   ),
   chequeBookApplied: get(
-    getAccountInfo(state)[0],
+    getAccountInfo(state)[FIRST_ARRAY_INDEX],
     "chequeBookApplied",
-    prospect.accountInfo[0].chequeBookApplied
+    prospect.accountInfo[FIRST_ARRAY_INDEX].chequeBookApplied
   )
 });
 
