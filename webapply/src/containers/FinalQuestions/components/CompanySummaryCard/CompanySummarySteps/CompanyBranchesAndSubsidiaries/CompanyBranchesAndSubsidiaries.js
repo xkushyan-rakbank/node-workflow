4ec1e0dcaee1b2b5468aa6b4faee49a3f1886dd3
OlegSuvorov <oleg.suvorov@designit.com>
2019-12-03 12:21:37 +0200

    fix validation ui bugs

--- webapply/src/containers/FinalQuestions/components/CompanySummaryCard/CompanySummarySteps/CompanyBranchesAndSubsidiaries/CompanyBranchesAndSubsidiaries.js
+++ webapply/src/containers/FinalQuestions/components/CompanySummaryCard/CompanySummarySteps/CompanyBranchesAndSubsidiaries/CompanyBranchesAndSubsidiaries.js
@@ -76,198 +76,201 @@ export const CompanyBranchesAndSubsidiariesComponent = ({
         onSubmit={handleSubmit}
         validationSchema={companyBranchesAndSubsidiariesSchema}
       >
-        {({ values, setFieldValue }) => {
-          return (
-            <Form>
-              <FieldArray name="entitiesInUAE">
-                {arrayHelpers => (
-                  <>
-                    <h4 className={classes.groupLabel}>
-                      Branches or subsidiaries or other companies in the UAE
-                    </h4>
-                    <Field
-                      name="otherEntitiesInUAE"
-                      path="prospect.orgKYCDetails.otherEntitiesInUAE"
-                      label="The company has branches, subsidiaries or other companies in the UAE"
-                      component={Checkbox}
-                      onSelect={() => {
-                        if (values.otherEntitiesInUAE) {
-                          setFieldValue(
-                            "entitiesInUAE",
-                            prospect.orgKYCDetails.entitiesInUAE.map(item => ({
-                              ...item,
-                              id: uniqueId()
-                            }))
-                          );
-                          updateProspect({
-                            "prospect.orgKYCDetails.entitiesInUAE":
-                              prospect.orgKYCDetails.entitiesInUAE
-                          });
-                        }
-                      }}
-                    />
-                    {values.otherEntitiesInUAE && (
-                      <>
-                        <Grid container spacing={3} className={classes.flexContainer}>
-                          {values.entitiesInUAE.map((item, index) => (
-                            <React.Fragment key={item.id}>
-                              <Grid item sm={12}>
-                                <Field
-                                  name={`entitiesInUAE[${index}].companyName`}
-                                  path={`${basisPath}.entitiesInUAE[${index}].companyName`}
-                                  label="Company name"
-                                  placeholder="Company name"
-                                  component={Input}
+        {({ values, setFieldValue, setFieldTouched }) => (
+          <Form>
+            <FieldArray name="entitiesInUAE">
+              {arrayHelpers => (
+                <>
+                  <h4 className={classes.groupLabel}>
+                    Branches or subsidiaries or other companies in the UAE
+                  </h4>
+                  <Field
+                    name="otherEntitiesInUAE"
+                    path="prospect.orgKYCDetails.otherEntitiesInUAE"
+                    label="The company has branches, subsidiaries or other companies in the UAE"
+                    component={Checkbox}
+                    onSelect={() => {
+                      if (values.otherEntitiesInUAE) {
+                        setFieldValue(
+                          "entitiesInUAE",
+                          prospect.orgKYCDetails.entitiesInUAE.map(item => ({
+                            ...item,
+                            id: uniqueId()
+                          }))
+                        );
+                        updateProspect({
+                          "prospect.orgKYCDetails.entitiesInUAE":
+                            prospect.orgKYCDetails.entitiesInUAE
+                        });
+                        setFieldTouched("entitiesInUAE[0].companyName", false);
+                        setFieldTouched("entitiesInUAE[0].tradeLicenseNo", false);
+                        setFieldTouched("entitiesInUAE[0].emirate", false);
+                      }
+                    }}
+                  />
+                  {values.otherEntitiesInUAE && (
+                    <>
+                      <Grid container spacing={3} className={classes.flexContainer}>
+                        {values.entitiesInUAE.map((item, index) => (
+                          <React.Fragment key={item.id}>
+                            <Grid item sm={12}>
+                              <Field
+                                name={`entitiesInUAE[${index}].companyName`}
+                                path={`${basisPath}.entitiesInUAE[${index}].companyName`}
+                                label="Company name"
+                                placeholder="Company name"
+                                component={Input}
+                              />
+                            </Grid>
+                            <Grid
+                              item
+                              md={6}
+                              sm={12}
+                              className={cx(classes.relative, { [classes.tablet]: !index })}
+                            >
+                              <Field
+                                name={`entitiesInUAE[${index}].tradeLicenseNo`}
+                                path={`${basisPath}.entitiesInUAE[${index}].tradeLicenseNo`}
+                                label="Trade License Number"
+                                placeholder="Trade License Number"
+                                component={Input}
+                              />
+                            </Grid>
+                            <Grid
+                              item
+                              md={6}
+                              sm={12}
+                              className={cx(classes.relative, { [classes.tablet]: !index })}
+                            >
+                              <Field
+                                name={`entitiesInUAE[${index}].emirate`}
+                                path={`prospect.orgKYCDetails.entitiesInUAE[${index}].emirate`}
+                                datalistId="emirate"
+                                label="Emirate"
+                                extractLabel={item => item.displayText}
+                                component={CustomSelect}
+                              />
+                              {!!index && (
+                                <ArrayRemoveButton
+                                  arrayHelpers={arrayHelpers}
+                                  dataArray={entitiesInUAE}
+                                  itemIndex={index}
+                                  prospectPath="prospect.orgKYCDetails.entitiesInUAE"
+                                  className={classes.container}
+                                  title="Delete"
                                 />
-                              </Grid>
-                              <Grid
-                                item
-                                md={6}
-                                sm={12}
-                                className={cx(classes.relative, { [classes.tablet]: !index })}
-                              >
-                                <Field
-                                  name={`entitiesInUAE[${index}].tradeLicenseNo`}
-                                  path={`${basisPath}.entitiesInUAE[${index}].tradeLicenseNo`}
-                                  label="Trade License Number"
-                                  placeholder="Trade License Number"
-                                  component={Input}
+                              )}
+                            </Grid>
+                          </React.Fragment>
+                        ))}
+                      </Grid>
+                      {values.entitiesInUAE.length < limits.INSIDE_SUBSIDIARY_COUNT && (
+                        <ArrayAddButton
+                          title="Add another subsidiary inside the UAE"
+                          limit={limits.INSIDE_SUBSIDIARY_COUNT}
+                          requiredFields={["companyName", "tradeLicenseNo", "emirate"]}
+                          addedItem={{ companyName: "", tradeLicenseNo: "", emirate: "" }}
+                          dataArray={values.entitiesInUAE}
+                          arrayHelpers={arrayHelpers}
+                        />
+                      )}
+                    </>
+                  )}
+                </>
+              )}
+            </FieldArray>
+            <div className={classes.divider} />
+            <FieldArray name="entitiesOutsideUAE">
+              {arrayHelpers => (
+                <>
+                  <h4 className={classes.groupLabel}>
+                    Branches or subsidiaries or other companies outside the UAE
+                  </h4>
+                  <Field
+                    name="otherEntitiesOutsideUAE"
+                    path="prospect.orgKYCDetails.otherEntitiesOutsideUAE"
+                    label="The company has branches, subsidiaries or other companies outside the UAE"
+                    component={Checkbox}
+                    onSelect={() => {
+                      if (values.otherEntitiesOutsideUAE) {
+                        setFieldValue(
+                          "entitiesOutsideUAE",
+                          prospect.orgKYCDetails.entitiesOutsideUAE.map(item => ({
+                            ...item,
+                            id: uniqueId()
+                          }))
+                        );
+                        updateProspect({
+                          "prospect.orgKYCDetails.entitiesOutsideUAE":
+                            prospect.orgKYCDetails.entitiesOutsideUAE
+                        });
+                        setFieldTouched("entitiesOutsideUAE[0].companyName", false);
+                        setFieldTouched("entitiesOutsideUAE[0].country", false);
+                      }
+                    }}
+                  />
+                  {values.otherEntitiesOutsideUAE && (
+                    <>
+                      <Grid container spacing={3} className={classes.flexContainer}>
+                        {values.entitiesOutsideUAE.map((item, index) => (
+                          <React.Fragment key={item.id}>
+                            <Grid item md={6} sm={12}>
+                              <Field
+                                name={`entitiesOutsideUAE[${index}].companyName`}
+                                path={`${basisPath}.entitiesOutsideUAE[${index}].companyName`}
+                                label="Company name"
+                                placeholder="Company name"
+                                component={Input}
+                              />
+                            </Grid>
+                            <Grid
+                              item
+                              md={6}
+                              sm={12}
+                              className={cx({ [classes.relative]: !!index })}
+                            >
+                              <Field
+                                name={`entitiesOutsideUAE[${index}].country`}
+                                path={`${basisPath}.entitiesOutsideUAE[${index}].country`}
+                                label="Country"
+                                datalistId="country"
+                                extractLabel={item => item.displayText}
+                                component={CustomSelect}
+                              />
+                              {!!index && (
+                                <ArrayRemoveButton
+                                  arrayHelpers={arrayHelpers}
+                                  dataArray={entitiesOutsideUAE}
+                                  itemIndex={index}
+                                  prospectPath="prospect.orgKYCDetails.entitiesOutsideUAE"
+                                  className={classes.container}
+                                  title="Delete"
                                 />
-                              </Grid>
-                              <Grid
-                                item
-                                md={6}
-                                sm={12}
-                                className={cx(classes.relative, { [classes.tablet]: !index })}
-                              >
-                                <Field
-                                  name={`entitiesInUAE[${index}].emirate`}
-                                  path={`prospect.orgKYCDetails.entitiesInUAE[${index}].emirate`}
-                                  datalistId="emirate"
-                                  label="Emirate"
-                                  extractLabel={item => item.displayText}
-                                  component={CustomSelect}
-                                />
-                                {!!index && (
-                                  <ArrayRemoveButton
-                                    arrayHelpers={arrayHelpers}
-                                    dataArray={entitiesInUAE}
-                                    itemIndex={index}
-                                    prospectPath="prospect.orgKYCDetails.entitiesInUAE"
-                                    className={classes.container}
-                                    title="Delete"
-                                  />
-                                )}
-                              </Grid>
-                            </React.Fragment>
-                          ))}
-                        </Grid>
-                        {values.entitiesInUAE.length < limits.INSIDE_SUBSIDIARY_COUNT && (
-                          <ArrayAddButton
-                            title="Add another subsidiary inside the UAE"
-                            limit={limits.INSIDE_SUBSIDIARY_COUNT}
-                            requiredFields={["companyName", "tradeLicenseNo", "emirate"]}
-                            addedItem={{ companyName: "", tradeLicenseNo: "", emirate: "" }}
-                            dataArray={values.entitiesInUAE}
-                            arrayHelpers={arrayHelpers}
-                          />
-                        )}
-                      </>
-                    )}
-                  </>
-                )}
-              </FieldArray>
-              <div className={classes.divider} />
-              <FieldArray name="entitiesOutsideUAE">
-                {arrayHelpers => (
-                  <>
-                    <h4 className={classes.groupLabel}>
-                      Branches or subsidiaries or other companies outside the UAE
-                    </h4>
-                    <Field
-                      name="otherEntitiesOutsideUAE"
-                      path="prospect.orgKYCDetails.otherEntitiesOutsideUAE"
-                      label="The company has branches, subsidiaries or other companies outside the UAE"
-                      component={Checkbox}
-                      onSelect={() => {
-                        if (values.otherEntitiesOutsideUAE) {
-                          setFieldValue(
-                            "entitiesOutsideUAE",
-                            prospect.orgKYCDetails.entitiesOutsideUAE.map(item => ({
-                              ...item,
-                              id: uniqueId()
-                            }))
-                          );
-                          updateProspect({
-                            "prospect.orgKYCDetails.entitiesOutsideUAE":
-                              prospect.orgKYCDetails.entitiesOutsideUAE
-                          });
-                        }
-                      }}
-                    />
-                    {values.otherEntitiesOutsideUAE && (
-                      <>
-                        <Grid container spacing={3} className={classes.flexContainer}>
-                          {values.entitiesOutsideUAE.map((item, index) => (
-                            <React.Fragment key={item.id}>
-                              <Grid item md={6} sm={12}>
-                                <Field
-                                  name={`entitiesOutsideUAE[${index}].companyName`}
-                                  path={`${basisPath}.entitiesOutsideUAE[${index}].companyName`}
-                                  label="Company name"
-                                  placeholder="Company name"
-                                  component={Input}
-                                />
-                              </Grid>
-                              <Grid
-                                item
-                                md={6}
-                                sm={12}
-                                className={cx({ [classes.relative]: !!index })}
-                              >
-                                <Field
-                                  name={`entitiesOutsideUAE[${index}].country`}
-                                  path={`${basisPath}.entitiesOutsideUAE[${index}].country`}
-                                  label="Country"
-                                  datalistId="country"
-                                  extractLabel={item => item.displayText}
-                                  component={CustomSelect}
-                                />
-                                {!!index && (
-                                  <ArrayRemoveButton
-                                    arrayHelpers={arrayHelpers}
-                                    dataArray={entitiesOutsideUAE}
-                                    itemIndex={index}
-                                    prospectPath="prospect.orgKYCDetails.entitiesOutsideUAE"
-                                    className={classes.container}
-                                    title="Delete"
-                                  />
-                                )}
-                              </Grid>
-                            </React.Fragment>
-                          ))}
-                        </Grid>
-                        {values.entitiesOutsideUAE.length < limits.OUTSIDE_SUBSIDIARY_COUNT && (
-                          <ArrayAddButton
-                            title="Add another subsidiary outside the UAE"
-                            limit={limits.OUTSIDE_SUBSIDIARY_COUNT}
-                            requiredFields={["companyName", "country"]}
-                            addedItem={{ companyName: "", country: "" }}
-                            dataArray={values.entitiesOutsideUAE}
-                            arrayHelpers={arrayHelpers}
-                          />
-                        )}
-                      </>
-                    )}
-                  </>
-                )}
-              </FieldArray>
-              <div className={classes.buttonWrapper}>
-                <ContinueButton type="submit" />
-              </div>
-            </Form>
-          );
-        }}
+                              )}
+                            </Grid>
+                          </React.Fragment>
+                        ))}
+                      </Grid>
+                      {values.entitiesOutsideUAE.length < limits.OUTSIDE_SUBSIDIARY_COUNT && (
+                        <ArrayAddButton
+                          title="Add another subsidiary outside the UAE"
+                          limit={limits.OUTSIDE_SUBSIDIARY_COUNT}
+                          requiredFields={["companyName", "country"]}
+                          addedItem={{ companyName: "", country: "" }}
+                          dataArray={values.entitiesOutsideUAE}
+                          arrayHelpers={arrayHelpers}
+                        />
+                      )}
+                    </>
+                  )}
+                </>
+              )}
+            </FieldArray>
+            <div className={classes.buttonWrapper}>
+              <ContinueButton type="submit" />
+            </div>
+          </Form>
+        )}
       </Formik>
     </div>
   );
