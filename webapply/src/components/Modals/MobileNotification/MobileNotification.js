1ff9039e672c2fdc717b31f10d7847220dce79e1
jarick <yaroslav.zolotarev@designit.com>
2019-12-08 10:21:33 +0400

    fix: rename `desctop.svg` to `desktop.svg`

--- webapply/src/components/Modals/MobileNotification/MobileNotification.js
+++ webapply/src/components/Modals/MobileNotification/MobileNotification.js
@@ -2,8 +2,10 @@ import React from "react";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
+
 import { ContainedButton } from "../../Buttons/ContainedButton";
-import desktopImg from "../../../assets/images/desctop.svg";
+
+import { ReactComponent as DesktopImg } from "../../../assets/images/desktop.svg";
 
 import { useStyles } from "./styled";
 
@@ -42,7 +44,7 @@ export const MobileNotification = () => {
         <Fade in={open}>
           <div className={classes.paper}>
             <h2>Almost there!</h2>
-            <img src={desktopImg} alt="interrogation" />
+            <DesktopImg alt="interrogation" />
             <p>
               Applying for your account is going to take a bit. Better to do it when you’re not on
               the go. So get comfy and use your desktop. See you there!
