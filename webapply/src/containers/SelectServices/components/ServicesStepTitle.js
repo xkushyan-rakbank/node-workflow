c43594ce5b63e8f9106c240ba1ac5b5e0366733d
VjacheslavZ <vjacheslav.zemlyaniy@designit.com>
2019-12-10 11:44:04 +0200

    refactor ServicesStepTitle change import

--- webapply/src/containers/SelectServices/components/ServicesStepTitle.js
+++ webapply/src/containers/SelectServices/components/ServicesStepTitle.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
-import { Icon } from "../../../components/Icons/Icon";
+import { Icon } from "../../../components/Icons";
 import { LinkButton } from "../../../components/Buttons/LinkButton";
 
 const useStyles = makeStyles(theme => ({
