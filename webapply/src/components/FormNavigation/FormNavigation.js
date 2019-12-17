7503daed760146d1a1699e2752fb0e6ae0627aac
VjacheslavZ <vjacheslav.zemlyaniy@designit.com>
2019-12-17 13:00:21 +0200

    move out AccountInfo in to separate file, refactor FormNavigation component

--- webapply/src/components/FormNavigation/FormNavigation.js
+++ webapply/src/components/FormNavigation/FormNavigation.js
@@ -1,123 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import cx from "classnames";
 import Typography from "@material-ui/core/Typography";
 
 import FormNavigationStep from "../FormNavigationStep";
 import Chat from "../Chat";
-import { ContainedButton } from "../Buttons/ContainedButton";
-import { MobileNotification } from "../Modals/index";
 import { IslamicBankingSwitcherMobile } from "../IslamicBankingSwitcher/IslamicBankingSwitcherMobile";
-
+import { AccountInfo } from "./AccountInfo";
 import routes from "../../routes";
 import { accountsNames, formStepper, searchProspectStepper } from "../../constants";
-import { accountsInfo } from "./constants";
 import { checkIsShowAccountInfo, checkIsShowSmallBg } from "./utils";
 
 import { useStyles } from "./styled";
-
-const AccountInfo = ({ accountType }) => {
-  const classes = useStyles();
-  const history = useHistory();
-  const { location: { pathname } = {} } = history;
-
-  const handleClick = path => () => history.push(path);
-  const isApplicationOverview = pathname === routes.applicationOverview;
-
-  return (
-    <div className={classes.contentContainer}>
-      {accountType && !isApplicationOverview ? (
-        <>
-          <div>
-            <Typography variant="h2" component="h2" classes={{ root: classes.sectionTitle }}>
-              {accountsInfo[accountType].title}
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              component="span"
-              classes={{ root: classes.sectionSubtitle }}
-            >
-              {accountsInfo[accountType].subtitle}
-            </Typography>
-          </div>
-          <ContainedButton
-            withRightArrow
-            justify="flex-start"
-            label="Apply now"
-            handleClick={handleClick(routes.applicationOverview)}
-          />
-        </>
-      ) : (
-        <>
-          <Typography variant="h2" component="h2" classes={{ root: classes.sectionTitle }}>
-            {(() => {
-              switch (pathname) {
-                case routes.applicationOverview:
-                  return "Opening an account has never been this simple.";
-                case routes.MyApplications:
-                  return "Your  applications, at a glance";
-                case routes.comeBackLogin:
-                  return "Good to see you back!";
-                case routes.comeBackLoginVerification:
-                  return "Confirm that it's you";
-                default:
-                  return "All businesses start with an account. Get yours now.";
-              }
-            })()}
-          </Typography>
-          {isApplicationOverview && (
-            <>
-              <div className="hide-on-mobile">
-                <ContainedButton
-                  withRightArrow
-                  justify="flex-start"
-                  label="Start application"
-                  handleClick={handleClick(routes.applicantInfo)}
-                />
-              </div>
-              <div className="show-on-mobile">
-                <MobileNotification />
-              </div>
-            </>
-          )}
-        </>
-      )}
-    </div>
-  );
-};
-
-const FormStepper = ({ step, path, isLogin }) => (
-  <ul>
-    {(isLogin ? searchProspectStepper : formStepper).map(currentStep => (
-      <FormNavigationStep
-        key={currentStep.step}
-        title={currentStep.title}
-        activeStep={path === currentStep.path || path === currentStep.relatedPath}
-        filled={step > currentStep.step}
-      />
-    ))}
-  </ul>
-);
 
 export const FormNavigationComponent = ({
   applicationInfo: { islamicBanking, accountType },
   isLogin
 }) => {
   const {
-    location: { pathname, key }
+    location: { pathname }
   } = useHistory();
 
   const getRouteConfig = () =>
     formStepper.find(step => [step.path, step.relatedPath].some(path => pathname === path));
 
   const [isSwitcherShow, setIsSwitcherShow] = useState(false);
-  const [step, setStep] = useState((getRouteConfig() || {}).step || 1);
-
-  useEffect(() => {
-    const { step = 1 } = getRouteConfig() || {};
-    setStep(step);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [key, step, pathname]);
 
   const classes = useStyles();
   const bgTypeClass = cx({ brown: accountType === accountsNames.elite, green: islamicBanking });
@@ -143,7 +50,18 @@ export const FormNavigationComponent = ({
       {checkIsShowAccountInfo(pathname) ? (
         <AccountInfo classes={classes} accountType={accountType} />
       ) : (
-        pathname !== routes.login && <FormStepper step={step} path={pathname} isLogin={isLogin} />
+        pathname !== routes.login && (
+          <ul>
+            {(isLogin ? searchProspectStepper : formStepper).map(currentStep => (
+              <FormNavigationStep
+                key={currentStep.step}
+                title={currentStep.title}
+                activeStep={pathname === currentStep.path || pathname === currentStep.relatedPath}
+                filled={(getRouteConfig() || {}).step > currentStep.step}
+              />
+            ))}
+          </ul>
+        )
       )}
       {!(isLogin || pathname === routes.login) && <Chat />}
     </div>
