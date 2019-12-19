7a17d39d1bae9e872f6d825085fecd217bb6fff4
VjacheslavZ <vjacheslav.zemlyaniy@designit.com>
2019-12-19 14:06:19 +0200

    add user name in title op page CompanyInfo

--- webapply/src/containers/CompanyInfo/styled.js
+++ webapply/src/containers/CompanyInfo/styled.js
@@ -1,9 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export const useStyles = makeStyles({
-  sectionTitleIndent: {
-    marginBottom: "24px"
-  },
+export const useStyles = makeStyles(theme => ({
   topIndent: {
     marginTop: "40px"
   },
@@ -12,5 +9,14 @@ export const useStyles = makeStyles({
     fontSize: "20px",
     fontWeight: 600,
     color: "#373737"
+  },
+  username: {
+    fontSize: "20px",
+    lineHeight: "1.3",
+    margin: "0",
+    color: theme.palette.text.color
+  },
+  sectionTitleIndent: {
+    marginBottom: "24px"
   }
-});
+}));
