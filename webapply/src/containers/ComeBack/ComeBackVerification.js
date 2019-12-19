72888f0808a52c03e057c61f4240bc79dfcb3d46
VjacheslavZ <vjacheslav.zemlyaniy@designit.com>
2019-12-19 13:18:47 +0200

    add BackLink in OTPform component

--- webapply/src/containers/ComeBack/ComeBackVerification.js
+++ webapply/src/containers/ComeBack/ComeBackVerification.js
@@ -3,4 +3,6 @@ import React from "react";
 import routes from "../../routes";
 import { OTPform } from "../../components/OTPform";
 
-export const ComeBackVerification = () => <OTPform redirectRoute={routes.MyApplications} />;
+export const ComeBackVerification = () => (
+  <OTPform redirectRoute={routes.MyApplications} goBackRoute={routes.verifyOtp} />
+);
