dc7c4108c90e7516da7da3f72b539ad942d869ca
jarick <yaroslav.zolotarev@designit.com>
2019-12-23 17:05:29 +0200

    fix: refactoring `getIsVirtualCurrency` selector

--- webapply/src/store/selectors/companyInfo.js
+++ webapply/src/store/selectors/companyInfo.js
@@ -1,9 +1,9 @@
 import get from "lodash/get";
+
 import { getOrgKYCDetails } from "./appConfig";
 
 export const getIsVirtualCurrency = state => {
-  const getIndustryMultiSelect = getOrgKYCDetails(state).industryMultiSelect || [];
-  const industry = get(getIndustryMultiSelect, "[0].industry", []);
+  const { getIndustryMultiSelect } = getOrgKYCDetails(state);
 
-  return getIndustryMultiSelect.length > 0 && industry.includes("Virtual currency");
+  return get(getIndustryMultiSelect, "[0].industry", []).includes("Virtual currency");
 };
