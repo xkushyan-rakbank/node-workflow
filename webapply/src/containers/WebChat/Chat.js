05e583f46e2b583b8c096fd5fadd83694a75d8d3
jarick <yaroslav.zolotarev@designit.com>
2019-12-25 16:26:58 +0200

    fix: change `Chat` component

--- webapply/src/containers/WebChat/Chat.js
+++ webapply/src/containers/WebChat/Chat.js
@@ -1,56 +1,19 @@
-import React, { Suspense, lazy, useCallback, useReducer } from "react";
+import React, { Suspense, lazy, useCallback } from "react";
 import { connect } from "react-redux";
 import cx from "classnames";
 
+import { useWebChatState } from "./hooks/useWebChatState";
 import { getApplicantInfo } from "../../store/selectors/appConfig";
+
 import chatIcon from "./../../assets/webchat/black.svg";
+
 import { useStyles } from "./styled";
 
 const WebChatComponent = lazy(() => import("./componets/WebChat"));
 
-const initialState = {
-  isOpened: false,
-  isClosed: true,
-  isMinimized: false,
-  isExpanded: false
-};
-
-const webChatReducer = (state, { type }) => {
-  switch (type) {
-    case "open":
-      return {
-        ...state,
-        isOpened: true,
-        isClosed: false,
-        isMinimized: false
-      };
-    case "close":
-      return {
-        ...state,
-        isOpened: false,
-        isClosed: true
-      };
-    case "minimize":
-      return {
-        ...state,
-        isOpened: true,
-        isMinimized: true,
-        isClosed: false,
-        isExpanded: false
-      };
-    case "expand":
-      return { ...state, isExpanded: true, isMinimized: false };
-    default:
-      return state;
-  }
-};
-
 const ChatComponent = ({ name, mobileNo, countryCode, email }) => {
   const classes = useStyles();
-  const [state, dispatch] = useReducer(webChatReducer, initialState);
-  const { isOpened, isClosed, isMinimized } = state;
-  const toogleChat = isMinimized ? classes.mimimized : classes.expand;
-  const mobileNumber = `${countryCode + mobileNo}`;
+  const [{ isOpened, isClosed, isMinimized }, dispatch] = useWebChatState();
 
   const openChat = useCallback(() => {
     dispatch({ type: isClosed ? "open" : "expand" });
@@ -58,41 +21,44 @@ const ChatComponent = ({ name, mobileNo, countryCode, email }) => {
   const closeWebChat = useCallback(() => dispatch({ type: "close" }), [dispatch]);
   const minimizeChat = useCallback(() => dispatch({ type: "minimize" }), [dispatch]);
 
-  return (
-    <>
-      {(isClosed || isMinimized) && (
-        <div className={classes.chat}>
-          <div className={classes.chatInner} onClick={openChat}>
-            <div>
-              <span>
-                {isMinimized && (
-                  <div className={classes.messagesCount}>
-                    <p>0</p>
-                  </div>
-                )}
-                <img src={chatIcon} alt="chat" />
-              </span>
-            </div>
-            <div className="hide-on-mobile"> Chat with Us</div>
+  return [
+    (isClosed || isMinimized) && (
+      <div key="link" className={classes.chat}>
+        <div className={classes.chatInner} onClick={openChat}>
+          <div>
+            <span>
+              {isMinimized && (
+                <div className={classes.messagesCount}>
+                  <p>0</p>
+                </div>
+              )}
+              <img src={chatIcon} alt="chat" />
+            </span>
           </div>
+          <div className="hide-on-mobile"> Chat with Us</div>
         </div>
-      )}
-
-      {isOpened && (
-        <div className={cx(classes.chatWrapper, toogleChat)}>
-          <Suspense fallback={<div>Loading...</div>}>
-            <WebChatComponent
-              onClose={closeWebChat}
-              onMinimize={minimizeChat}
-              name={name}
-              mobileNumber={mobileNumber}
-              email={email}
-            />
-          </Suspense>
-        </div>
-      )}
-    </>
-  );
+      </div>
+    ),
+    isOpened && (
+      <div
+        key="window"
+        className={cx(classes.chatWrapper, {
+          [classes.mimimized]: isMinimized,
+          [classes.expand]: !isMinimized
+        })}
+      >
+        <Suspense fallback={<div>Loading...</div>}>
+          <WebChatComponent
+            onClose={closeWebChat}
+            onMinimize={minimizeChat}
+            name={name}
+            mobileNumber={`${countryCode}${mobileNo}`}
+            email={email}
+          />
+        </Suspense>
+      </div>
+    )
+  ];
 };
 
 const mapStateToProps = state => ({
