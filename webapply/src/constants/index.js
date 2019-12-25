9f1fc99e96ff8afe514fafde22fe08083dbe49cc
jarick <yaroslav.zolotarev@designit.com>
2019-12-25 15:58:26 +0200

    fix: remove unused functions from `constants/index`

--- webapply/src/constants/index.js
+++ webapply/src/constants/index.js
@@ -1,67 +1,9 @@
-import isUndefined from "lodash/isUndefined";
-import isArray from "lodash/isArray";
 import routes from "../routes";
 
 export const authorityType = [
   { value: "AuthorityType1", label: "AuthorityType1" },
   { value: "AuthorityType2", label: "AuthorityType2" }
 ];
-
-export const defineDynamicInputId = (id, indexes) => {
-  if (isArray(indexes) && indexes.length > 0) {
-    return `${id}_${indexes.join("_")}`;
-  }
-  return id;
-};
-
-export const defineInputFormatByConfig = fieldConfig => {
-  switch (fieldConfig.format) {
-    case "amount":
-      return "number";
-    case "email":
-      return "email";
-    case "date":
-      return "text";
-    default:
-      return "text";
-  }
-};
-
-export const defineInputMaxLengthByConfig = fieldConfig => {
-  const type = defineInputFormatByConfig(fieldConfig);
-  if (type !== "text" || isUndefined(fieldConfig.max)) {
-    return fieldConfig.maxlength;
-  }
-  return fieldConfig.max;
-};
-
-export const defineInputMinLengthByConfig = fieldConfig => {
-  const type = defineInputFormatByConfig(fieldConfig);
-  if (type !== "text" || isUndefined(fieldConfig.min)) {
-    return fieldConfig.minlength;
-  }
-  return fieldConfig.min;
-};
-
-export const fieldAttr = (id, fieldConfig, indexes) => {
-  return {
-    id: defineDynamicInputId(id, indexes),
-    type: defineInputFormatByConfig(fieldConfig),
-    min: fieldConfig.min,
-    max: fieldConfig.max,
-    maxLength: defineInputMaxLengthByConfig(fieldConfig),
-    minLength: defineInputMinLengthByConfig(fieldConfig),
-    pattern: fieldConfig.pattern,
-    required: !!fieldConfig.required,
-    readOnly: !!fieldConfig.readOnly
-  };
-};
-
-export const errorType = {
-  required: "required",
-  invalid: "invalid",
-  multiline: "multiline"
-};
 
 export const formStepper = [
   {
