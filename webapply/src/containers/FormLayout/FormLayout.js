0048bf806527c7b0bfd1750d4541a39f445eb65c
Ivan Shinkarenko <ivan.shinkarenko@designit.com>
2020-01-11 11:42:52 +0200

    fix PR issue: part 2

--- webapply/src/containers/FormLayout/FormLayout.js
+++ webapply/src/containers/FormLayout/FormLayout.js
@@ -33,7 +33,7 @@ export const FormLayoutComponent = ({
 
               <Notifications />
 
-              {screeningError ? <ApplicationStatus {...screeningError} /> : children}
+              {screeningError.error ? <ApplicationStatus {...screeningError} /> : children}
             </div>
           </div>
         </div>
--- webapply/src/store/reducers/sendProspectToAPI.js
+++ webapply/src/store/reducers/sendProspectToAPI.js
@@ -11,7 +11,7 @@ import {
 export const initialState = {
   loading: false,
   resetStep: false,
-  screeningError: null,
+  screeningError: {},
   prospectCopy: {},
   screeningResults: {}
 };
