f445676deec6d767715df3425ffe764a58a25165
jarick <yaroslav.zolotarev@designit.com>
2020-01-21 16:13:35 +0200

    fix: `checkIsAccountInfoTypeNumber` function

--- webapply/src/containers/FinalQuestions/components/SignatorySummaryCard/utils.js
+++ webapply/src/containers/FinalQuestions/components/SignatorySummaryCard/utils.js
@@ -1,12 +1,11 @@
 export const checkIsAccountInfoTypeNumber = (authorityTypeValue, uiConfig) => {
-  const isAuthorityTypeNaN = isNaN(Number(authorityTypeValue));
-  if (!isAuthorityTypeNaN) {
-    const fieldConfig = Object.values(uiConfig).find(item => item.datalistId === "authorityType");
-    const authorityTypeFromDatalist = (fieldConfig.datalist || []).find(
-      item => item.value === authorityTypeValue
-    );
-
-    return authorityTypeFromDatalist ? authorityTypeFromDatalist.displayText || "" : "";
+  if (isNaN(Number(authorityTypeValue))) {
+    return authorityTypeValue;
   }
-  return authorityTypeValue;
+
+  const fieldConfig = Object.values(uiConfig).find(item => item.datalistId === "authorityType");
+  const authorityTypeFromDatalist =
+    (fieldConfig.datalist || []).find(item => item.value === authorityTypeValue) || {};
+
+  return authorityTypeFromDatalist.displayText || "";
 };
