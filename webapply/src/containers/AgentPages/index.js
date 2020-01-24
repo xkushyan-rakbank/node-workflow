3e71f15f2a848c08ab3320364244825c2c040740
jarick <yaroslav.zolotarev@designit.com>
2020-01-24 22:26:32 +0200

    fix: try fix agent authenticate

--- webapply/src/containers/AgentPages/index.js
+++ webapply/src/containers/AgentPages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 import routes from "../../routes";
 import { AgentProtectedRoute } from "../../components/Routers";
@@ -8,11 +8,13 @@ import Login from "../../containers/AgentPages/Login";
 import Admin from "../../containers/AgentPages/SearchProspect";
 import SearchedAppInfo from "../../containers/AgentPages/SearchedAppInfo";
 
-const AgentPages = () => [
-  <Route key="login" exact path={routes.login} component={Login} />,
-  <AgentProtectedRoute key="search" exact path={routes.searchProspect} component={Admin} />,
-  <AgentProtectedRoute key="info" path={routes.SearchedAppInfo} component={SearchedAppInfo} />,
-  <Redirect key="redirect" to={routes.searchProspect} />
-];
+const AgentPages = () => (
+  <Switch>
+    <Route key="login" exact path={routes.login} component={Login} />,
+    <AgentProtectedRoute key="search" exact path={routes.searchProspect} component={Admin} />,
+    <AgentProtectedRoute key="info" path={routes.SearchedAppInfo} component={SearchedAppInfo} />,
+    <Redirect key="redirect" to={routes.searchProspect} />
+  </Switch>
+);
 
 export default AgentPages;
