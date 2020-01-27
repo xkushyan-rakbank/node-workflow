ef14925100a4a1b9a9e8718121b5929cf9da54c0
Vladislav.Kirilenok <vladislav.kirilenok@youpal.se>
2020-01-27 13:48:44 +0300

    removed redundant change

--- webapply/src/containers/FinalQuestions/components/FinalQuestionStepComponent/FinalQuestionStepComponent.js
+++ webapply/src/containers/FinalQuestions/components/FinalQuestionStepComponent/FinalQuestionStepComponent.js
@@ -34,7 +34,7 @@ export const FinalQuestionStepComponent = ({
       step={item.step}
       title={item.title}
       infoTitle={item.infoTitle}
-      isActiveStep={5 === item.step}
+      isActiveStep={step === item.step}
       isFilled={availableSteps.includes(item.step)}
       handleClick={createSetStepHandler(item.step)}
       handleContinue={handleContinue}
