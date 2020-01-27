6142fe34d4b4e94588d9380f9adf90ac8c4a7643
Dmitriy Soldatov <dmitrsold@gmail.com>
2020-01-27 10:23:45 +0400

    set radiobtn values by default from initial values

--- webapply/src/containers/SelectServices/components/Channels/Channels.js
+++ webapply/src/containers/SelectServices/components/Channels/Channels.js
@@ -140,6 +140,7 @@ export const ChannelsComponent = ({
             path="prospect.accountInfo[0].eStatements"
             label="I want online bank statements"
             classes={{ formControlRoot: classes.eStatementsFormControl }}
+            isLoadDefaultValueFromStore={false}
             onChange={() => {
               setFieldValue("mailStatements", false);
               setFieldValue("eStatements", true);
@@ -152,6 +153,7 @@ export const ChannelsComponent = ({
             path="prospect.accountInfo[0].mailStatements"
             label="I want paper statements (monthly charges apply)"
             classes={{ formControlRoot: classes.mailStatementsFormControl }}
+            isLoadDefaultValueFromStore={false}
             onChange={() => {
               setFieldValue("eStatements", false);
               setFieldValue("mailStatements", true);
