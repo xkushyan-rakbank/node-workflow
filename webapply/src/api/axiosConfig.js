de6d5e87f1c32f24972ad9c52adb677270431451
vadim.chernikov <vadim.chernikov@designit.com>
2020-01-29 13:21:20 +0400

    Fix server validation error notification

--- webapply/src/api/axiosConfig.js
+++ webapply/src/api/axiosConfig.js
@@ -102,14 +102,15 @@ instance.interceptors.response.use(
 
     if (status === 400 && jsonData.errorType === "ReCaptchaError") {
       store.dispatch(setError(data.errors));
-    } else if ((status === 400 || status === 500) && jsonData.errorType === "FieldsValidation") {
-      NotificationsManager.add &&
-        NotificationsManager.add({
-          title: "Validation Error",
-          message: jsonData.errors[0] ? jsonData.errors[0].message : "Validation Error"
-        });
     } else if (status === 400 && jsonData.errors) {
       store.dispatch(setInputsErrors(data.errors));
+      if (jsonData.errorType === "FieldsValidation") {
+        NotificationsManager.add &&
+          NotificationsManager.add({
+            title: "Validation Error On Server",
+            message: jsonData.errors[0] ? jsonData.errors[0].message : "Validation Error"
+          });
+      }
     } else {
       log(jsonData);
       NotificationsManager.add && NotificationsManager.add();
