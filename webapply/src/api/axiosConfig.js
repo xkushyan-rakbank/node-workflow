e64ef988faccc6330569a5786700c159c595e70b
vadim.chernikov <vadim.chernikov@designit.com>
2020-01-29 12:45:53 +0400

    Fix error mapping notification

--- webapply/src/api/axiosConfig.js
+++ webapply/src/api/axiosConfig.js
@@ -102,14 +102,14 @@ instance.interceptors.response.use(
 
     if (status === 400 && jsonData.errorType === "ReCaptchaError") {
       store.dispatch(setError(data.errors));
-    } else if (status === 400 && jsonData.errors) {
-      store.dispatch(setInputsErrors(data.errors));
     } else if ((status === 400 || status === 500) && jsonData.errorType === "FieldsValidation") {
       NotificationsManager.add &&
         NotificationsManager.add({
           title: "Validation Error",
           message: jsonData.errors[0] ? jsonData.errors[0].message : "Validation Error"
         });
+    } else if (status === 400 && jsonData.errors) {
+      store.dispatch(setInputsErrors(data.errors));
     } else {
       log(jsonData);
       NotificationsManager.add && NotificationsManager.add();
