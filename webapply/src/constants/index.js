b4985a07e36e3b82a2bc24afc6b5a77ee030db10
Anna Korzhavykh <anna.korzhavykh@designit.com>
2020-01-31 11:06:11 +0200

    Remove unused constant

--- webapply/src/constants/index.js
+++ webapply/src/constants/index.js
@@ -62,8 +62,6 @@ export const MAX_STAKEHOLDERS_LENGTH = 12;
 export const MAX_SHAREHOLDERS_LENGTH = 4;
 export const MAX_SIGNATORIES_LENGTH = 8;
 
-export const POA = "2";
-
 export const REQUEST_LOADING = "loading";
 export const REQUEST_SUCCESS = "success";
 export const REQUEST_FAILED = "error";
