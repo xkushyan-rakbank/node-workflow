f2d87a7d26dffe841dfa0ebbeefcaadbbaa0f9bd
VjacheslavZ <vjacheslav.zemlyaniy@designit.com>
2020-02-03 12:45:44 +0400

    fix show 'undefined' text in file name on UploadDocuments page

--- webapply/src/containers/UploadDocuments/components/UploadDocument.js
+++ webapply/src/containers/UploadDocuments/components/UploadDocument.js
@@ -115,7 +115,7 @@ export const UploadDocuments = ({
           {isUploading
             ? `Uploading ${document.documentTitle}`
             : isUploaded
-            ? `${selectedFile.name} ${selectedFile.fileSize}`
+            ? `${selectedFile.name}`
             : document.documentTitle}
 
           {selectedFile && (
