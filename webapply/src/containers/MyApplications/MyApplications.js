27dc6fe55ba03b22d4fd42b1457f1aca2ccf9aae
vadim.chernikov <vadim.chernikov@designit.com>
2020-02-06 11:36:48 +0400

    Come back login fix

--- webapply/src/containers/MyApplications/MyApplications.js
+++ webapply/src/containers/MyApplications/MyApplications.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { searchApplications } from "../../store/actions/searchProspect";
 import { getProspectInfo } from "./../../store/actions/retrieveApplicantInfo";
 import { getApplicantInfo } from "../../store/selectors/appConfig";
+import { displayScreenBasedOnViewId } from "../../store/actions/appConfig";
 import { getSearchResult } from "./../../store/selectors/searchProspect";
 
 import { MyApplications as BaseComponent } from "./components/MyApplications";
@@ -21,6 +22,7 @@ export const MyApplications = () => {
     <BaseComponent
       searchResults={searchResults}
       getProspectInfo={prospectId => dispatch(getProspectInfo(prospectId))}
+      displayScreenBasedOnViewId={() => displayScreenBasedOnViewId()}
     />
   );
 };
