e11b1d143c3a32efb8ec9d780e139b3516b4ee61
Dmitry L <pro@a1425s-MacBook-Pro.local>
2020-02-06 16:21:40 +0200

    added max length for years In Business fild

--- webapply/src/containers/CompanyInfo/components/LicenseInformation.js
+++ webapply/src/containers/CompanyInfo/components/LicenseInformation.js
@@ -13,7 +13,7 @@ import {
 } from "../../../components/Form";
 import { ContinueButton } from "../../../components/Buttons/ContinueButton";
 import { InfoTitle } from "../../../components/Notifications";
-import { MAX_LICENSE_NUMBER_LENGTH } from "../constants";
+import { MAX_LICENSE_NUMBER_LENGTH, MAX_YEARS_IN_BUSINESS_LENGTH } from "../constants";
 import { UAE, DATE_FORMAT } from "../../../constants";
 import { getRequiredMessage, getInvalidMessage } from "../../../utils/getValidationMessage";
 import { useStyles } from "../styled";
@@ -36,11 +36,7 @@ const licenseInformationSchema = Yup.object({
   countryOfIncorporation: Yup.string().required(getRequiredMessage("Country of incorporation")),
   licenseIssuingAuthority: Yup.string().required(getRequiredMessage("License issuing authority")),
   dateOfIncorporation: Yup.date().required(getRequiredMessage("Date of incorporation")),
-  yearsInBusiness: Yup.number()
-    .typeError("Not valid number")
-    .min(0, "Must be more than 0")
-    .max(999, "Must be less than 1000")
-    .integer(getInvalidMessage("Years in business"))
+  yearsInBusiness: Yup.number().integer(getInvalidMessage("Years in business"))
 });
 
 const changeDateProspectHandler = (_, value, path) =>
@@ -141,7 +137,12 @@ export const LicenseInformation = ({ handleContinue }) => {
                 component={Input}
                 InputProps={{
                   inputComponent: NumberFormat,
-                  inputProps: { tabIndex: 0, allowNegative: false, decimalScale: 0 }
+                  inputProps: {
+                    maxLength: MAX_YEARS_IN_BUSINESS_LENGTH,
+                    tabIndex: 0,
+                    allowNegative: false,
+                    decimalScale: 0
+                  }
                 }}
               />
             </Grid>
