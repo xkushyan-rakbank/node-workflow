9addaeb5e4d53879404ef10f125bfaa1d4d95302
vadim.chernikov <vadim.chernikov@designit.com>
2020-02-09 18:10:06 +0400

    Hotfix comeback stakeholders edit

--- webapply/src/store/sagas/retrieveApplicantInfo.js
+++ webapply/src/store/sagas/retrieveApplicantInfo.js
@@ -1,9 +1,11 @@
 import { all, call, put, takeLatest, select } from "redux-saga/effects";
+import uniqueId from "lodash/uniqueId";
 import * as actions from "../actions/retrieveApplicantInfo";
 import { displayScreenBasedOnViewId, setConfig } from "../actions/appConfig";
 import { retrieveApplicantInfos, prospect } from "../../api/apiClient";
 import { log } from "../../utils/loggger";
-import { getAuthorizationHeader } from "./../selectors/appConfig";
+import { getAuthorizationHeader } from "../selectors/appConfig";
+import { updateStakeholdersIds } from "../actions/stakeholders";
 
 function* retrieveApplicantInfoSaga({ payload }) {
   try {
@@ -32,6 +34,13 @@ function* getProspectIdInfo({ payload }) {
     const config = { prospect: response.data };
 
     yield put(setConfig(config));
+    const stakeholdersIds = config.prospect.signatoryInfo.map(info => ({
+      id: uniqueId(),
+      done: false,
+      isEditting: false
+    }));
+
+    yield put(updateStakeholdersIds(stakeholdersIds));
     if (payload.isUpdateView) {
       yield put(displayScreenBasedOnViewId());
     }
