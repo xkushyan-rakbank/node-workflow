0131b3e05d3b60b96f42447eae6ce595828cc9f8
Konstantin <konstantin.zhirkov@designit.com>
2020-02-11 17:22:44 +0300

    Fix issues

--- webapply/src/utils/useDisplayScreenBasedOnViewId.js
+++ webapply/src/utils/useDisplayScreenBasedOnViewId.js
@@ -23,7 +23,7 @@ export const useDisplayScreenBasedOnViewId = () => {
     const isApplicationSubmitted =
       viewId === VIEW_IDS.SubmitApplication && viewId !== VIEW_IDS.SearchProspect;
     const pathTo = isApplicationSubmitted ? VIEW_IDS.CompanyInfo : viewId;
-    const isEditRedirect = viewId.includes("SearchedAppInfo");
+    const isEditRedirect = viewId.includes(VIEW_IDS.SearchedAppInfo);
 
     if (!isROScreens) {
       if (isSubmit && isRetrieveMode) {
@@ -36,8 +36,9 @@ export const useDisplayScreenBasedOnViewId = () => {
     } else {
       if (isEditRedirect) {
         history.push(routes.companyInfo);
+      } else {
+        history.push(`${prefix}${pathTo}`);
       }
-      history.push(`${prefix}${pathTo}`);
     }
   }, [applicationInfo, isROScreens]);
 
