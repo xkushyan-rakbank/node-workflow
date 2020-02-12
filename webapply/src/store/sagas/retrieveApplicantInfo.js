d6df22307bbb6a37dc00c4b2d055a9aee51e4ddd
Orkhan Jafarov <orkhanjafarovr@gmail.com>
2020-02-12 13:42:50 +0400

    minor changes

--- webapply/src/store/sagas/retrieveApplicantInfo.js
+++ webapply/src/store/sagas/retrieveApplicantInfo.js
@@ -2,7 +2,7 @@ import { all, call, put, takeLatest, select } from "redux-saga/effects";
 import uniqueId from "lodash/uniqueId";
 import * as actions from "../actions/retrieveApplicantInfo";
 import { setConfig } from "../actions/appConfig";
-import { retrieveApplicantInfos, prospect } from "../../api/apiClient";
+import { retrieveApplicantInfos, prospect as prospectApi } from "../../api/apiClient";
 import { log } from "../../utils/loggger";
 import { getAuthorizationHeader, getProspect } from "../selectors/appConfig";
 import { updateStakeholdersIds } from "../actions/stakeholders";
@@ -30,11 +30,11 @@ function* getProspectIdInfo({ payload }) {
   try {
     const state = yield select();
     const headers = getAuthorizationHeader(state);
-    const response = yield call(prospect.get, payload.prospectId, headers);
+    const response = yield call(prospectApi.get, payload.prospectId, headers);
     const config = { prospect: response.data };
 
     yield put(setConfig(config));
-    const prospectConfig = yield select(getProspect);
+    const prospect = yield select(getProspect);
 
     const stakeholdersIds = config.prospect.signatoryInfo.map(info => ({
       id: uniqueId(),
@@ -43,7 +43,7 @@ function* getProspectIdInfo({ payload }) {
     }));
 
     yield put(updateStakeholdersIds(stakeholdersIds));
-    yield put(actions.getProspectInfoSuccess(prospectConfig));
+    yield put(actions.getProspectInfoSuccess(prospect));
   } catch (error) {
     log(error);
     yield put(actions.getProspectInfoFail());
