a352ab4ae3fb7e7b951a8740e87836a6bee0d899
Vladislav.Kirilenok <vladislav.kirilenok@youpal.se>
2020-02-14 12:13:06 +0300

    initial viewId set

--- webapply/src/store/sagas/appConfig.js
+++ webapply/src/store/sagas/appConfig.js
@@ -30,6 +30,7 @@ import {
   ISLAMIC_BANK
 } from "../../constants";
 import { getEndpoints, getIsIslamicBanking, getAccountType } from "../selectors/appConfig";
+import routes, { smeBaseName } from "../../routes";
 
 function* receiveAppConfigSaga({ payload }) {
   try {
@@ -55,6 +56,7 @@ function* receiveAppConfigSaga({ payload }) {
     const newConfig = cloneDeep(response.data);
     const prospectModel = cloneDeep(newConfig.prospect);
     if (newConfig.prospect) {
+      newConfig.prospect.applicationInfo.viewId = routes.companyInfo.replace(smeBaseName, "");
       newConfig.prospect.signatoryInfo = [];
       newConfig.prospect.accountInfo[0].accountCurrency = UAE_CURRENCY;
       if (!newConfig.prospect.applicantInfo.countryCode) {
