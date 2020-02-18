9a51c967a589670f0cf769286f8bac051e9bc4ec
vsaakian <vladimir.saakyan@designit.com>
2020-02-18 17:03:55 +0200

    change condition

--- webapply/src/containers/CompanyStakeholders/CompanyStakeholders.js
+++ webapply/src/containers/CompanyStakeholders/CompanyStakeholders.js
@@ -183,12 +183,12 @@ const CompanyStakeholdersComponent = ({
       {stakeholders.length && (stakeholdersIds[0].done && !hasSignatories) && (
         <ErrorMessage error="At least one signatory is required. Edit Signatory rights or Add new stakeholder." />
       )}
-      {stakeholders.length && (stakeholdersIds[0].done && isLowPercentage) ? (
+      {stakeholders.length && (stakeholdersIds[0].done && isLowPercentage) && (
         <ErrorMessage
           error={`Shareholders ${percentage}% is less than 100%, either add a new stakeholder
           or edit the shareholding % for the added stakeholders.`}
         />
-      ) : null}
+      )}
       <div className="linkContainer">
         <BackLink path={routes.companyInfo} />
 
