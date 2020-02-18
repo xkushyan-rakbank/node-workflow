6de3329b8ce021495dc3a8cd5da95832f1572f84
Dmitriy Soldatov <dmitrsold@gmail.com>
2020-02-18 12:13:58 +0200

    Revert multiline

--- webapply/src/containers/FinalQuestions/components/CompanySummaryCard/CompanySummarySteps/CompanyBusinessRelationships/CompanyBusinessRelationships.js
+++ webapply/src/containers/FinalQuestions/components/CompanySummaryCard/CompanySummarySteps/CompanyBusinessRelationships/CompanyBusinessRelationships.js
@@ -154,7 +154,6 @@ export const CompanyBusinessRelationshipsComponent = ({
                                 </>
                               }
                               placeholder="Customer name"
-                              multiline
                               component={Input}
                               InputProps={{
                                 inputProps: { maxLength: MAX_COMPANY_NAME_LENGTH, tabIndex: 0 }
@@ -255,7 +254,6 @@ export const CompanyBusinessRelationshipsComponent = ({
                                 </>
                               }
                               component={Input}
-                              multiline
                               disabled={values.isDontHaveSuppliersYet}
                               InputProps={{
                                 inputProps: { maxLength: MAX_COMPANY_NAME_LENGTH, tabIndex: 0 }
