e1996b47a77f8cd48a27e279951e497b57c22734
vsaakian <vladimir.saakyan@designit.com>
2020-02-18 15:35:47 +0200

    change validation for ErrorMessage company stakholders

--- webapply/src/containers/CompanyStakeholders/CompanyStakeholders.js
+++ webapply/src/containers/CompanyStakeholders/CompanyStakeholders.js
@@ -180,10 +180,10 @@ const CompanyStakeholdersComponent = ({
           <AddStakeholderButton handleClick={addNewStakeholder} />
         </div>
       )}
-      {stakeholders.length > 0 && !hasSignatories && (
+      {stakeholders.length >= 1 && stakeholdersIds[0].done && !hasSignatories && (
         <ErrorMessage error="At least one signatory is required. Edit Signatory rights or Add new stakeholder." />
       )}
-      {stakeholders.length && isLowPercentage ? (
+      {stakeholders.length >= 1 && stakeholdersIds[0].done && isLowPercentage ? (
         <ErrorMessage
           error={`Shareholders ${percentage}% is less than 100%, either add a new stakeholder
           or edit the shareholding % for the added stakeholders.`}
