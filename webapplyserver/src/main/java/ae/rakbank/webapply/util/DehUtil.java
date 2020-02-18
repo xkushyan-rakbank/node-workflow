820d9da7bd8422a5125f383b3e6a834af0b674c6
oleksandr-ansimov <alex.ansimov.ua@gmail.com>
2020-02-18 15:50:48 +0200

    minor changes

--- webapplyserver/src/main/java/ae/rakbank/webapply/util/DehUtil.java
+++ webapplyserver/src/main/java/ae/rakbank/webapply/util/DehUtil.java
@@ -6,30 +6,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.client.HttpStatusCodeException;
-
-import java.io.IOException;
 
 @Slf4j
 @Component
 public class DehUtil {
 
     public ApiError initApiError(HttpStatusCodeException e, HttpStatus status) {
-        return initApiError(e, status, null);
-    }
-
-    public ApiError initApiError(HttpStatusCodeException e, HttpStatus status, String errorMessage) {
-        String message;
-        if (StringUtils.isEmpty(errorMessage)) {
-            message = e.getMessage();
-        } else {
-            message = errorMessage;
-        }
         ApiError apiError = ApiError.builder()
                 .status(status)
                 .statusCode(status.value())
-                .message(message)
+                .message("Error response from DEH Server")
                 .debugMessage(e.getResponseBodyAsString())
                 .exceptionClassName(e.getClass().getSimpleName())
                 .stackTrace(e.getStackTrace())
@@ -45,8 +32,6 @@ public class DehUtil {
         ObjectMapper mapper = new ObjectMapper();
         try {
             return mapper.readTree(e.getResponseBodyAsString()).get("errors");
-//            JsonNode jsonNode = mapper.readTree(e.getResponseBodyAsString());
-//            return jsonNode.get("errors").toString();
         } catch (Exception e1) {
             log.warn("Can't parse errors from the response", e1);
             return null;
