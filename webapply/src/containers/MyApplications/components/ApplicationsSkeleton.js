90acef3df7f898c2c6160ed9e8759e794dde9753
Dmitry L <pro@a1425s-MacBook-Pro.local>
2020-02-18 18:40:46 +0200

    some fix

--- webapply/src/containers/MyApplications/components/ApplicationsSkeleton.js
+++ webapply/src/containers/MyApplications/components/ApplicationsSkeleton.js
@@ -7,7 +7,7 @@ const range = (end, start = 0, length = end - start) => Array.from({ length }, (
 
 export const ApplicationsSkeleton = ({ count = 4 }) => {
   const classes = useStyles();
-  return range(count).map((_, index) => (
+  return range(count).map(index => (
     <div className={classes.wrapper} key={index}>
       <div className={classes.applicationRow}>
         <SkeletonLoader className={classes.companyNameSkeleton} />
