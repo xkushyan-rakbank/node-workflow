7375fea8835cb23c54ea496f1721c26d9181caf2
Orkhan Jafarov <orkhanjafarovr@gmail.com>
2020-02-20 15:38:01 +0400

    fixed viewId on submit

--- webapply/src/containers/SelectServices/components/SubmitApplication/SubmitApplication.js
+++ webapply/src/containers/SelectServices/components/SubmitApplication/SubmitApplication.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import routes, { smeBaseName } from "../../../../routes";
 import { submitApplication } from "../../../../constants/index";
+import { PROSPECT_STATUSES } from "../../../AgentPages/constants";
 
 import { BackLink } from "../../../../components/Buttons/BackLink";
 import { FormTitle } from "../FormTitle";
@@ -21,24 +22,27 @@ export const SubmitApplicationComponent = ({
   updateActionType,
   updateSaveType,
   isApplyEditApplication,
-  updateViewId
+  updateViewId,
+  currentProspectStatus
 }) => {
   const pushHistory = useTrackingHistory();
   const [formFieldsValues, setFormFields] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const pathname = routes.ApplicationSubmitted;
+  const isROSubmit =
+    isApplyEditApplication && currentProspectStatus === PROSPECT_STATUSES.ASSESSING;
+  const pathname = isROSubmit ? routes.ApplicationSubmitted : routes.SubmitApplication;
+
   const isSubmitButtonEnable =
     isApplyEditApplication ||
     (formFieldsValues.isInformationProvided && formFieldsValues.areTermsAgreed);
+
   const handleSubmit = () => {
-    if (isApplyEditApplication) {
-      updateViewId(pathname.replace(smeBaseName, ""), false);
-    }
+    updateViewId(pathname.replace(smeBaseName, ""), false);
     setIsSubmitting(true);
     updateActionType(SUBMIT);
     updateSaveType(NEXT);
     sendProspectToAPI()
-      .then(() => pushHistory(pathname), () => {})
+      .then(() => pushHistory(routes.ApplicationSubmitted), () => {})
       .finally(() => setIsSubmitting(false));
   };
 
