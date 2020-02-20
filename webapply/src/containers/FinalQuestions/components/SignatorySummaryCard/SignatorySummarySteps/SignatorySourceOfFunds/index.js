99e4533d7a5f31e7ddb75f1124fdaf2661b4df1f
jarick <yaroslav.zolotarev@designit.com>
2020-02-20 18:35:50 +0200

    fix: revert changes

--- webapply/src/containers/FinalQuestions/components/SignatorySummaryCard/SignatorySummarySteps/SignatorySourceOfFunds/index.js
+++ webapply/src/containers/FinalQuestions/components/SignatorySummaryCard/SignatorySummarySteps/SignatorySourceOfFunds/index.js
@@ -1,11 +1,1 @@
-import { connect } from "react-redux";
-
-import { getSignatories } from "../../../../../../store/selectors/appConfig";
-
-import { SignatorySourceOfFundsComponent } from "./SignatorySourceOfFunds";
-
-const mapStateToProps = state => ({
-  signatories: getSignatories(state)
-});
-
-export const SignatorySourceOfFunds = connect(mapStateToProps)(SignatorySourceOfFundsComponent);
+export { SignatorySourceOfFunds } from "./SignatorySourceOfFunds";
