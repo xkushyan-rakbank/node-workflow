9e2e70eed86a4d8038fa9a988576d887d6efcff7
hlib moiseiev <hlib.moiseiev@designit.com>
2020-02-22 11:19:36 +0400

    fixed incomplete status

--- webapply/src/containers/AgentPages/constants.js
+++ webapply/src/containers/AgentPages/constants.js
@@ -1,6 +1,7 @@
 export const PROSPECT_STATUSES = {
   ASSESSING: "Assessing",
-  DOCUMENTS_NEEDED: "Documents needed"
+  DOCUMENTS_NEEDED: "Documents needed",
+  INCOMPLETE: "Incomplete"
 };
 
 export const ALLOWED_EDIT_STATUSES = [PROSPECT_STATUSES.ASSESSING];
