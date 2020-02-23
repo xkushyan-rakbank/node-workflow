8878e8d3369ae6a84f81fcecfb922514bff62edd
Orkhan Jafarov <orkhanjafarovr@gmail.com>
2020-02-23 13:40:09 +0400

    untrack files

---
+++ webapply/src/containers/AgentPages/SearchedAppInfo/SearchedAppInfo.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect, useCallback } from "react";
+import get from "lodash/get";
+
+import { FormCard } from "../../../components/FormCard/FormCard";
+import { StepComponent } from "../../../components/StepComponent/StepComponent";
+import routes from "../../../routes";
+import { SubmitButton } from "../../../components/Buttons/SubmitButton";
+import { BackLink } from "../../../components/Buttons/BackLink";
+import { ConfirmDialog } from "../../../components/Modals";
+import { searchedAppInfoSteps, CONFIRM_MESSAGE, STEP_1, STATUS_LOCKED } from "./constants";
+import { APP_STOP_SCREEN_RESULT } from "../../../constants";
+
+import { useStyles } from "./styled";
+import { useDisplayScreenBasedOnViewId } from "../../../utils/useDisplayScreenBasedOnViewId";
+
+export const SearchedAppInfoComponent = ({
+  searchResults,
+  match,
+  updateProspectId,
+  retrieveDocDetails,
+  getProspectInfo,
+  setIsApplyEditApplication,
+  isApplyEditApplication,
+  prospectInfo
+}) => {
+  const classes = useStyles();
+  const initialAvailableSteps = searchedAppInfoSteps.map(item => item.step);
+  const [step, setStep] = useState(STEP_1);
+
+  const handleSetStep = nextStep => {
+    if (initialAvailableSteps.includes(nextStep)) {
+      setStep(nextStep);
+    } else if (!nextStep) {
+      setStep(null);
+    }
+  };
+
+  const createSetStepHandler = nextStep => () => handleSetStep(nextStep);
+
+  const [isDisplayConfirmDialog, setIsDisplayConfirmDialog] = useState(false);
+
+  useEffect(() => {
+    updateProspectId(match.params.id);
+    getProspectInfo(match.params.id);
+  }, [updateProspectId, retrieveDocDetails, match.params.id, getProspectInfo]);
+
+  useEffect(() => {
+    if (isApplyEditApplication) {
+      pushDisplayScreenToHistory();
+    }
+  }, [isApplyEditApplication]);
+
+  const redirectUserPage = useCallback(() => {
+    setIsDisplayConfirmDialog(true);
+  }, [setIsDisplayConfirmDialog]);
+
+  const { pushDisplayScreenToHistory } = useDisplayScreenBasedOnViewId();
+
+  const confirmHandler = useCallback(() => {
+    setIsApplyEditApplication(true);
+  }, [setIsApplyEditApplication, pushDisplayScreenToHistory]);
+
+  const confirmDialogHandler = useCallback(() => {
+    setIsDisplayConfirmDialog(false);
+  }, [setIsDisplayConfirmDialog]);
+
+  const searchResult = (searchResults.searchResult || []).find(
+    item => item.prospectId === match.params.id
+  );
+  const isDisabled =
+    get(searchResult, "status.reasonCode") === STATUS_LOCKED ||
+    get(prospectInfo, "organizationInfo.screeningInfo.statusOverAll") === APP_STOP_SCREEN_RESULT;
+
+  const fullName = get(searchResult, "applicantInfo.fullName", "");
+  const [firstName, lastName] = fullName.split(/\s/);
+
+  return (
+    <>
+      <h2>Application Details</h2>
+      <p className="formDescription" />
+      <FormCard
+        fullName={fullName}
+        firstName={firstName}
+        lastName={lastName}
+        content={<div className={classes.title}>{fullName}</div>}
+      >
+        <div className={classes.formContent}>
+          {searchedAppInfoSteps.map(item => {
+            return (
+              <StepComponent
+                key={item.step}
+                title={item.title}
+                subTitle={item.infoTitle}
+                isActiveStep={step === item.step}
+                isFilled={true}
+                handleClick={createSetStepHandler(item.step)}
+                hideContinue={true}
+                prospectInfo={prospectInfo}
+                stepForm={item.component}
+                searchResult={searchResult}
+              />
+            );
+          })}
+        </div>
+      </FormCard>
+      <div className="linkContainer">
+        <BackLink path={routes.searchProspect} />
+        <SubmitButton
+          label="Edit"
+          justify="flex-end"
+          handleClick={redirectUserPage}
+          disabled={isDisabled}
+        />
+      </div>
+
+      <ConfirmDialog
+        isOpen={isDisplayConfirmDialog}
+        handleConfirm={confirmHandler}
+        handleReject={confirmDialogHandler}
+        handleClose={confirmDialogHandler}
+        message={CONFIRM_MESSAGE}
+      />
+    </>
+  );
+};
---
+++ webapply/src/utils/useDisplayScreenBasedOnViewId.js
@@ -0,0 +1,49 @@
+import { useCallback } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import { getApplicationInfo } from "../store/selectors/appConfig";
+import { getIsEditableStatusSearchInfo } from "../store/selectors/searchProspect";
+import routes, { smeBaseName } from "../routes";
+import { ACTION_TYPES, VIEW_IDS } from "../constants";
+
+export const useDisplayScreenBasedOnViewId = () => {
+  const history = useHistory();
+  const { applicationInfo, isROScreens } = useSelector(state => ({
+    applicationInfo: getApplicationInfo(state),
+    isROScreens: getIsEditableStatusSearchInfo(state)
+  }));
+
+  const pushDisplayScreenToHistory = useCallback(
+    prospect => {
+      const newApplicationInfo = prospect ? prospect.applicationInfo : applicationInfo;
+      const viewId = newApplicationInfo.viewId || routes.companyInfo.replace(smeBaseName, "");
+      const isSubmit = newApplicationInfo.actionType === ACTION_TYPES.submit;
+      const isRetrieveMode = newApplicationInfo.retrieveMode;
+      const isApplicationSubmitted =
+        viewId === VIEW_IDS.SubmitApplication && viewId !== VIEW_IDS.SearchProspect;
+      const isEditRedirect = viewId.includes(VIEW_IDS.SearchedAppInfo);
+
+      if (!isROScreens) {
+        if (isSubmit && isRetrieveMode) {
+          history.push(`${smeBaseName}${routes.ApplicationSubmitted}`);
+        } else if (isSubmit && !isRetrieveMode) {
+          history.push(`${smeBaseName}${newApplicationInfo.reUploadDocuments}`);
+        } else if (viewId && !isApplicationSubmitted) {
+          history.push(`${smeBaseName}${viewId}`);
+        }
+      } else {
+        if (isEditRedirect || viewId === VIEW_IDS.SubmitApplication) {
+          history.push(routes.companyInfo);
+        } else {
+          history.push(`${smeBaseName}${viewId}`);
+        }
+      }
+    },
+    [applicationInfo, isROScreens, history]
+  );
+
+  return {
+    pushDisplayScreenToHistory
+  };
+};
