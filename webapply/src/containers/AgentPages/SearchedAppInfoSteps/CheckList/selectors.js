85cea3c5bb5e5180e6570c5d834d0ef4ce60c1d9
Orkhan Jafarov <orkhanjafarovr@gmail.com>
2020-02-24 18:52:28 +0400

    fixed

---
+++ webapply/src/containers/AgentPages/SearchedAppInfoSteps/CheckList/selectors.js
@@ -0,0 +1,25 @@
+import { createSelector } from "reselect";
+import { COMPANY_CHECK_NAMES, RISK_RATING } from "./constants";
+
+import {
+  getOrganizationScreeningResults,
+  getProspectRiskScore
+} from "../../../../store/selectors/appConfig";
+
+const screeningResults = state => getOrganizationScreeningResults(state);
+const riskScore = state => getProspectRiskScore(state);
+
+export const getCompanyChecksSelector = createSelector(
+  screeningResults,
+  riskScore,
+  (screeningResults, riskScore) => [
+    ...COMPANY_CHECK_NAMES.map(check => ({
+      ...check,
+      ...(screeningResults.find(s => s.screeningType === check.screeningType) || {})
+    })),
+    {
+      ...RISK_RATING,
+      screeningReason: riskScore
+    }
+  ]
+);
