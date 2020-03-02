6c858ceaa05c325a06bb69b5ea4c9d325bc38870
Dmitriy Soldatov <dmitrsold@gmail.com>
2020-03-02 11:27:35 +0200

    labels fix

--- webapply/src/constants/index.js
+++ webapply/src/constants/index.js
@@ -47,9 +47,7 @@ export const submitApplication = {
     "https://rakbank.ae/wps/wcm/connect/3f9d99b1-d7a2-4634-82b5-08f03e734295/%28A%29%2BJ00781%2BRAK%2B%2BDebit%2BCard%2B-%2BBusiness%2BA4-T%26C-New%2BGuide-EN%26AR%28withe%2Bout%2Bc....pdf?MOD=AJPERES&CVID=lTLVCHV",
   formTitle: "Submit application",
   formInfo:
-    "And just like that, we have reached the end! Here’s the overview of what you’re applying for.",
-  trueNdCompleteAcknldgelabel: "I confirm that the information provided is true and complete",
-  needCommunicationLabel: "I want to receive marketing and promotional communication from RAKBANK"
+    "And just like that, we have reached the end! Here’s the overview of what you’re applying for."
 };
 
 export const accountNames = {
--- webapply/src/containers/SelectServices/components/SubmitApplication/BlockConfirm/index.js
+++ webapply/src/containers/SelectServices/components/SubmitApplication/BlockConfirm/index.js
@@ -7,12 +7,7 @@ import { AutoSaveField as Field, Checkbox } from "../../../../../components/Form
 
 import { useStyles } from "./styled";
 
-const {
-  termConditionUrl,
-  termOfEnrolmentUrl,
-  trueNdCompleteAcknldgelabel,
-  needCommunicationLabel
-} = submitApplication;
+const { termConditionUrl, termOfEnrolmentUrl } = submitApplication;
 
 const blockConfirmSchema = Yup.object({
   isInformationProvided: Yup.boolean().oneOf([true], "Required"),
@@ -62,7 +57,7 @@ export const BlockConfirm = ({ setFormFields }) => {
           <Form>
             <Field
               name="isInformationProvided"
-              label={trueNdCompleteAcknldgelabel}
+              label="I confirm that the information provided is true and complete"
               onChange={e => setFieldsValue(e, setFieldValue, values)}
               component={Checkbox}
               inputProps={{ tabIndex: 0 }}
@@ -79,7 +74,7 @@ export const BlockConfirm = ({ setFormFields }) => {
             <Field
               name="needCommunication"
               path="prospect.channelServicesInfo.marketingSMS"
-              label={needCommunicationLabel}
+              label="I want to receive marketing and promotional communication from RAKBANK"
               component={Checkbox}
               inputProps={{ tabIndex: 0 }}
             />
