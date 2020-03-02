c601e4a051351bc111b001e54af548d9fa201c4b
Dmitry L <pro@a1425s-MacBook-Pro.local>
2020-03-02 23:49:27 +0200

    RO login it is showing name correctly

--- webapply/src/store/selectors/loginSelector.js
+++ webapply/src/store/selectors/loginSelector.js
@@ -6,4 +6,4 @@ export const getLoginSelector = state => state.login;
 
 export const checkLoginStatus = state => state.login.loginStatus;
 
-export const getAgentName = state => state.login.loginResponse.agentName;
+export const getAgentName = state => state.appConfig.login.userName;
