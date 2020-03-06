e901ddeabec3889b8e654e3b4e3da85722f2e6f0
Orkhan Jafarov <orkhanjafarovr@gmail.com>
2020-03-06 15:12:58 +0400

    reverted

--- webapply/src/containers/FormLayout/index.js
+++ webapply/src/containers/FormLayout/index.js
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import { compose } from "redux";
 import { withRouter } from "react-router-dom";
 
-import { getErrorCode, getIsEditableStatusSearchInfo } from "../../store/selectors/searchProspect";
+import { getErrorCode } from "../../store/selectors/searchProspect";
 import {
   getAccountType,
   getIsIslamicBanking,
@@ -10,23 +10,18 @@ import {
 } from "../../store/selectors/appConfig";
 import { updateViewId } from "../../store/actions/appConfig";
 import { setLockStatusByROAgent } from "../../store/actions/searchProspect";
-import { logout } from "../../store/actions/loginForm";
 import { FormLayoutComponent } from "./FormLayout";
-import { checkLoginStatus } from "../../store/selectors/loginSelector";
 
 const mapStateToProps = state => ({
   screeningError: getScreeningError(state),
   accountType: getAccountType(state),
   isIslamicBanking: getIsIslamicBanking(state),
-  errorCode: getErrorCode(state),
-  isApplyEditApplication: getIsEditableStatusSearchInfo(state),
-  isAgentLoggedIn: checkLoginStatus(state)
+  errorCode: getErrorCode(state)
 });
 
 const mapDispatchToProps = {
   updateViewId,
-  setLockStatusByROAgent,
-  logout
+  setLockStatusByROAgent
 };
 
 export const FormLayout = compose(
