360239300063f6a37acff5cc5ab14d3e9cbaaf59
Oleksandr Yemits <oleksandr.yemits@youpal.se>
2020-03-10 20:51:03 +0400

    add new tests

--- webapplyserver/src/test/java/ae/rakbank/webapply/client/OauthClientTest.java
+++ webapplyserver/src/test/java/ae/rakbank/webapply/client/OauthClientTest.java
@@ -1,5 +1,7 @@
 package ae.rakbank.webapply.client;
 
+import ae.rakbank.webapply.dto.ApiError;
+import ae.rakbank.webapply.exception.ApiException;
 import ae.rakbank.webapply.util.DehUtil;
 import ae.rakbank.webapply.util.FileUtil;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -13,7 +15,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 public class OauthClientTest {
@@ -53,6 +58,7 @@ public class OauthClientTest {
         oAuthConfigs.put("OAuthLoginFlag", "");
         oAuthConfigs.put("OAuthLoginType", "");
         oAuthConfigs.put("OAuthStateMode", "");
+        oAuthConfigs.put("OAuthRefreshGrantType", "TOKEN");
 
         config.set("OtherConfigs", mapper.createObjectNode().set("local", oAuthConfigs));
         config.set("BaseURLs", mapper.createObjectNode().set("local", mapper.createObjectNode().put("OAuthBaseUrl", "http://base-url")));
@@ -62,16 +68,50 @@ public class OauthClientTest {
     }
 
     @Test
-    public void authorize() {
+    public void authorizeWithOkResponse() {
         Mockito.when(restTemplate.exchange(eq("http://base-url/token-uri"), eq(HttpMethod.POST), any(), eq(JsonNode.class)))
                 .thenReturn(ResponseEntity.ok().build());
 
         final ResponseEntity<JsonNode> responseEntity = oauthClient.authorize("userName", "password");
 
         Assert.assertNotNull(responseEntity);
+        Assert.assertSame(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    @Test(expected = ApiException.class)
+    public void authorizeWithBadResponse() {
+
+        final HttpClientErrorException httpClientErrorException = new HttpClientErrorException(HttpStatus.BAD_REQUEST);
+
+        Mockito.when(dehUtil.initApiError(httpClientErrorException, HttpStatus.BAD_REQUEST)).thenReturn(ApiError.builder().status(HttpStatus.BAD_REQUEST).build());
+
+        Mockito.when(restTemplate.exchange(eq("http://base-url/token-uri"), eq(HttpMethod.POST), any(), eq(JsonNode.class)))
+                .thenThrow(httpClientErrorException);
+
+        oauthClient.authorize("userName", "password");
+    }
+
+    @Test(expected = ApiException.class)
+    public void authorizeWithInternalServerErrorResponse() {
+
+        final HttpServerErrorException httpServerErrorException = new HttpServerErrorException(HttpStatus.BAD_GATEWAY);
+
+        Mockito.when(dehUtil.initApiError(httpServerErrorException, HttpStatus.INTERNAL_SERVER_ERROR)).thenReturn(ApiError.builder().status(HttpStatus.BAD_GATEWAY).build());
+
+        Mockito.when(restTemplate.exchange(eq("http://base-url/token-uri"), eq(HttpMethod.POST), any(), eq(JsonNode.class)))
+                .thenThrow(httpServerErrorException);
+
+        oauthClient.authorize("userName", "password");
     }
 
     @Test
     public void refreshAccessToken() {
+
+        Mockito.when(restTemplate.exchange(eq("http://base-url/token-uri"), eq(HttpMethod.POST), any(), eq(JsonNode.class)))
+                .thenReturn(ResponseEntity.ok().build());
+
+        final ResponseEntity<JsonNode> responseEntity = oauthClient.refreshAccessToken("refresh_token_123");
+        Assert.assertNotNull(responseEntity);
+        Assert.assertSame(HttpStatus.OK, responseEntity.getStatusCode());
     }
 }
