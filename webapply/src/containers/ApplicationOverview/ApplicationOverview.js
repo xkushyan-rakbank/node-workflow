ce068dd5656b539be7336cd5f751d1e432d3d1e8
Orkhan Jafarov <orkhanjafarovr@gmail.com>
2020-03-10 19:59:43 +0400

    added useFormNavigation'

--- webapply/src/containers/ApplicationOverview/ApplicationOverview.js
+++ webapply/src/containers/ApplicationOverview/ApplicationOverview.js
@@ -3,8 +3,11 @@ import { useDispatch } from "react-redux";
 
 import { ApplicationOverviewComponent } from "./components/ApplicationOverviewComponent";
 import { removeProspectId } from "../../store/actions/appConfig";
+import { useFormNavigation } from "../../components/FormNavigation/FormNavigationProvider";
 
 export const ApplicationOverview = () => {
+  useFormNavigation([true, false]);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(removeProspectId());
