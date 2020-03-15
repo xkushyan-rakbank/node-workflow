d358f19fa474c7c647fcf82af7df08424d972761
oleksandr-ansimov <alex.ansimov.ua@gmail.com>
2020-03-15 16:34:05 +0200

    unit tests task

--- document_uploader/src/main/java/ae/rakbank/documentuploader/dto/FileDto.java
+++ document_uploader/src/main/java/ae/rakbank/documentuploader/dto/FileDto.java
@@ -1,26 +1,11 @@
 package ae.rakbank.documentuploader.dto;
 
+import lombok.Data;
+
+@Data
 public class FileDto {
 
     private final String contentType;
     private final String fileName;
     private final byte[] content;
-
-    public FileDto(String contentType, String fileName, byte[] content) {
-        this.contentType = contentType;
-        this.fileName = fileName;
-        this.content = content;
-    }
-
-    public String getContentType() {
-        return contentType;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public byte[] getContent() {
-        return content;
-    }
 }
