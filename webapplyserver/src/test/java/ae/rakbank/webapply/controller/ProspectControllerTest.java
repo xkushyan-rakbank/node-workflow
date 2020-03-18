28eb6ebaae4770e9b467b08ff3f7acca7ecd7993
Oleksandr Yemits <oleksandr.yemits@youpal.se>
2020-03-18 13:26:56 +0200

    added tests for controllers

---
+++ webapplyserver/src/test/java/ae/rakbank/webapply/controller/ProspectControllerTest.java
@@ -0,0 +1,24 @@
+package ae.rakbank.webapply.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProspectControllerTest {
+
+    @Test
+    public void createSMEProspect() {
+    }
+
+    @Test
+    public void getProspectById() {
+    }
+
+    @Test
+    public void updateSMEProspect() {
+    }
+
+    @Test
+    public void searchProspect() {
+    }
+}
