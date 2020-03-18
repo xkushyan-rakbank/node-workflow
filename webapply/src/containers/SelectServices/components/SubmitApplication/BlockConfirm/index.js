7f4d537050f54e038ebbb706f9e2d471e10f5fe5
Dmitriy Soldatov <dmitrsold@gmail.com>
2020-03-18 15:48:16 +0200

    add new line

--- webapply/src/containers/SelectServices/components/SubmitApplication/BlockConfirm/index.js
+++ webapply/src/containers/SelectServices/components/SubmitApplication/BlockConfirm/index.js
@@ -5,5 +5,6 @@ import { getIsIslamicBanking } from "../../../../../store/selectors/appConfig";
 
 export const BlockConfirm = rest => {
   const isIslamicBanking = useSelector(getIsIslamicBanking);
+
   return <BlockConfirmComponent isIslamicBanking={isIslamicBanking} {...rest} />;
 };
