c3d1774933733b7b3149c48ca79de575858c6d0e
Oleksandr Yemits <oleksandr.yemits@youpal.se>
2020-03-22 11:54:57 +0200

    added new test cases

---
+++ webapplyserver/src/test/java/ae/rakbank/webapply/controller/WebApplyControllerTest.java
@@ -0,0 +1,20 @@
+package ae.rakbank.webapply.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WebApplyControllerTest {
+
+    @Test
+    public void health() {
+    }
+
+    @Test
+    public void generateVerifyOTP() {
+    }
+
+    @Test
+    public void login() {
+    }
+}
