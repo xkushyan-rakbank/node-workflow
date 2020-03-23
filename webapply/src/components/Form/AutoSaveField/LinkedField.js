39a98ac0d3b0f2c10dc269dd3a117c176533c0d0
jarick <yaroslav.zolotarev@designit.com>
2020-03-23 17:45:30 +0200

    fix: LinkedField changes

--- webapply/src/components/Form/AutoSaveField/LinkedField.js
+++ webapply/src/components/Form/AutoSaveField/LinkedField.js
@@ -2,24 +2,24 @@ import React, { useCallback } from "react";
 import { useFormikContext, getIn } from "formik";
 import { AutoSaveField } from "./AutoSaveField";
 
-const noop = () => {};
-
 export const LinkedField = ({
   linkedFieldName,
   linkedPath,
-  onChange = noop,
+  onChange = () => {},
   changeProspect = prospect => prospect,
   ...rest
 }) => {
   const { values, setFieldValue, setFieldTouched } = useFormikContext();
   const linkedFieldValue = getIn(values, linkedFieldName);
+
   const onChangeHandler = useCallback(
     e => {
       setFieldValue(rest.name, e.target.value);
       linkedFieldValue && setFieldTouched(linkedFieldName, true);
       onChange(e);
     },
-    [onChange, linkedFieldName, linkedFieldValue]
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [linkedFieldValue]
   );
 
   const changeProspectHandler = useCallback(
@@ -29,7 +29,8 @@ export const LinkedField = ({
         : { ...prospect, [linkedPath]: getIn(values, linkedFieldName) };
       return changeProspect(newProspect, value, path, errors);
     },
-    [changeProspect, linkedPath, linkedFieldName]
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
   );
 
   return (
