b88039c4ebcf0b0000dad2ea706b9a2ddcf645a6
jarick <yaroslav.zolotarev@designit.com>
2020-03-27 14:14:15 +0200

    fix: code style in `searchProspect` reducer tests

--- webapply/__tests__/store/reducers/searchProspect.test.js
+++ webapply/__tests__/store/reducers/searchProspect.test.js
@@ -10,76 +10,58 @@ import {
 import { logout } from "../../../src/store/actions/loginForm";
 
 describe("searchProspect reducer test", () => {
-  it("SEARCH_APPLICATIONS_REQUEST action type", () => {
-    const expectedState = {
-      ...initialState,
+  it("should handle SEARCH_APPLICATIONS_REQUEST action type", () => {
+    expect(reducer(undefined, searchApplications())).toMatchObject({
       isSearchLoading: true
-    };
-    expect(reducer(initialState, searchApplications())).toStrictEqual(expectedState);
+    });
   });
 
-  it("SEARCH_APPLICATIONS_SUCCESS action type", () => {
+  it("should handle SEARCH_APPLICATIONS_SUCCESS action type", () => {
     const searchResults = [1, 2];
-    const expectedState = {
-      ...initialState,
+
+    expect(reducer(undefined, searchApplicationsSuccess(searchResults))).toMatchObject({
       searchResults,
       isSearchLoading: false
-    };
-    expect(reducer(initialState, searchApplicationsSuccess(searchResults))).toStrictEqual(
-      expectedState
+    });
+  });
+
+  it("should handle SEARCH_APPLICATIONS_SUCCESS action type with undefined payload", () => {
+    expect(reducer(undefined, searchApplicationsSuccess(undefined))).toHaveProperty(
+      "searchResults",
+      []
     );
   });
 
-  it("SEARCH_APPLICATIONS_SUCCESS action type with undefined payload", () => {
-    expect(reducer(initialState, searchApplicationsSuccess(undefined)).searchResults).toStrictEqual([]);
-  });
-
-  it("SEARCH_APPLICATIONS_FAILURE action type", () => {
-    const searchResults = [1, 2];
-    const updatedState = {
-      ...initialState,
-      searchResults,
+  it("should handle SEARCH_APPLICATIONS_FAILURE action type", () => {
+    expect(reducer(undefined, searchApplicationsFailure())).toMatchObject({
       isSearchLoading: false
-    };
-    expect(reducer(updatedState, searchApplicationsFailure(searchResults))).toStrictEqual(
-      initialState
-    );
+    });
   });
 
-  it("IS_APPLY_EDIT_APPLICATION action type", () => {
-    const expectedState = {
-      ...initialState,
+  it("should handle IS_APPLY_EDIT_APPLICATION action type", () => {
+    expect(reducer(undefined, setIsApplyEditApplication(true))).toMatchObject({
       isApplyEditApplication: true
-    };
-    expect(reducer(initialState, setIsApplyEditApplication(true))).toStrictEqual(expectedState);
+    });
   });
 
-  it("SET_ERROR_OCCURRED_WHILE_PERFORMING action type", () => {
-    const payload = {
-      errorCode: 500
-    };
-    const expectedState = {
-      ...initialState,
-      errorCode: payload.errorCode
-    };
-    expect(reducer(initialState, setErrorOccurredWhilePerforming(payload))).toStrictEqual(
-      expectedState
+  it("should handle SET_ERROR_OCCURRED_WHILE_PERFORMING action type", () => {
+    const error = { errorCode: 500 };
+
+    expect(reducer(undefined, setErrorOccurredWhilePerforming(error))).toHaveProperty(
+      "errorCode",
+      500
     );
   });
 
-  it("GET_PROSPECT_OVERVIEW_SUCCESS action type", () => {
-    const prospect = {}
-    const expectedState = {
-      ...initialState,
+  it("should handle GET_PROSPECT_OVERVIEW_SUCCESS action type", () => {
+    const prospect = {};
+
+    expect(reducer(undefined, getProspectOverviewSuccess(prospect))).toMatchObject({
       prospectOverview: prospect
-    };
-    expect(reducer(initialState, getProspectOverviewSuccess(prospect))).toStrictEqual(
-      expectedState
-    );
+    });
   });
 
-  it("LOGOUT action type", () => {
-    const updatedState = {};
-    expect(reducer(updatedState, logout())).toStrictEqual(initialState);
+  it("should handle LOGOUT action type", () => {
+    expect(reducer({}, logout())).toStrictEqual(initialState);
   });
 });
--- webapply/src/store/actions/searchProspect.js
+++ webapply/src/store/actions/searchProspect.js
@@ -9,43 +9,45 @@ export const GET_PROSPECT_OVERVIEW_REQUEST = "GET_PROSPECT_OVERVIEW_REQUEST";
 export const GET_PROSPECT_OVERVIEW_SUCCESS = "GET_PROSPECT_OVERVIEW_SUCCESS";
 export const GET_PROSPECT_OVERVIEW_FAIL = "GET_PROSPECT_OVERVIEW_FAIL";
 
-export const searchApplications = payload => {
-  return { type: SEARCH_APPLICATIONS_REQUEST, payload };
-};
+export const searchApplications = data => ({
+  type: SEARCH_APPLICATIONS_REQUEST,
+  payload: data
+});
 
-export const searchApplicationsSuccess = payload => {
-  return { type: SEARCH_APPLICATIONS_SUCCESS, payload };
-};
+export const searchApplicationsSuccess = data => ({
+  type: SEARCH_APPLICATIONS_SUCCESS,
+  payload: data
+});
 
-export const searchApplicationsFailure = () => {
-  return {
-    type: SEARCH_APPLICATIONS_FAILURE
-  };
-};
+export const searchApplicationsFailure = () => ({
+  type: SEARCH_APPLICATIONS_FAILURE
+});
 
-export const setIsApplyEditApplication = payload => {
-  return { type: IS_APPLY_EDIT_APPLICATION, payload };
-};
+export const setIsApplyEditApplication = isApplyEditApplication => ({
+  type: IS_APPLY_EDIT_APPLICATION,
+  payload: isApplyEditApplication
+});
 
-export const setErrorOccurredWhilePerforming = payload => {
-  return { type: SET_ERROR_OCCURRED_WHILE_PERFORMING, payload };
-};
+export const setErrorOccurredWhilePerforming = error => ({
+  type: SET_ERROR_OCCURRED_WHILE_PERFORMING,
+  payload: error
+});
 
-export const getProspectOverviewPromisify = prospectId => {
-  return {
-    type: GET_PROSPECT_OVERVIEW_REQUEST,
-    [WAIT_FOR_ACTION]: GET_PROSPECT_OVERVIEW_SUCCESS,
-    [ERROR_ACTION]: GET_PROSPECT_OVERVIEW_FAIL,
-    payload: {
-      prospectId
-    }
-  };
-};
+export const getProspectOverviewPromisify = prospectId => ({
+  type: GET_PROSPECT_OVERVIEW_REQUEST,
+  [WAIT_FOR_ACTION]: GET_PROSPECT_OVERVIEW_SUCCESS,
+  [ERROR_ACTION]: GET_PROSPECT_OVERVIEW_FAIL,
+  payload: {
+    prospectId
+  }
+});
 
-export const getProspectOverviewSuccess = prospect => {
-  return { type: GET_PROSPECT_OVERVIEW_SUCCESS, payload: { prospect } };
-};
+export const getProspectOverviewSuccess = prospect => ({
+  type: GET_PROSPECT_OVERVIEW_SUCCESS,
+  payload: { prospect }
+});
 
-export const getProspectOverviewFail = error => {
-  return { type: GET_PROSPECT_OVERVIEW_FAIL, error };
-};
+export const getProspectOverviewFail = error => ({
+  type: GET_PROSPECT_OVERVIEW_FAIL,
+  error
+});
