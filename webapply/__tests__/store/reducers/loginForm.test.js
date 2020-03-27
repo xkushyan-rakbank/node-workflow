910eb9fa1801dd73e1a4637624412152779c0bd9
jarick <yaroslav.zolotarev@designit.com>
2020-03-27 12:25:11 +0200

    fix: `loginForm` reducer

--- webapply/__tests__/store/reducers/loginForm.test.js
+++ webapply/__tests__/store/reducers/loginForm.test.js
@@ -1,23 +1,16 @@
 import reducer, { initialState } from "../../../src/store/reducers/loginForm";
 import { loginInfoFormSuccess, logout } from "../../../src/store/actions/loginForm";
 
-  describe("loginForm reducer test", () => {
-    it("LOGIN_INFO_FORM_SUCCESS action type", () => {
-      const payload = {};
-      const expectedState = {
-        ...initialState,
-        loginResponse: payload,
-        loginStatus: true
-      };
-      expect(reducer(initialState, loginInfoFormSuccess(payload))).toStrictEqual(expectedState);
+describe("loginForm reducer test", () => {
+  it("should handle LOGIN_INFO_FORM_SUCCESS action type", () => {
+    const response = "some data";
+    expect(reducer(undefined, loginInfoFormSuccess(response))).toMatchObject({
+      loginResponse: response,
+      loginStatus: true
     });
-  
-    it("LOGOUT action type", () => {
-      const expectedState = {
-        ...initialState,
-        loginResponse: {},
-        loginStatus: false
-      };
-      expect(reducer(initialState, logout())).toStrictEqual(expectedState);
-    });
-  })
+  });
+
+  it("should handle LOGOUT action type", () => {
+    expect(reducer({}, logout())).toStrictEqual(initialState);
+  });
+});
