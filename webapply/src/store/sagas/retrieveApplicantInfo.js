4167e316c986a9288c735c14ebba2be24a2581bd
Dmitriy Soldatov <dmitrsold@gmail.com>
2020-03-27 15:09:34 +0200

    fix when stakeholders is blank on comeback

--- webapply/src/store/sagas/retrieveApplicantInfo.js
+++ webapply/src/store/sagas/retrieveApplicantInfo.js
@@ -1,9 +1,9 @@
 import { all, call, put, takeLatest, select } from "redux-saga/effects";
 import * as actions from "../actions/retrieveApplicantInfo";
-import { setConfig, loadMetaData } from "../actions/appConfig";
+import { setConfig, loadMetaData, updateProspect } from "../actions/appConfig";
 import { retrieveApplicantInfos, prospect as prospectApi } from "../../api/apiClient";
 import { log } from "../../utils/loggger";
-import { getAuthorizationHeader } from "../selectors/appConfig";
+import { getAuthorizationHeader, getSignatoryModel } from "../selectors/appConfig";
 import { updateStakeholdersIds } from "../actions/stakeholders";
 import { COMPANY_STAKEHOLDER_ID } from "../../containers/CompanyStakeholders/constants";
 
@@ -33,6 +33,7 @@ function* getProspectIdInfo({ payload }) {
     const response = yield call(prospectApi.get, payload.prospectId, headers);
     const config = { prospect: response.data };
     const freeFieldsInfo = config.prospect.freeFieldsInfo;
+    const newStakeholder = yield select(getSignatoryModel);
 
     yield put(setConfig(config));
 
@@ -51,6 +52,13 @@ function* getProspectIdInfo({ payload }) {
               };
             }, {});
           yield put(updateStakeholdersIds(Object.values(stakeholdersIds)));
+          if (!config.prospect.signatoryInfo.length) {
+            yield put(
+              updateProspect({
+                "prospect.signatoryInfo": [newStakeholder]
+              })
+            );
+          }
         } catch (error) {
           log(error);
         }
