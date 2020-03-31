2530b7823045847bedade2d741dfbda298aa0707
jarick <yaroslav.zolotarev@designit.com>
2020-03-31 13:25:27 +0200

    Fix `completedSteps` selector test

--- webapply/__tests__/selectors/completedSteps.test.js
+++ webapply/__tests__/selectors/completedSteps.test.js
@@ -1,34 +1,61 @@
 import {
-    getCompletedSteps,
-    getIsAnyStakeholderStepsCompleted,
-    getIsStakeholderStepsCompleted,
-    getStakeholderSteps
+  getCompletedSteps,
+  getIsAnyStakeholderStepsCompleted,
+  getIsStakeholderStepsCompleted,
+  getStakeholderSteps
 } from "../../src/store/selectors/completedSteps";
 
 describe("completedSteps selector test", () => {
-    const completedSteps = [
-        { flowId: "companyInfo", step: 1, status: "COMPLETED" },
-        { flowId: "finalQuestionsCompany", step: 1, status: "COMPLETED" },
-        { flowId: "companySignatory_8", step: 2, status: "NOT_AVAILABLE" },
-        { flowId: "companyStakeholder_4", step: 2, status: "COMPLETED" },
-        { flowId: "companyStakeholder_3", step: 2, status: "NOT_AVAILABLE" }
-    ];
-    it("should return completed steps array", () => {
-        expect(getCompletedSteps({ completedSteps })).toEqual(completedSteps);
-    });
+  const companyStep = { flowId: "companyInfo", step: 1, status: "COMPLETED" };
+  const completedCompanyStakeholderStep = {
+    flowId: "companyStakeholder_1",
+    step: 1,
+    status: "COMPLETED"
+  };
+  const notCompletedCompanyStakeholderStep = {
+    flowId: "companyStakeholder_1",
+    step: 2,
+    status: "NOT_AVAILABLE"
+  };
+  const completedSteps = [
+    companyStep,
+    completedCompanyStakeholderStep,
+    notCompletedCompanyStakeholderStep
+  ];
+  const state = { completedSteps };
 
-    it("should return company stakeholders steps array ", () => {
-        expect(getStakeholderSteps({ completedSteps })).toEqual([
-            { flowId: "companyStakeholder_4", step: 2, status: "COMPLETED" },
-            { flowId: "companyStakeholder_3", step: 2, status: "NOT_AVAILABLE" }
-        ]);
-    });
+  it("should return completed steps", () => {
+    expect(getCompletedSteps(state)).toEqual(completedSteps);
+  });
 
-    it("should return IsAnyStakeholderStepsCompleted status", () => {
-        expect(getIsAnyStakeholderStepsCompleted({ completedSteps })).toBe(false);
-    });
+  it("should return empty array when completed steps is not set", () => {
+    expect(getCompletedSteps({})).toEqual([]);
+  });
 
-    it("should return IsStakeholderStepsCompleted status", () => {
-        expect(getIsStakeholderStepsCompleted({ completedSteps })).toBe(false);
-    });
+  it("should return company stakeholders steps", () => {
+    expect(getStakeholderSteps(state)).toEqual([
+      completedCompanyStakeholderStep,
+      notCompletedCompanyStakeholderStep
+    ]);
+  });
+
+  it("should return false when not any stakeholder steps have completed status", () => {
+    expect(getIsAnyStakeholderStepsCompleted(state)).toBe(false);
+  });
+
+  it("should return true when any stakeholder steps have completed status", () => {
+    expect(
+      getIsAnyStakeholderStepsCompleted({ completedSteps: [completedCompanyStakeholderStep] })
+    ).toBe(true);
+  });
+
+  it("should return false when not all stakeholder steps have completed status", () => {
+    expect(getIsStakeholderStepsCompleted(state)).toBe(false);
+  });
+
+  it("should return true when all stakeholder steps have completed status", () => {
+    expect(
+      getIsStakeholderStepsCompleted({ completedSteps: [completedCompanyStakeholderStep] })
+    ).toBe(true);
+  });
 });
