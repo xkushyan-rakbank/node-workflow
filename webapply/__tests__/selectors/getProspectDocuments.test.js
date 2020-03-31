8025bd2ac26a5f6ec48023ae80687b0e66d51bba
hlib.moiseiev <hlib.moiseiev@designit.com>
2020-03-31 16:59:48 +0300

    created getProspectDocuments selector test

---
+++ webapply/__tests__/selectors/getProspectDocuments.test.js
@@ -0,0 +1,65 @@
+import {
+  getUploadDocuments,
+  getisLoadingDocuments,
+  getIsRequiredDocsUploaded,
+  getOtherDocuments,
+  getProgress,
+  getProspectDocuments,
+  getUploadErrors
+} from "../../src/store/selectors/getProspectDocuments";
+
+describe("getProspectDocuments selector test", () => {
+  const progress = "some progress";
+  const uploadErrors = "some uploadErrors";
+  const uploadDocuments = {
+    progress,
+    uploadErrors,
+    isLoading: true
+  };
+  const companyDocuments = [{ uploadStatus: "Uploaded" }, { uploadStatus: "Uploaded" }];
+  const stakeholdersDocuments = {
+    O_: { documents: [{ uploadStatus: "Uploaded" }, { uploadStatus: "Uploaded" }] }
+  };
+  const otherDocuments = ["other documents"];
+  const documents = {
+    companyDocuments,
+    stakeholdersDocuments,
+    otherDocuments
+  };
+  const state = { uploadDocuments };
+  it("should return upload documents", () => {
+    expect(getUploadDocuments(state)).toEqual(uploadDocuments);
+  });
+
+  it("should return progress", () => {
+    expect(getProgress(state)).toBe(progress);
+  });
+
+  it("should return upload errors", () => {
+    expect(getUploadErrors(state)).toEqual(uploadErrors);
+  });
+
+  it("should return empty object when documents is not set", () => {
+    expect(getProspectDocuments({ appConfig: { prospect: {} } })).toEqual({});
+  });
+
+  it("should return prospect documents", () => {
+    expect(getProspectDocuments({ appConfig: { prospect: { documents } } })).toEqual(documents);
+  });
+
+  it("should return empty array when otherDocuments is not set", () => {
+    expect(getOtherDocuments({ appConfig: { prospect: { documents: {} } } })).toEqual([]);
+  });
+
+  it("should return other documents array", () => {
+    expect(getOtherDocuments({ appConfig: { prospect: { documents } } })).toEqual(otherDocuments);
+  });
+
+  it("should return true when isLoading equal true", () => {
+    expect(getisLoadingDocuments(state)).toBe(true);
+  });
+
+  it("should return true when all required docs are uploaded", () => {
+    expect(getIsRequiredDocsUploaded({ appConfig: { prospect: { documents } } })).toBe(true);
+  });
+});
