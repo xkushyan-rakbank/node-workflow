ef1ed3b9a130565b25140850427d608fe51d0429
hlib.moiseiev <hlib.moiseiev@designit.com>
2020-04-01 10:50:23 +0300

    created test

---
+++ webapply/__tests__/selectors/loginSelector.test.js
@@ -0,0 +1,24 @@
+import {checkLoginStatus, getAgentName, getLoginResponse} from "../../src/store/selectors/loginSelector";
+
+describe("loginSelector test", () => {
+  const loginStatus = "some login status";
+  const agentName = "some login response";
+  const loginResponse = {agentName};
+  const state = {login:{loginStatus, loginResponse}};
+
+  it("should return loginStatus", () => {
+    expect(checkLoginStatus(state)).toBe(loginStatus)
+  });
+
+  it("should return empty object when loginResponse is not set", () => {
+    expect(getLoginResponse({login: {}})).toEqual({})
+  });
+
+  it("should return loginStatus", () => {
+    expect(getLoginResponse(state)).toEqual(loginResponse)
+  });
+
+  it("should return agentName", () => {
+    expect(getAgentName(state)).toBe(agentName)
+  })
+});
