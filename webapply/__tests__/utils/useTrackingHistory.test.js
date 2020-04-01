9fe4e4642251ee3e6d2fa235184686134467e649
Dmitriy Soldatov <dmitrsold@gmail.com>
2020-04-01 21:03:45 +0300

    fix useTrackingHistory

--- webapply/__tests__/utils/useTrackingHistory.test.js
+++ webapply/__tests__/utils/useTrackingHistory.test.js
@@ -1,24 +1,32 @@
 import { renderHook } from "@testing-library/react-hooks";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { useTrackingHistory } from "../../src/utils/useTrackingHistory";
+import { gaEventsMap } from "../../src/utils/useTrackingHistory/constants";
+import routes from "../../src/routes";
+import { sendGoogleAnalyticsMetrics } from "../../src/store/actions/googleAnalytics";
+
+let dispatchedArgs = null;
+let historyPushArgs = null;
+let historyReplaceArgs = null;
 
 jest.mock("react-router-dom", () => ({
   useHistory: () => ({
-    push: jest.fn()
+    push: jest.fn(path => (historyPushArgs = path)),
+    replace: jest.fn(path => (historyReplaceArgs = path))
   })
 }));
 
 jest.mock("react-redux", () => ({
-  useSelector: jest.fn(fn => fn()),
-  useDispatch: () => jest.fn()
+  useDispatch: () => jest.fn(action => (dispatchedArgs = action))
 }));
 
 describe("useTrackingHistory tests", () => {
   afterEach(() => {
     jest.clearAllMocks();
-  });
-
-  afterAll(() => {
-    jest.restoreAllMocks();
+    dispatchedArgs = null;
+    historyPushArgs = null;
+    historyReplaceArgs = null;
   });
 
   it("should return function", () => {
@@ -26,5 +34,18 @@ describe("useTrackingHistory tests", () => {
     expect(typeof result.current).toBe("function");
   });
 
-  it.todo("should change path");
+  it("should call history push method", () => {
+    const { result } = renderHook(() => useTrackingHistory());
+    result.current(routes.verifyOtp);
+
+    expect(dispatchedArgs).toEqual(sendGoogleAnalyticsMetrics(gaEventsMap[routes.verifyOtp]));
+    expect(historyPushArgs).toEqual(routes.verifyOtp);
+  });
+
+  it("should call history replace method", () => {
+    const { result } = renderHook(() => useTrackingHistory());
+    result.current(routes.verifyOtp, true);
+    expect(dispatchedArgs).toEqual(sendGoogleAnalyticsMetrics(gaEventsMap[routes.verifyOtp]));
+    expect(historyReplaceArgs).toEqual(routes.verifyOtp);
+  });
 });
