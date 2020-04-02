0e33e2ef8a353acea7c20cb57e4d2ca30ee4f048
jarick <yaroslav.zolotarev@designit.com>
2020-04-02 15:02:19 +0200

    fix: useOnClickOutside test

--- webapply/__tests__/utils/useOnClickOutside.test.js
+++ webapply/__tests__/utils/useOnClickOutside.test.js
@@ -1,52 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
 import ReactDOM from "react-dom";
-import ReactTestUtils from "react-dom/test-utils";
-import { renderHook } from "@testing-library/react-hooks";
+import { act } from "react-dom/test-utils";
+
 import { useOnClickOutside } from "../../src/utils/useOnClickOutside";
 
-let root;
+describe("useOnClickOutside test", () => {
+  let container;
 
-describe("useOnClickOutside tests", () => {
   beforeEach(() => {
-    root = document.createElement("div");
+    container = document.createElement("div");
+    document.body.appendChild(container);
   });
+
   afterEach(() => {
-    ReactDOM.unmountComponentAtNode(root);
+    document.body.removeChild(container);
+    container = null;
   });
-  it("should return handler call when clicked on target", () => {
-    const mouseEvent = new MouseEvent("mousedown");
 
-    const spanHandler = jest.fn(() => true);
-    const outsideHandler = jest.fn(() => false);
-    let outsideRef = null;
-    let divRef = null;
-    let spanRef = null;
+  it("should handle callback on click outside", () => {
+    let fn = jest.fn();
+    const TestComponent = () => {
+      const ref = useRef();
+      useOnClickOutside(ref, fn);
 
-    const Component = () => {
-      return (
-        <div id="outside" ref={node => (outsideRef = node)}>
-          <div id="container" ref={node => (divRef = node)}>
-            <span id="nested" ref={node => (spanRef = node)} onClick={spanHandler}>
-              test
-            </span>
-          </div>
-        </div>
-      );
+      return <div id="test" ref={ref} />;
     };
 
-    ReactDOM.render(<Component />, root);
-
-    const containerRef = { current: divRef }; // emulate UseRef output
-    const { rerender } = renderHook(() => useOnClickOutside(containerRef, outsideHandler));
-
-    ReactTestUtils.Simulate.click(spanRef);
-    expect(spanHandler).toBeCalled();
-    expect(spanHandler).toHaveReturnedWith(true);
-
-    rerender(containerRef, outsideHandler);
-    ReactTestUtils.Simulate.click(outsideRef);
-    document.dispatchEvent(mouseEvent);
-    expect(outsideHandler).toBeCalled();
-    expect(outsideHandler).toHaveReturnedWith(false);
+    act(() => {
+      ReactDOM.render(<TestComponent />, container);
+    });
+    act(() => {
+      container
+        .querySelector("div#test")
+        .dispatchEvent(new MouseEvent("mousedown", { bubbles: true }));
+    });
+    expect(fn).not.toHaveBeenCalled();
+    act(() => {
+      container.dispatchEvent(new MouseEvent("mousedown", { bubbles: true }));
+    });
+    expect(fn).toHaveBeenCalled();
   });
 });
