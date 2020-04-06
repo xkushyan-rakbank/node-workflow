7227e1f56ffcbbf0804e0f864622ae1fdeac06d7
Dmitriy Soldatov <dmitrsold@gmail.com>
2020-04-06 14:56:04 +0300

    DRY, fix 2 cases and naming

--- webapply/__tests__/utils/useDisplayScreenBasedOnViewId.test.js
+++ webapply/__tests__/utils/useDisplayScreenBasedOnViewId.test.js
@@ -50,103 +50,87 @@ jest.mock("react-redux", () => ({
 }));
 
 describe("useDisplayScreenBasedOnViewId ", () => {
+  beforeEach(() => {
+    getApplicationInfo.mockReturnValue(mockDefaultApplicationInfo);
+    getProspectId.mockReturnValue(mockProspectId);
+    getIsEditableStatusSearchInfo.mockReturnValue(mockIsROScreensFalse);
+    getSearchResultsStatuses.mockReturnValue(mockDefaultStatuses);
+  });
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   it("should take prospect from store if prospect was not provided", () => {
     getApplicationInfo.mockReturnValue(mockSubmitApplicationInfo);
-    getProspectId.mockReturnValue(mockProspectId);
     getIsEditableStatusSearchInfo.mockReturnValue(mockIsROScreensTrue);
-    getSearchResultsStatuses.mockReturnValue(mockDefaultStatuses);
     const { result } = renderHook(() => useDisplayScreenBasedOnViewId());
     act(() => {
       result.current.pushDisplayScreenToHistory();
     });
-    expect(mockPush).toHaveBeenCalledWith(routes.companyInfo);
+    expect(mockPush).toBeCalledWith(routes.companyInfo);
   });
 
   it("should redirect to company info page when app was sumbitted and RO mode", () => {
     getApplicationInfo.mockReturnValue(mockSubmitApplicationInfo);
-    getProspectId.mockReturnValue(mockProspectId);
     getIsEditableStatusSearchInfo.mockReturnValue(mockIsROScreensTrue);
-    getSearchResultsStatuses.mockReturnValue(mockDefaultStatuses);
     const { result } = renderHook(() => useDisplayScreenBasedOnViewId());
     act(() => {
       result.current.pushDisplayScreenToHistory();
     });
-    expect(mockPush).toHaveBeenCalledWith(routes.companyInfo);
+    expect(mockPush).toBeCalledWith(routes.companyInfo);
   });
 
   it("should redirect to company info page when viewId is ApplicationSubmitted/ReUploadDocuments and RO mode", () => {
-    getApplicationInfo.mockReturnValue(mockDefaultApplicationInfo);
-    getProspectId.mockReturnValue(mockProspectId);
     getIsEditableStatusSearchInfo.mockReturnValue(mockIsROScreensTrue);
-    getSearchResultsStatuses.mockReturnValue(mockDefaultStatuses);
     const { result } = renderHook(() => useDisplayScreenBasedOnViewId());
     act(() => {
       result.current.pushDisplayScreenToHistory();
     });
-    expect(mockPush).toHaveBeenCalledWith(routes.companyInfo);
+    expect(mockPush).toBeCalledWith(routes.companyInfo);
   });
 
   it("should redirect to reupload documents page when status is Document Needed and RO mode", () => {
-    getApplicationInfo.mockReturnValue(mockDefaultApplicationInfo);
-    getProspectId.mockReturnValue(mockProspectId);
-    getIsEditableStatusSearchInfo.mockReturnValue(mockIsROScreensFalse);
     getSearchResultsStatuses.mockReturnValue(mockDocumentNeededStatuses);
     const { result } = renderHook(() => useDisplayScreenBasedOnViewId());
     act(() => {
       result.current.pushDisplayScreenToHistory(prospect);
     });
-    expect(mockPush).toHaveBeenCalledWith(routes.reUploadDocuments);
+    expect(mockPush).toBeCalledWith(routes.reUploadDocuments);
   });
 
   it("should redirect to reupload documents page when app is submitted, Customer mode and retrive mode enable", () => {
     getApplicationInfo.mockReturnValue(mockSubmitApplicationInfo);
-    getProspectId.mockReturnValue(mockProspectId);
-    getIsEditableStatusSearchInfo.mockReturnValue(mockIsROScreensFalse);
-    getSearchResultsStatuses.mockReturnValue(mockDefaultStatuses);
     const { result } = renderHook(() => useDisplayScreenBasedOnViewId());
     act(() => {
       result.current.pushDisplayScreenToHistory(prospect);
     });
-    expect(mockPush).toHaveBeenCalledWith(routes.reUploadDocuments);
+    expect(mockPush).toBeCalledWith(routes.reUploadDocuments);
   });
 
   it("should redirect to application submitted page when app is submitted, Customer mode and no additional docs needed", () => {
     getApplicationInfo.mockReturnValue(mockSubmitApplicationInfo);
-    getProspectId.mockReturnValue(mockProspectId);
-    getIsEditableStatusSearchInfo.mockReturnValue(mockIsROScreensFalse);
-    getSearchResultsStatuses.mockReturnValue(mockDefaultStatuses);
     const { result } = renderHook(() => useDisplayScreenBasedOnViewId());
     act(() => {
       result.current.pushDisplayScreenToHistory();
     });
-    expect(mockPush).toHaveBeenCalledWith(routes.ApplicationSubmitted);
+    expect(mockPush).toBeCalledWith(routes.ApplicationSubmitted);
   });
 
-  it("should take companyInfo path when no viewId provided", () => {
+  it("should redirect to companyInfo page when no viewId was provided", () => {
     getApplicationInfo.mockReturnValue({ ...mockDefaultApplicationInfo, viewId: "" });
-    getProspectId.mockReturnValue(mockProspectId);
-    getIsEditableStatusSearchInfo.mockReturnValue(mockIsROScreensFalse);
-    getSearchResultsStatuses.mockReturnValue(mockDefaultStatuses);
     const { result } = renderHook(() => useDisplayScreenBasedOnViewId());
     act(() => {
       result.current.pushDisplayScreenToHistory();
     });
-    expect(mockPush).toBeCalled();
+    expect(mockPush).toBeCalledWith(routes.companyInfo);
   });
 
-  it("should take undefined prospectStatus when it did not find matches with the prospectId", () => {
-    getApplicationInfo.mockReturnValue(mockDefaultApplicationInfo);
-    getProspectId.mockReturnValue(mockProspectId);
-    getIsEditableStatusSearchInfo.mockReturnValue(mockIsROScreensFalse);
+  it("should redirect to application submitted page when prospectStatus is undefined", () => {
     getSearchResultsStatuses.mockReturnValue([{ prospectId: 0 }]);
     const { result } = renderHook(() => useDisplayScreenBasedOnViewId());
     act(() => {
       result.current.pushDisplayScreenToHistory();
     });
-    expect(mockPush).toBeCalled();
+    expect(mockPush).toBeCalledWith(routes.ApplicationSubmitted);
   });
 });
