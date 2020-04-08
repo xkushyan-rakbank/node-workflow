ac9392ccd4d1d5a8723a029544e9192bfb698425
Dmitriy Soldatov <dmitrsold@gmail.com>
2020-04-08 14:22:43 +0300

    fix test #1

--- webapply/__tests__/containers/ApplicationOverview.test.js
+++ webapply/__tests__/containers/ApplicationOverview.test.js
@@ -5,11 +5,10 @@ import { ApplicationOverview } from "../../src/containers/ApplicationOverview/Ap
 import { ApplicationOverviewComponent } from "../../src/containers/ApplicationOverview/components/ApplicationOverviewComponent";
 import { useAccountTypeByPathname } from "../../src/utils/useAccountTypeByPathname";
 import { useFormNavigation } from "../../src/components/FormNavigation/FormNavigationProvider";
-import { removeProspectId } from "../../src/store/actions/appConfig";
+import { REMOVE_PROSPECT_ID } from "../../src/store/actions/appConfig";
 
 jest.mock("../../src/utils/useAccountTypeByPathname");
 jest.mock("../../src/components/FormNavigation/FormNavigationProvider");
-jest.mock("../../src/store/actions/appConfig");
 jest.mock("../../src/containers/ApplicationOverview/components/ApplicationOverviewComponent");
 
 const mockDispatch = jest.fn();
@@ -28,15 +27,10 @@ describe("ApplicationOverview tests", () => {
     jest.clearAllMocks();
   });
 
-  it("should call useAccountTypeByPathname and useFormNavigation on render", () => {
+  it("should dispatch removeProspectId action on mount", () => {
     render(<ApplicationOverview />);
     expect(useAccountTypeByPathname).toBeCalled();
     expect(useFormNavigation).toBeCalledWith([true, false]);
-  });
-
-  it("should dispatch removeProspectId action on mount", () => {
-    render(<ApplicationOverview />);
-    expect(mockDispatch).toBeCalled();
-    expect(removeProspectId).toBeCalled();
+    expect(mockDispatch).toBeCalledWith({ type: REMOVE_PROSPECT_ID });
   });
 });
