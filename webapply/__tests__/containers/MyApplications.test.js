85e6acb9e56455ded2d3560fbb2a7bbe5ee3b78b
Dmitriy Soldatov <dmitrsold@gmail.com>
2020-04-08 13:43:38 +0300

    remove pkg lock from PR

---
+++ webapply/__tests__/containers/MyApplications.test.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { render, act } from "@testing-library/react";
+
+import { MyApplications } from "../../src/containers/MyApplications/MyApplications";
+import { MyApplications as MyApplicationsComponent } from "../../src/containers/MyApplications/components/MyApplications";
+import { getApplicantInfo } from "../../src/store/selectors/appConfig";
+import { searchApplications } from "../../src/store/actions/searchProspect";
+import { getProspectInfoPromisify } from "../../src/store/actions/retrieveApplicantInfo";
+import { useDisplayScreenBasedOnViewId } from "../../src/utils/useDisplayScreenBasedOnViewId";
+
+jest.mock("../../src/store/selectors/appConfig");
+jest.mock("../../src/utils/useDisplayScreenBasedOnViewId");
+jest.mock("../../src/store/actions/searchProspect");
+jest.mock("../../src/store/actions/retrieveApplicantInfo");
+jest.mock("../../src/containers/MyApplications/components/MyApplications");
+
+const inputParams = { email: "some_email@g.com" };
+const mockPushDisplayScreenToHistory = jest.fn();
+
+jest.mock("react-redux", () => ({
+  __esModule: true,
+  useSelector: jest.fn(fn => fn()),
+  useDispatch: jest.fn(() => () => {}),
+  connect: jest.fn(() => () => {})
+}));
+
+describe("MyApplications tests", () => {
+  beforeEach(() => {
+    getApplicantInfo.mockReturnValue(inputParams);
+    useDisplayScreenBasedOnViewId.mockReturnValue({
+      pushDisplayScreenToHistory: mockPushDisplayScreenToHistory
+    });
+    getProspectInfoPromisify.mockReturnValue(321);
+    MyApplicationsComponent.mockImplementation(() => null);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should dispatch searchApplications action  on mount", () => {
+    render(<MyApplications />);
+    expect(searchApplications).toBeCalledWith(inputParams);
+  });
+
+  it("should call pushDisplayScreenToHistory with prospectId", async () => {
+    const prospectId = "some prospectId";
+    const asynсDispatchMock = jest.fn().mockResolvedValue(prospectId);
+    useDispatch.mockImplementation(() => asynсDispatchMock);
+
+    render(<MyApplications />);
+
+    await act(() => MyApplicationsComponent.mock.calls[0][0].getProspectInfo(prospectId));
+    expect(getProspectInfoPromisify).toBeCalledWith(prospectId);
+    expect(mockPushDisplayScreenToHistory).toBeCalledWith(prospectId);
+  });
+});
