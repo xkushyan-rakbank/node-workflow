2ab43bc4466d5c5cdfed0f6a53d5255e1ba579b5
jarick <yaroslav.zolotarev@designit.com>
2020-04-09 15:09:23 +0200

    fix: DetailedAccount test

--- webapply/__tests__/containers/DetailedAccount.test.js
+++ webapply/__tests__/containers/DetailedAccount.test.js
@@ -5,30 +5,43 @@ import { DetailedAccountComponent } from "../../src/containers/DetailedAccount/c
 import { useAccountTypeByPathname } from "../../src/utils/useAccountTypeByPathname";
 
 import { useFormNavigation } from "../../src/components/FormNavigation/FormNavigationProvider";
+import { VerticalPaginationContext } from "../../src/components/VerticalPagination";
 
 jest.mock("../../src/components/FormNavigation/FormNavigationProvider");
 jest.mock("../../src/utils/useAccountTypeByPathname");
 jest.mock("../../src/containers/DetailedAccount/components/DetailedAccount");
 
-const accountType = "some accountType";
-const isIslamicBanking = true;
-
 describe("DetailedAccount test", () => {
+  const accountType = "some accountType";
+  const isIslamicBanking = "some bool";
+  const setCurrentSection = jest.fn();
+  const contextvalue = { setCurrentSection };
+
   beforeAll(() => {
     useFormNavigation.mockReturnValue(null);
     useAccountTypeByPathname.mockReturnValue({ accountType, isIslamicBanking });
     DetailedAccountComponent.mockReturnValue(null);
 
-    render(<DetailedAccountContainer />);
+    render(
+      <VerticalPaginationContext.Provider value={contextvalue}>
+        <DetailedAccountContainer />
+      </VerticalPaginationContext.Provider>
+    );
   });
 
-  it("should call useFormNavigation", () => {
+  it("should call useFormNavigation hook", () => {
     expect(useFormNavigation).toBeCalledWith([true, false]);
   });
 
+  it("should call useAccountTypeByPathname hook", () => {
+    expect(useAccountTypeByPathname).toBeCalled();
+  });
+
   it("should render DetailedAccountComponent", () => {
-    expect(DetailedAccountComponent.mock.calls[0][0]).toMatchObject({
-      accountType
+    expect(DetailedAccountComponent.mock.calls[0][0]).toEqual({
+      accountType,
+      isIslamicBanking,
+      setCurrentSection
     });
   });
 });
