b6fc394db14fdd82e9505524411538e2c5d7c039
Dmitriy Soldatov <dmitrsold@gmail.com>
2020-04-09 15:57:24 +0300

    fix notFound btn style

--- webapply/src/containers/NotFoundPage/components/NotFoundComponent.js
+++ webapply/src/containers/NotFoundPage/components/NotFoundComponent.js
@@ -5,8 +5,6 @@ import { useIconsByAccount } from "../../../utils/useIconsByAccount";
 
 import { useStyles } from "./styled";
 
-import { ReactComponent as Arrow } from "../../../assets/icons/whiteArrow.svg";
-
 export const NotFoundComponent = ({ goToHomePage }) => {
   const classes = useStyles();
   const { notFound } = useIconsByAccount();
@@ -21,14 +19,7 @@ export const NotFoundComponent = ({ goToHomePage }) => {
         height={232}
       />
       <span className={classes.appStatusText}>Sorry, we couldn{"'"}t find that page.</span>
-      <ContainedButton
-        label={
-          <span className={classes.buttonText}>
-            Go to homepage <Arrow alt="arrow" />
-          </span>
-        }
-        handleClick={goToHomePage}
-      />
+      <ContainedButton label="Go to homepage" withRightArrow handleClick={goToHomePage} />
     </div>
   );
 };
