4caf55ba9133220be4426c1b826abeae2198289d
jarick <yaroslav.zolotarev@designit.com>
2020-04-09 11:26:07 +0200

    fix: NotFoundPage tests

--- webapply/__tests__/containers/NotFoundPage.test.js
+++ webapply/__tests__/containers/NotFoundPage.test.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { render, act } from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router";
 
 import { NotFoundPage } from "../../src/containers/NotFoundPage/NotFoundPage";
 import { NotFoundComponent } from "../../src/containers/NotFoundPage/components/NotFoundComponent";
@@ -9,26 +11,27 @@ import routes from "../../src/routes";
 jest.mock("../../src/containers/NotFoundPage/components/NotFoundComponent");
 jest.mock("../../src/components/FormNavigation/FormNavigationProvider");
 
-const mockPush = jest.fn();
-jest.mock("react-router-dom", () => ({
-  useHistory: () => ({ push: mockPush })
-}));
-
 describe("NotFoundPage tests", () => {
-  beforeEach(() => {
+  const history = createMemoryHistory();
+
+  beforeAll(() => {
     NotFoundComponent.mockImplementation(() => null);
-    jest.clearAllMocks();
   });
 
   it("should redirect to accountsComparison", () => {
-    render(<NotFoundPage />);
+    render(
+      <Router history={history}>
+        <NotFoundPage />
+      </Router>
+    );
+
+    expect(NotFoundComponent).toHaveBeenCalledTimes(1);
+    expect(useFormNavigation).toBeCalledWith([false, false]);
 
     act(() => {
       NotFoundComponent.mock.calls[0][0].goToHomePage();
     });
 
-    expect(NotFoundComponent).toHaveBeenCalledTimes(1);
-    expect(useFormNavigation).toBeCalledWith([false, false]);
-    expect(mockPush).toBeCalledWith(routes.accountsComparison);
+    expect(history.location.pathname).toBe(routes.accountsComparison);
   });
 });
