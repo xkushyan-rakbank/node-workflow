f9a35780055d4073863a0440be43667a50f80ad7
oleksandr-ansimov <alex.ansimov.ua@gmail.com>
2020-04-09 10:12:51 +0300

    unit tests for S3Factory

--- document_uploader/src/test/java/ae/rakbank/documentuploader/s3/ECSS3FactoryTest.java
+++ document_uploader/src/test/java/ae/rakbank/documentuploader/s3/ECSS3FactoryTest.java
@@ -2,24 +2,16 @@ package ae.rakbank.documentuploader.s3;
 
 import ae.rakbank.documentuploader.util.FileUtil;
 import com.emc.object.s3.S3Client;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import java.net.URISyntaxException;
 
-import static ae.rakbank.documentuploader.stub.util.FileUtilStub.getAppConfigJSON;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
