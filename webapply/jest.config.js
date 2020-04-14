d524d421fddfb4908fb13100e1c4eeef3106f606
jarick <yaroslav.zolotarev@designit.com>
2020-04-14 14:06:04 +0200

    Append execute tests when user is commiting changes

--- webapply/jest.config.js
+++ webapply/jest.config.js
@@ -38,16 +38,14 @@ module.exports = {
     "./src/constants",
     "./src/assets"
   ],
-  /*
   coverageThreshold: {
     global: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
     }
   },
-  */
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js"
