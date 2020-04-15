784758ac547ce214628197b00f1fb7aaae77eeb4
hlib.moiseiev <hlib.moiseiev@designit.com>
2020-04-15 12:21:17 +0300

    prettier

--- webapply/__tests__/containers/DetailedAccount.test.js
+++ webapply/__tests__/containers/DetailedAccount.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import { render } from "@testing-library/react";
 import { DetailedAccount as DetailedAccountContainer } from "../../src/containers/DetailedAccount/DetailedAccount";
@@ -36,7 +36,6 @@ describe("DetailedAccount test", () => {
           <DetailedAccountContainer />
         </VerticalPaginationContext.Provider>
       </Provider>
-
     );
   });
 
@@ -49,7 +48,6 @@ describe("DetailedAccount test", () => {
   });
 
   it("should render DetailedAccountComponent", () => {
-
     expect(DetailedAccountComponent.mock.calls[0][0]).toEqual({
       accountType,
       isIslamicBanking,
