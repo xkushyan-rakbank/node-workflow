9c9a8d9479e930c7097e697010500c3e0e5f20b4
Dmitriy Soldatov <dmitrsold@gmail.com>
2020-04-16 14:06:35 +0300

    upd unit-test

--- webapply/__tests__/containers/Routers/AccountTypeProtectedRoute.test.js
+++ webapply/__tests__/containers/Routers/AccountTypeProtectedRoute.test.js
@@ -16,20 +16,20 @@ describe("AccountTypeProtectedRoute test", () => {
   const baseProps = { render: routeRender };
 
   it("should render route when component passed", () => {
-    const Component = jest.fn().mockImplementation(() => "some_content");
+    const Component = jest.fn().mockImplementation(() => null);
     const props = {
       ...baseProps,
       component: Component,
       computedMatch: { params: { accountType: RAKSTARTER_ROUTE_PARAM } }
     };
 
-    const { container } = render(
+    render(
       <Router history={history}>
         <AccountTypeProtectedRoute {...props} />
       </Router>
     );
 
-    expect(container.innerHTML).toMatch("some_content");
+    expect(Component).toHaveBeenCalledTimes(1);
   });
 
   it("should render roote when render function passed instead component", () => {
@@ -38,14 +38,20 @@ describe("AccountTypeProtectedRoute test", () => {
       computedMatch: { params: { accountType: RAKSTARTER_ROUTE_PARAM } }
     };
 
-    const { container } = render(
+    render(
       <Router history={history}>
         <AccountTypeProtectedRoute {...props} />
       </Router>
     );
 
-    expect(container.innerHTML).toMatch(rootPath);
     expect(history.location.pathname).toMatch(rootPath);
+    expect(routeRender).toHaveBeenCalledWith(
+      expect.objectContaining({
+        history: expect.any(Object),
+        match: expect.any(Object),
+        location: expect.any(Object)
+      })
+    );
   });
 
   it("should redirect to accountsComparison when accountType param didn't match", () => {
