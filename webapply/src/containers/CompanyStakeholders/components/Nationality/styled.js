9699001b155a470c68d33445b6836c43f3f0efa4
Randeep.kaur1 <randeep.kaur1@wipro.com>
2020-06-11 23:10:18 +0530

    'Has another citizenship' checkbox fix under Nationality section for small screens

--- webapply/src/containers/CompanyStakeholders/components/Nationality/styled.js
+++ webapply/src/containers/CompanyStakeholders/components/Nationality/styled.js
@@ -1,9 +1,14 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export const useStyles = makeStyles({
+export const useStyles = makeStyles(theme => ({
   divider: {
     marginTop: "30px",
     marginBottom: "15px",
     borderBottom: "solid 1px rgba(230, 230, 230, 0.5)"
-  }
-});
+  },
+  [theme.breakpoints.only("xs")]: {
+    anotherCitizenshipContainer: {
+      order: 1
+    },
+  } 
+}));
