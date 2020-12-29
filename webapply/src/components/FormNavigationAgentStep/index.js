65ce76ef7c62cdd05cfcfa0efd0d6ce09df3ad8f
Rayees pk <rayeespk1@gmail.com>
2020-12-29 17:33:07 +0400

    sidebar navigation added in agent login

---
+++ webapply/src/components/FormNavigationAgentStep/index.js
@@ -0,0 +1,1 @@
+export { FormNavigationAgentStep } from "./FormNavigationAgentStep";
---
+++ webapply/src/components/FormNavigationAgentStep/styled.js
@@ -0,0 +1,94 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles(theme => ({
+  stepItem: {
+    position: "relative",
+    color: "rgba(255,255,255,.5)",
+    listStyleType: "none",
+    fontSize: "20px",
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingLeft: "60px",
+    fontWeight: "400",
+    lineHeight: "26px",
+    "&:not(:last-child)": {
+      paddingBottom: "20px"
+    },
+    "& span": {
+      position: "absolute",
+      top: "1px",
+      left: "20px",
+      marginLeft: "0"
+    },
+    [theme.breakpoints.only("xs")]: {
+      marginLeft: "-15px",
+      paddingLeft: "40px",
+      fontSize: "16px",
+      "& span": {
+        left: "10px"
+      }
+    },
+    "&:before": {
+      display: props => (props.isDisplayProgress ? "inline" : "none"),
+      content: "''",
+      position: "absolute",
+      left: "0",
+      top: "0",
+      height: "100%",
+      width: "2px",
+      backgroundColor: "#fff",
+      opacity: ".4"
+    }
+  },
+  activeStepItem: {
+    fontWeight: "600",
+    color: "#fff",
+    "&:last-child:after": {
+      height: "100%"
+    },
+    "&:after": {
+      display: props => (props.isDisplayProgress ? "inline" : "none"),
+      content: "''",
+      position: "absolute",
+      left: "0",
+      top: "0",
+      height: "calc(100% - 20px)",
+      width: "2px",
+      backgroundColor: "#fff"
+    }
+  },
+  filledStepItem: {
+    "&:before": {
+      opacity: "0.4"
+    }
+  },
+  circle: {
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "24px",
+    height: "24px",
+    lineHeight: "24px",
+    opacity: 0,
+    borderRadius: "50%",
+    border: "1px solid #fff",
+    boxSizing: "border-box"
+  },
+  activeCircle: {
+    borderColor: "#fff",
+    borderWidth: "2px",
+    opacity: 1
+  },
+  doneIcon: {
+    width: "20px",
+    fill: "#fff"
+  },
+  activeIcon: {
+    width: "17px"
+  },
+  stepLink: {
+    color: "inherit"
+  }
+}));
