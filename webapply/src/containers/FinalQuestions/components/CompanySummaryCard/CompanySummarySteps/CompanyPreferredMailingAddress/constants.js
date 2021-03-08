916e8fabea008f0d053290914571f4cbc042fa50
Rayees pk <rayees.k@rakbank.ae>
2021-03-08 11:58:14 +0400

    flat villa field length increased to 50

--- webapply/src/containers/FinalQuestions/components/CompanySummaryCard/CompanySummarySteps/CompanyPreferredMailingAddress/constants.js
+++ webapply/src/containers/FinalQuestions/components/CompanySummaryCard/CompanySummarySteps/CompanyPreferredMailingAddress/constants.js
@@ -10,4 +10,4 @@ export const MAX_OFFICE_NUMBER_LENGTH = 10;
 export const MAX_STREET_NUMBER_LENGTH = 50;
 export const MAX_PO_BOX_NUMBER_LENGTH = 10;
 export const MAX_OTHER_FIELD_LENGTH = 50;
-export const MAX_FLAT_NUMBER_LENGTH = 10;
+export const MAX_FLAT_NUMBER_LENGTH = 50;
