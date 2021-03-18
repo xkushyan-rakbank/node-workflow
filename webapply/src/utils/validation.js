37f6a23afd73f440e31b800a9dfef5890a52c043
Rayees pk <rayees.k@rakbank.ae>
2021-03-18 14:00:47 +0400

    website regex changed

--- webapply/src/utils/validation.js
+++ webapply/src/utils/validation.js
@@ -28,7 +28,7 @@ export const SPACE_OCCUPIED_OTHER_REGEX = /^[a-zA-Z. ]{1,50}$/;
 export const POBOX_REGEX = /^([a-zA-Z0-9])*$/;
 
 // eslint-disable-next-line max-len
-export const WEBSITE_REGEX = /^(((https?):\/\/)?(www.)?[a-z0-9-]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#-]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?))$/;
+export const WEBSITE_REGEX = /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})$/;
 export const FILE_SIZE = 5 * 1048576;
 export const SUPPORTED_FORMATS = ["image/png", "image/jpeg", "application/pdf", "application/txt"];
 
