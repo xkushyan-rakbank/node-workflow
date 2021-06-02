a6b1646bd368e27c1991a88c9298c3bf605b54a1
Merine George <merine.george@rakbank.ae>
2021-06-02 09:17:42 +0400

    partnercode changes on agent screen

--- webapply/src/containers/AgentPages/SearchedAppInfo/components/CompanyDetails/constants.js
+++ webapply/src/containers/AgentPages/SearchedAppInfo/components/CompanyDetails/constants.js
@@ -1,11 +1,13 @@
 export const errorMsgs = {
   APPLICANT_DETAIL_ERROR: "Applicant details are not found.",
   RAKTRACK_LEAD_REFERENCE_ERROR: "RAKtrack lead reference number is not generated.",
-  COMPANY_DETAIL_ERROR: "Company details are not found."
+  COMPANY_DETAIL_ERROR: "Company details are not found.",
+  PARTNER_CODE_ERROR: "Partner Code is not found."
 };
 
 export const titles = {
   APPLICANT_DETAIL_TITLE: "Applicant's Detail",
   RAKTRACK_LEAD_REFERENCE_TITLE: "RAKtrack Lead Reference Number",
-  COMPANY_DETAIL_TITLE: "Company Detail"
+  COMPANY_DETAIL_TITLE: "Company Detail",
+  PARTNER_CODE_TITLE: "Partner Code"
 };
