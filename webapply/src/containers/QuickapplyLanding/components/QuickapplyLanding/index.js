aeb3373bab5375a72464ec55cdb116273014fd95
Tina Abraham <Tina.mary@rakbank.ae>
2022-03-17 10:26:57 +0400

    Page Title updated as per marketing feedback

--- webapply/src/containers/QuickapplyLanding/components/QuickapplyLanding/index.js
+++ webapply/src/containers/QuickapplyLanding/components/QuickapplyLanding/index.js
@@ -8,19 +8,21 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 export const QuickapplyLandingComponent = () => {
   const classes = useStyles();
   const { setCurrentSection } = useContext(VerticalPaginationContext);
-  
+
   return (
     <HelmetProvider>
       <Helmet>
-        <title>{"Quick Apply | Online Application for Business Accounts and Finance | RAKBANK"}</title>
+        <title>
+          {"Quick Apply | Online Application for Business Accounts and Finance | RAKBANK"}
+        </title>
       </Helmet>
-    <div className={classes.container}>
-      <div className={classes.paginationWrapper}>
-        <div className={classes.childWrapper}>
-          <LandingVideoPlayer video={landingVideo} classes={{ video: classes.video }} />
+      <div className={classes.container}>
+        <div className={classes.paginationWrapper}>
+          <div className={classes.childWrapper}>
+            <LandingVideoPlayer video={landingVideo} classes={{ video: classes.video }} />
+          </div>
         </div>
       </div>
-    </div>
     </HelmetProvider>
   );
 };
