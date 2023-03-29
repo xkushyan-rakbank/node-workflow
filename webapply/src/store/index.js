83d013743cd151eabbc7d1806c40025e3572d522
Rajasekhar Pasupula <xprajase@rakbank.ae>
2023-03-29 09:38:55 +0000

    Updated index.js

--- webapply/src/store/index.js
+++ webapply/src/store/index.js
@@ -5,7 +5,7 @@ import { configureStore } from "./configureStore";
 
 if (process.env.NODE_ENV !== "test") {
   /* eslint-disable */
-  history = createBrowserHistory();
+  history = createBrowserHistory({basename: process.env.REACT_APP_CONTEXT_PATH ||_'/'});
   store = configureStore(initialState, history).store;
   persistor = persistStore(store);
   /* eslint-enable */
