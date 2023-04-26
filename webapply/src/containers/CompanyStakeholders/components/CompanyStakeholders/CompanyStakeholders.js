75e2f42e4cfb3abb4c6987662d22cbd3b6897cf5
Amitha Babu <amitha.babu@rakbank.ae>
2023-04-26 14:34:31 +0400

    feat: Remove commented code: PDB-330

--- webapply/src/containers/CompanyStakeholders/components/CompanyStakeholders/CompanyStakeholders.js
+++ webapply/src/containers/CompanyStakeholders/components/CompanyStakeholders/CompanyStakeholders.js
@@ -4,7 +4,7 @@ import { BackLink } from "../../../../components/Buttons/BackLink";
 import routes from "../../../../routes";
 import { NextStepButton } from "../../../../components/Buttons/NextStepButton";
 import StakeholdersDetail from "./StakeholdersDetail";
-import { ReactComponent as VerifyMobileIcon } from "../../../../assets/icons/Vector.svg";
+import { ReactComponent as VerifyMobileIcon } from "../../../../assets/icons/verify_mobile.svg";
 import { FaceRecognition } from "../../../../components/FaceRecognition/FaceRecognition";
 import { UploadFileWrapper } from "../../../../components/UploadFileWrapper/UploadFileWrapper";
 import { useStyles } from "./styled";
@@ -33,7 +33,9 @@ export const CompanyStakeholdersComponent = ({
             </p>
           </div>
         </div>
-        <Button variant="outlined" className={classes.continueBtn}>Continue on mobile</Button>
+        <Button variant="outlined" className={classes.continueBtn}>
+          Continue on mobile
+        </Button>
       </div>
 
       <div className={classes.horizontalLine} />
@@ -67,59 +69,6 @@ export const CompanyStakeholdersComponent = ({
         />
       </div>
 
-      {/* <h2>Add your company’s stakeholders</h2>
-      <p className="formDescription">
-        Now we need to know about the key people in your company. This includes shareholders,
-        partners, signatories/Power of Attorney. Check our guide below to see which one applies to
-        your company
-      </p>
-      <div className={classes.stakeholdersTitleWrapper}>
-        <ContexualHelp
-          title={
-            <>
-              This stakeholder should be defined / mentioned in valid legal document of the Company.
-              <br />
-              <b>Examples:</b>
-              <br />- Sole Proprietorship Company &gt; Trade License
-              <br />- Partnership Company &gt; Trade License / Partners agreement / Share
-              Certificate, etc
-              <br />- Limited Liability Company (LLC) &gt; Trade License / Memorandum of Association
-              / Articles of Association, etc
-            </>
-          }
-          placement="right"
-          isDisableHoverListener={false}
-        >
-          <span className={classes.questionIcon}>
-            <Icon name={ICONS.question} alt="question" className={classes.iconSize} />
-          </span>
-        </ContexualHelp>
-        <span className={classes.stakeholderTitle}>Who is a stakeholder?</span>
-      </div>
-      <div>
-        {stakeholders.map((stakeholder, index) => (
-          <StakeholderStepper
-            stakeholder={stakeholder}
-            key={stakeholder.id}
-            deleteStakeholder={handleDeleteStakeholder}
-            orderIndex={index}
-          />
-        ))}
-      </div>
-      {stakeholders.length > 0 && editableStakeholder === null && (
-        <div className={classes.buttonsWrapper}>
-          <AddStakeholderButton disabled={isSendingProspect} handleClick={addNewStakeholder} />
-        </div>
-      )}
-      {isSignatoryErrorDisplayed && (
-        <ErrorMessage error="At least one signatory is required. Edit Signatory rights or Add new stakeholder." />
-      )}
-      {isLowPercentageErrorDisplayed && (
-        <ErrorMessage
-          error={`Shareholders ${percentage}% is less than 100%, either add a new stakeholder
-          or edit the shareholding % for the added stakeholders.`}
-        />
-      )} */}
       <div className="linkContainer">
         <BackLink path={routes.companyInfo} />
 
