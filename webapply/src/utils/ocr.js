ba39ea3a198e484ec1d5ce210b0f6a46a726bacf
Shyam Kumar <shyam.kumar@rakbank.ae>
2023-05-28 13:59:01 +0400

    chore: OCR document side validation check changed

--- webapply/src/utils/ocr.js
+++ webapply/src/utils/ocr.js
@@ -5,14 +5,14 @@ export const getOcrFieldValueBySource = (node, source) => {
 export const removeEncodingPrefix = imgStr => imgStr.replace(/data:.*;base64,/, "");
 
 export const checkDocumentSideValid = node => {
-  return node[0].value === "FRONT" && node[1].value === "BACK";
+  return node[0].value === "1";
 };
 
 export const checkDocumentValid = node => {
   if (!node?.documentNumber) {
     return false;
   }
-  const isDifferentSide = checkDocumentSideValid(node?.documentSide);
+  const isDifferentSide = checkDocumentSideValid(node?.documentMismatch);
   const documentNumber = node?.documentNumber[0]?.value;
   return isDifferentSide && node?.documentNumber.every(x => x?.value === documentNumber);
 };
