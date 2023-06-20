36fd9c783c89e3e45396746543849c16f9bb3d93
Rajasekhar Pasupula <xprajase@rakbank.ae>
2023-06-20 07:02:23 +0000

    Updated set-endpoint-and-recaptcha.sh

--- set-endpoint-and-recaptcha.sh
+++ set-endpoint-and-recaptcha.sh
@@ -19,7 +19,8 @@ then
     echo 'REACT_APP_RAKBANK_KYC_API_PATH=https://quickapplydev.rakbank.ae/digitalbank/rakbank-kyc' >> webapply/.env.production
     echo 'REACT_APP_WCM_PATH=https://revamp.rakbank.ae' >> webapply/.env.production
     echo 'REACT_APP_WCM_API_PATH=https://revamp.rakbank.ae' >> webapply/.env.production
-    echo 'REACT_APP_WTM_SCHEDULER_INTERVAL=5000' >> webapply/.env.production   
+    echo 'REACT_APP_WTM_SCHEDULER_INTERVAL=5000' >> webapply/.env.production
+    echo 'REACT_APP_DISABLE_GTM=true' >> webapply/.env.production
     elif [ "$VAR_env_to_deploy" == "release" ];
     then
 		echo 'PUBLIC_URL=https://quickapplyuat.rakbank.ae/digitalbank' > webapply/.env.production
@@ -40,6 +41,7 @@ then
         echo 'REACT_APP_WCM_PATH=https://revamp.rakbank.ae' >> webapply/.env.production
         echo 'REACT_APP_WCM_API_PATH=https://revamp.rakbank.ae' >> webapply/.env.production
         echo 'REACT_APP_WTM_SCHEDULER_INTERVAL=5000' >> webapply/.env.production
+        echo 'REACT_APP_DISABLE_GTM=true' >> webapply/.env.production
     elif [ "$VAR_env_to_deploy" == "new_uat" ];
     then
 		echo 'PUBLIC_URL=https://quickapplyuat.rakbank.ae:8888/digitalbank' > webapply/.env.production
@@ -60,6 +62,7 @@ then
         echo 'REACT_APP_WCM_PATH=https://revamp.rakbank.ae' >> webapply/.env.production
         echo 'REACT_APP_WCM_API_PATH=https://revamp.rakbank.ae' >> webapply/.env.production
         echo 'REACT_APP_WTM_SCHEDULER_INTERVAL=5000' >> webapply/.env.production
+        echo 'REACT_APP_DISABLE_GTM=true' >> webapply/.env.production
     elif [ "$VAR_env_to_deploy" == "replica" ];
     then
         echo 'REACT_APP_API_PATH=https://quickapplyuat.rakbank.ae:9443' > webapply/.env.production
