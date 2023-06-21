9277340be5046776b7def2fecd23bcd584aebdb7
nidhi.unni <nidhi.unni@rakbank.ae>
2023-06-21 14:36:54 +0400

    enable language switcher for chat change

--- webapply/src/containers/WebChat/components/ClosedChat.js
+++ webapply/src/containers/WebChat/components/ClosedChat.js
@@ -1,7 +1,9 @@
-import React, { useState, forwardRef, useCallback } from "react";
+import React, { useState, forwardRef, useCallback, useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 import { useStyles } from "../styled";
 import chatIcon from "../../../assets/webchat/black.svg";
+import { updateProspect } from "../../../store/actions/appConfig";
 
 const ClosedChatScreen = ({ openChat, isMinimized }, ref) => {
   const classes = useStyles();
@@ -14,21 +16,54 @@ const ClosedChatScreen = ({ openChat, isMinimized }, ref) => {
   ]);
 
   ref.current = { incCounter, resetCounter };
+  const [isArabic, setIsArabic] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    setLang("en");
+  }, []);
+
+  const setLang = lang => {
+    lang === "ar" ? setIsArabic(false) : setIsArabic(true);
+    dispatch(
+      updateProspect({
+        "prospect.freeFieldsInfo.freeField3": lang === "en" ? "true" : "false"
+      })
+    );
+  };
 
   return (
     <div className={classes.chat}>
-      <div className={classes.chatInner} onClick={openChat}>
-        <div>
-          <span>
-            {isMinimized && (
-              <div className={classes.messagesCount}>
-                <p>{newMessagesCount}</p>
-              </div>
-            )}
-            <img src={chatIcon} alt="chat" />
-          </span>
+      <div className={classes.chatInner}>
+        <div className={classes.chatInner} onClick={openChat}>
+          <div>
+            <span>
+              {isMinimized && (
+                <div className={classes.messagesCount}>
+                  <p>{newMessagesCount}</p>
+                </div>
+              )}
+              <img src={chatIcon} alt="chat" />
+            </span>
+          </div>
+          <div className="hide-on-mobile small-menu-hide"> Chat with Us</div>
         </div>
-        <div className="hide-on-mobile small-menu-hide"> Chat with Us</div>
+        {isArabic ? (
+          <a
+            role="button"
+            style={{ color: "#fff", cursor: "pointer", textDecoration: "underline" }}
+            onClick={() => setLang("ar")}
+          >
+            {"(English)"}
+          </a>
+        ) : (
+          <a
+            role="button"
+            style={{ color: "#fff", cursor: "pointer", textDecoration: "underline" }}
+            onClick={() => setLang("en")}
+          >
+            {"(عربي)"}
+          </a>
+        )}
       </div>
     </div>
   );
