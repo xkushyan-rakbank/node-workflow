77947adb8f35a9dfebe9208c6e7e0952753b2ebb
Shyam Kumar <shyam.kumar@rakbank.ae>
2023-06-23 16:13:05 +0400

    feat: PDB-239: source of income validation

--- webapply/src/containers/Additional/components/StakeholderInformation/components/SourceOfIncome.js
+++ webapply/src/containers/Additional/components/StakeholderInformation/components/SourceOfIncome.js
@@ -1,30 +1,121 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Formik } from "formik";
 import { Grid } from "@material-ui/core";
+import * as Yup from "yup";
+import { useDispatch } from "react-redux";
 
 import { AutoSaveField as Field, Input, SelectAutocomplete } from "../../../../../components/Form";
 import { Accordion } from "../../../../../components/Accordion/CustomAccordion";
 import { Upload } from "../../../../../components/Upload";
-import { TL_ACCEPTED_FILE_TYPES, TL_COI_FILE_SIZE } from "../../../../../constants";
+import { MOA_FILE_SIZE, TL_ACCEPTED_FILE_TYPES, TL_COI_FILE_SIZE } from "../../../../../constants";
 
 import { useStyles } from "../../styled";
+import { getRequiredMessage } from "../../../../../utils/getValidationMessage";
+import { uploadDocuments } from "../../../../../store/actions/uploadDocuments";
+import { MAX_COMPANY_FULL_NAME_LENGTH } from "../../../../CompanyInfo/constants";
 
 export const SourceOfIncome = () => {
   const classes = useStyles();
-  const basePath = "prospect.stakeholderAdditionalInfo.sourceOfIncomeDetails";
-
+  const basePath = "prospect.signatoryInfo[0]stakeholderAdditionalInfo.sourceOfIncomeDetails";
+  const dispatch = useDispatch();
   const initialValues = {
-    sourceOfIncome: [],
+    sourceOfIncome: "",
     IBANType: "",
     IBAN: "",
-    companyNameforSOF: ""
+    companyNameforSOF: "",
+    proofOfIncome: "",
+    tradeLicense: ""
   };
 
-  return (
-    <Formik initialValues={initialValues} onSubmit={() => {}} validateOnChange={false}>
-      {({ values, setFieldValue }) => {
+  const sourceOfIncomeValidationSchema = Yup.object().shape({
+    sourceOfIncome: Yup.array().required(getRequiredMessage("Source of income")),
+    IBANType: Yup.string().required(getRequiredMessage("IBAN type")),
+    IBAN: Yup.string()
+      .required(getRequiredMessage("IBAN"))
+      .matches(/^AE\d{21}$/, "Invalid UAE IBAN format")
+      .max(23, "IBAN must have a maximum of 23 characters"),
+    proofOfIncome: Yup.mixed()
+      .test("required", getRequiredMessage("Proof of income"), file => {
+        if (file) return true;
+        return false;
+      })
+      .test("fileSize", "The file is too large", file => {
         return (
-          <Accordion title={"Source of income"} id={"sourceOfIncome"} isCompleted={true}>
+          file &&
+          (file === true ||
+            (file.size >= MOA_FILE_SIZE.minSize && file.size <= MOA_FILE_SIZE.maxSize))
+        );
+      }),
+    companyNameforSOF: Yup.string().when("IBANType", {
+      is: IBANType => IBANType === "BARO",
+      then: Yup.string()
+        .required(getRequiredMessage("Company name"))
+        // eslint-disable-next-line no-template-curly-in-string
+        .max(MAX_COMPANY_FULL_NAME_LENGTH, "Maximum ${max} characters allowed")
+    }),
+    tradeLicense: Yup.mixed().when("IBANType", {
+      is: IBANType => IBANType === "BARO",
+      then: Yup.mixed()
+        .required(getRequiredMessage("TradeLicense"))
+        .test("fileSize", "The file is too large", file => {
+          return (
+            file &&
+            (file === true ||
+              (file.size >= MOA_FILE_SIZE.minSize && file.size <= MOA_FILE_SIZE.maxSize))
+          );
+        })
+    })
+  });
+
+  const handleDropFile = useCallback((acceptedFiles, name, touched, setTouched, setFieldValue) => {
+    const file = acceptedFiles[0];
+    if (file) {
+      let path = "";
+      if (name === "proofOfIncome") {
+        path =
+          "prospect.prospectDocuments.stakeholderAdditionalInfo.sourceOfIncomeDetails.proofOfIncome";
+      } else {
+        path =
+          "prospect.prospectDocuments.stakeholderAdditionalInfo.sourceOfIncomeDetails.tradeLicense";
+      }
+      dispatch(
+        uploadDocuments({
+          docs: {
+            [path]: file
+          },
+          documentSection: "stakeholdersDocuments.index_stakeholderName",
+          onSuccess: () => {
+            setFieldValue(
+              name,
+              Object.assign(file, {
+                preview: URL.createObjectURL(file)
+              })
+            );
+            setTouched({ ...touched, ...{ [name]: true } });
+          },
+          onFailure: () => {
+            setFieldValue(name, "");
+          }
+        })
+      );
+    }
+  }, []);
+
+  const initialIsValid = sourceOfIncomeValidationSchema.isValidSync(initialValues);
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={sourceOfIncomeValidationSchema}
+      validateOnBlur={true}
+      validateOnMount={true}
+      isInitialValid={initialIsValid}
+      onSubmit={() => {}}
+    >
+      {({ values, setFieldValue, touched, setTouched, isValid, ...props }) => {
+        const isBARO = values.IBANType === "BARO";
+        return (
+          <Accordion title={"Source of income"} id={"sourceOfIncome"} isCompleted={isValid}>
             <>
               <Grid container>
                 <Grid item sm={12} xs={12}>
@@ -68,6 +159,7 @@ export const SourceOfIncome = () => {
                 <Grid item sm={12} xs={12}>
                   <Field
                     name="IBAN"
+                    isLoadDefaultValueFromStore={true}
                     path={`${basePath}.IBAN`}
                     label="IBAN of your UAE bank"
                     placeholder="IBAN of your UAE bank"
@@ -77,37 +169,49 @@ export const SourceOfIncome = () => {
                     component={Input}
                   />
                 </Grid>
-                <Grid item sm={12} xs={12}>
-                  <Field
-                    name="companyNameforSOF"
-                    path={`${basePath}.companyNameforSOF`}
-                    label="Company name"
-                    placeholder="Company name"
-                    InputProps={{
-                      inputProps: { tabIndex: 1 }
-                    }}
-                    component={Input}
-                  />
-                </Grid>
-                <Grid item sm={12} xs={12}>
-                  <Field
-                    name="tradeLicenceFile"
-                    type="file"
-                    fieldDescription={"Upload Trade licence"}
-                    helperText={
-                      "Supported formats are PDF, JPG and PNG | 5MB maximum | 10KB minimum"
-                    }
-                    accept={TL_ACCEPTED_FILE_TYPES}
-                    fileSize={TL_COI_FILE_SIZE}
-                    component={Upload}
-                    showUploadInfoIcon={true}
-                    contextualHelpText="Upload your other company's trade license for which the IBAN is shared above has context menu"
-                    contextualHelpProps={{
-                      isDisableHoverListener: false,
-                      placement: "left"
-                    }}
-                  />
-                </Grid>
+                {isBARO && (
+                  <>
+                    <Grid item sm={12} xs={12}>
+                      <Field
+                        name="companyNameforSOF"
+                        path={`${basePath}.companyNameforSOF`}
+                        label="Company name"
+                        placeholder="Company name"
+                        InputProps={{
+                          inputProps: { tabIndex: 1 }
+                        }}
+                        component={Input}
+                      />
+                    </Grid>
+                    <Grid item sm={12} xs={12}>
+                      <Field
+                        name="tradeLicense"
+                        path="prospect.prospectDocuments.additionalStakeholderDocument.tradeLicense"
+                        type="file"
+                        fieldDescription="Upload Trade licence"
+                        helperText={
+                          "Supported formats are PDF, JPG and PNG | 5MB maximum | 10KB minimum"
+                        }
+                        accept={TL_ACCEPTED_FILE_TYPES}
+                        fileSize={TL_COI_FILE_SIZE}
+                        onDrop={acceptedFile =>
+                          handleDropFile(
+                            acceptedFile,
+                            "tradeLicense",
+                            touched,
+                            setTouched,
+                            setFieldValue
+                          )
+                        }
+                        file={values.tradeLicense}
+                        onDelete={() => setFieldValue("tradeLicense", "")}
+                        component={Upload}
+                        content={values?.tradeLicense?.name}
+                      />
+                    </Grid>
+                  </>
+                )}
+
                 <Grid item sm={12} xs={12}>
                   <div className={classes.horizontalLine} />
                 </Grid>
@@ -123,8 +227,20 @@ export const SourceOfIncome = () => {
                     fileSize={TL_COI_FILE_SIZE}
                     component={Upload}
                     showInfoIcon={true}
-                    infoTitle={"You can select multiple options"}
-                    infoIcon={true}
+                    // infoTitle={"You can select multiple options"}
+                    // infoIcon={true}
+                    onDrop={acceptedFile =>
+                      handleDropFile(
+                        acceptedFile,
+                        "proofOfIncome",
+                        touched,
+                        setTouched,
+                        setFieldValue
+                      )
+                    }
+                    file={values.proofOfIncome}
+                    onDelete={() => setFieldValue("proofOfIncome", "")}
+                    content={values?.proofOfIncome?.name}
                   />
                 </Grid>
               </Grid>
