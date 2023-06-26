12eb9d65c015283f3d983042a74d9a96d27cd73e
thasnee.iqbal <thasnee.iqbal@rakbank.ae>
2023-06-26 17:26:11 +0400

    feat: RO search navigation from new to old

--- webapply/src/containers/AgentPages/SearchProspect/components/SearchResults/SearchItem.js
+++ webapply/src/containers/AgentPages/SearchProspect/components/SearchResults/SearchItem.js
@@ -1,24 +1,54 @@
 import React from "react";
 import { generatePath } from "react-router";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 import cx from "classnames";
 
 import routes from "../../../../../routes";
-import { BAU_PROSPECT_VERSION } from "../../../../../constants"
+import { BAU_PROSPECT_VERSION } from "../../../../../constants";
 import { useStyles } from "./styled";
+
+import { getAppConfig } from "../../../../../store/selectors/appConfig";
+import { getLoginResponse } from "../../../../../store/selectors/loginSelector";
 
 export const SearchItem = ({ application }) => {
   const classes = useStyles();
+  const history = useHistory();
+  const { authorizationToken } = useSelector(getAppConfig);
+  const { agentName, agentId, agentRole, deptName, roCode } = useSelector(getLoginResponse);
   const prospectVersion = application.prospectVersion ? application.prospectVersion : "";
+
+  function redirectToExternalURL(externalURL) {
+    const data = {
+      agentName,
+      agentId,
+      agentRole,
+      deptName,
+      roCode,
+      prospectId: application?.prospectId,
+      authToken: authorizationToken,
+      fullName: application.applicantInfo?.fullName
+    };
+
+    const params = new URLSearchParams();
+
+    for (const key in data) {
+      params.append(key, data[key]);
+    }
+
+    const encodedQuery = params.toString();
+    const url = `${externalURL}?${encodedQuery}`;
+    window.location.href = url;
+  }
+
+  const handleNavigation = () => {
+    return prospectVersion === BAU_PROSPECT_VERSION
+      ? redirectToExternalURL(process.env.REACT_APP_BAU_URL + "/business/application-redirect")
+      : history.push(generatePath(routes.SearchedAppInfo, { id: application?.prospectId }));
+  };
+
   return (
-    <Link
-      className={cx(classes.applicationRow)}
-      to={
-        prospectVersion === BAU_PROSPECT_VERSION
-          ? process.env.REACT_APP_BAU_URL + generatePath(routes.SearchedAppInfo, { id: application?.prospectId })
-          : generatePath(routes.SearchedAppInfo, { id: application?.prospectId })
-      }
-    >
+    <div className={cx(classes.applicationRow)} onClick={handleNavigation}>
       <div className={classes.column}>
         <div className={classes.fullName}>{application.applicantInfo?.fullName}</div>
         <div className={classes.account}>{application.applicantInfo?.email}</div>
@@ -44,6 +74,6 @@ export const SearchItem = ({ application }) => {
           <div className={classes.status}>Incomplete</div>
         )}
       </div>
-    </Link>
+    </div>
   );
 };
