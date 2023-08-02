d096cf892275df30bb7f39ae5808d1e29674a102
Jean Xavier <xxajean@rakbank.ae>
2023-08-02 12:52:02 +0400

    chore: PDB-1496: removed rakstarter information dialog from landing page

--- webapply/src/containers/AccountsComparison/AccountsComparison.js
+++ webapply/src/containers/AccountsComparison/AccountsComparison.js
@@ -1,18 +1,10 @@
-import React, { useState, useEffect, useCallback, useContext } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useLocation } from "react-router-dom";
 
-import { VerticalPaginationContext } from "../../components/VerticalPagination";
 import { accountTypes } from "./components/TableCompare/constants";
 import { AccountsComparisonComponent } from "./components/AccountsComparison/AccountsComparison";
-import {
-  accountNames,
-  applicationOverviewRoutesMap,
-  CONVENTIONAL,
-  DEFAULT_REFERRAL_NAME
-} from "../../constants";
+import { applicationOverviewRoutesMap, CONVENTIONAL, DEFAULT_REFERRAL_NAME } from "../../constants";
 import { useTrackingHistory } from "../../utils/useTrackingHistory";
-import { ConfirmDialog } from "../../components/Modals";
-import { RAK_STARTER_CONFIRM_MESSAGE } from "./constants";
 
 const useQuery = () => {
   return new URLSearchParams(useLocation().search);
@@ -21,7 +13,7 @@ const useQuery = () => {
 export const AccountsComparisonContainer = ({
   servicePricingGuideUrl,
   setProspectLead,
-  setRoCode
+  setRoCode,
 }) => {
   let query = useQuery();
 
@@ -38,10 +30,9 @@ export const AccountsComparisonContainer = ({
   }, [query, setRoCode, setProspectLead]);
 
   const [selectedAccount, setSelectedAccount] = useState(accountTypes.starter.name);
-  const [isDisplayConfirmDialog, setIsDisplayConfirmDialog] = useState(false);
 
   const goto = useCallback(
-    accountType => {
+    (accountType) => {
       if (queryParams) {
         pushHistory(applicationOverviewRoutesMap[accountType][CONVENTIONAL] + queryParams);
       } else {
@@ -52,25 +43,12 @@ export const AccountsComparisonContainer = ({
   );
 
   const handleSetAccountType = useCallback(
-    accountType => {
-      if (accountType === accountNames.starter) {
-        setIsDisplayConfirmDialog(true);
-      } else {
-        goto(accountType);
-      }
+    (accountType) => {
+      goto(accountType);
       setSelectedAccount(accountType);
     },
     [setSelectedAccount]
   );
-
-  const closeDialogHandler = useCallback(() => {
-    setIsDisplayConfirmDialog(false);
-  }, [setIsDisplayConfirmDialog]);
-
-  const confirmHandler = useCallback(() => {
-    closeDialogHandler();
-    goto(accountNames.starter);
-  }, [closeDialogHandler, goto]);
 
   return (
     <>
@@ -78,17 +56,6 @@ export const AccountsComparisonContainer = ({
         handleSetAccountType={handleSetAccountType}
         selectedAccount={selectedAccount}
         servicePricingGuideUrl={servicePricingGuideUrl}
-      />
-      <ConfirmDialog
-        title={null}
-        isOpen={isDisplayConfirmDialog}
-        handleConfirm={confirmHandler}
-        handleReject={closeDialogHandler}
-        handleClose={closeDialogHandler}
-        message={RAK_STARTER_CONFIRM_MESSAGE}
-        cancelLabel="No"
-        confirmLabel="Yes"
-        divider={false}
       />
     </>
   );
