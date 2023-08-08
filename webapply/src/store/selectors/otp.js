d4768fa3b3e112602a95fce5181b9e22f0766cc2
Shyam Kumar <shyam.kumar@rakbank.ae>
2023-08-08 14:53:18 +0400

    feat: PDB-1538: otp isRoEFR selector

--- webapply/src/store/selectors/otp.js
+++ webapply/src/store/selectors/otp.js
@@ -5,3 +5,5 @@ export const isOtpGenerated = state => getOtp(state).isGenerated;
 export const isOtpVerified = state => getOtp(state).isVerified;
 
 export const getIsGenerating = state => getOtp(state).isGenerating;
+
+export const getIsRoInviteEfr = state => getOtp(state).isRoInviteEFR;
