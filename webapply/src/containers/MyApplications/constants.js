532f68cb0f23dc2c0836705bd58ebee421dcaca8
Shyam Kumar <shyam.kumar@rakbank.ae>
2023-09-20 16:35:01 +0400

    fix: NO-JIRA: dec stopped status

--- webapply/src/containers/MyApplications/constants.js
+++ webapply/src/containers/MyApplications/constants.js
@@ -58,6 +58,10 @@ export const roActions = {
     buttonText: "View Details",
     mobileStatus: "We’re almost there! Please log in on a desktop to upload them."
   },
+  DEC_STOPPED: {
+    buttonText: "View Details",
+    mobileStatus: "We’re almost there! Please log in on a desktop to upload them."
+  },
   OPE_EDIT: {
     buttonText: "Update Application",
     mobileStatus: "You’re half way there! Please log in on a desktop to finish the application."
