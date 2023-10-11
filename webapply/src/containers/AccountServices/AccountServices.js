798f2f505b837673c1aaec919baf5ca8f3f199a9
Amitha Babu <amitha.babu@rakbank.ae>
2023-10-11 18:21:14 +0400

    fix:PDB-2074:KYC annexure validation update for text feilds

--- webapply/src/containers/AccountServices/AccountServices.js
+++ webapply/src/containers/AccountServices/AccountServices.js
@@ -333,7 +333,7 @@ export const AccountServices = ({ sendProspectToAPI }) => {
     businessModel: Yup.string()
       .nullable()
       .notRequired()
-      .matches(TOTAL_EXPERIENCE_YRS_REGEX, getInvalidMessage("Business model"))
+      .matches(/^[a-zA-Z0-9.\-\\\s%]+$/, getInvalidMessage("Business model"))
       .max(5000, "Business model is too long. Please enter upto 5000 characters."),
     signatoryName: Yup.string()
       .nullable()
@@ -341,35 +341,35 @@ export const AccountServices = ({ sendProspectToAPI }) => {
     ownerAdditionalInfo: Yup.string()
       .nullable()
       .notRequired()
-      .matches(TOTAL_EXPERIENCE_YRS_REGEX, getInvalidMessage("Additional Information of owner"))
+      .matches(/^[a-zA-Z0-9.\-\\\s%]+$/, getInvalidMessage("Additional Information of owner"))
       .max(5000, "Additional Information of owner is too long. Please enter upto 5000 characters."),
     generalRemarksRO: Yup.string()
       .nullable()
       .notRequired()
-      .matches(TOTAL_EXPERIENCE_YRS_REGEX, getInvalidMessage("General remarks (RO)"))
+      .matches(/^[a-zA-Z0-9.\-\\\s%]+$/, getInvalidMessage("General remarks (RO)"))
       .max(5000, "General remarks (RO) is too long. Please enter upto 5000 characters."),
     generalRemarksRM: Yup.string()
       .nullable()
       .notRequired()
-      .matches(TOTAL_EXPERIENCE_YRS_REGEX, getInvalidMessage("General remarks (RM)"))
+      .matches(/^[a-zA-Z0-9.\-\\\s%]+$/, getInvalidMessage("General remarks (RM)"))
       .max(5000, "General remarks (RM) is too long. Please enter upto 5000 characters."),
+    verificationRemarksRM: Yup.string()
+      .nullable()
+      .notRequired()
+      .matches(/^[a-zA-Z0-9.\-\\\s%]+$/, getInvalidMessage("Remarks on verification (RM)"))
+      .max(5000, "Remarks on verification (RM) is too long. Please enter upto 5000 characters."),
     isVisitConducted: Yup.string()
       .nullable()
       .notRequired(),
     verificationDetails: Yup.array().of(
       Yup.object().shape({
         verificationStatus: Yup.string(),
-        verificationRemarks: Yup.mixed().when("verificationStatus", {
-          is: verificationStatus => {
-            return verificationStatus === "FAIL" || verificationStatus === "REFR";
-          },
-          then: Yup.string()
-            .nullable()
-            .notRequired()
-            // eslint-disable-next-line no-template-curly-in-string
-            .max(5000, "Maximum ${max} characters allowed")
-            .matches(TOTAL_EXPERIENCE_YRS_REGEX, getInvalidMessage("Verification remarks"))
-        })
+        verificationRemarks: Yup.string()
+          .nullable()
+          .notRequired()
+          // eslint-disable-next-line no-template-curly-in-string
+          .max(5000, "Maximum ${max} characters allowed")
+          .matches(/^[a-zA-Z0-9.\-\\\s%]+$/, getInvalidMessage("Verification remarks"))
       })
     ),
     signatoryEIDinfo: Yup.string()
