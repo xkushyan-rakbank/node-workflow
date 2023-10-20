4b24c8b1d7c626b1511514255770c68e0bb75349
Shyam Kumar <shyam.kumar@rakbank.ae>
2023-10-20 16:04:41 +0400

    fix: PDB-2884: hide invite if no rocode

--- webapply/src/containers/AgentPages/SearchProspect/index.js
+++ webapply/src/containers/AgentPages/SearchProspect/index.js
@@ -8,14 +8,15 @@ import {
   getSearchError,
   getSearchErrorDesc
 } from "../../../store/selectors/searchProspect";
-import { getDatalist } from "../../../store/selectors/appConfig";
+import { getDatalist, getRoCode } from "../../../store/selectors/appConfig";
 
 const mapStateToProps = state => ({
   searchResults: getSearchResults(state),
   isLoading: getIsLoadingSearchProspects(state),
   searchError: getSearchError(state),
   searchErrorDesc: getSearchErrorDesc(state),
-  dataList: getDatalist(state)
+  dataList: getDatalist(state),
+  roCode: getRoCode(state)
 });
 
 const mapDispatchToProps = {
