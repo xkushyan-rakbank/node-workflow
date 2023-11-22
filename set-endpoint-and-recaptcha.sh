b69edb5dde8f3f526870b2c76d285155c9f35548
Nissar Akber <xanissar@rakbank.ae>
2023-11-22 09:16:01 +0000

    chore: New production build variable changes

--- set-endpoint-and-recaptcha.sh
+++ set-endpoint-and-recaptcha.sh
@@ -126,8 +126,8 @@ then
         echo 'REACT_APP_OTP_ENABLE=Y' >> webapply/.env.production
         echo 'REACT_APP_UPLOAD_PATH=https://rmtc.rakbankonline.ae/docUploader' >> webapply/.env.production
         echo 'REACT_APP_RSA_PUBLIC_KEY="-----BEGIN PUBLIC KEY-----\\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA30iQjaEzThxFY1gouuE3\\n/7Lle7ZhViUATYY7QzZ3UsX623Gi5/624smNQDJAbYDh0wWbwnNeJJOm2DYJYDld\\nz67bo8pmsEzRvARWx4hC+4Fdd003sguQJHcr7Rgg+g5+L912VZmmdW3c1H4v6IOz\\nHINla+HUTeDKw/d/rDi/kE6sfeND8tMCo5hbKkRIe356bYyoEMHwY1f/fss0T5Q/\\nwGWPwLvZwJHDv4vDErrwv7xcC/C5v9PRHXRNmchYyi3lhVqDFbLilfSreD6zdIBX\\nvBAgzGb9Ovd+Uqa5ju/v9jy48W7MoJLgkZpCYrNr6pk4mUwU6n6ar3NeVQjXxiwv\\n1iyyT2M02IKlCWEMo4trco96Whr29e79g32S/vpA5Ab+jU12N1X+L+qRcHtlo3dL\\nnt9EKFYVEGxLl1hmVEpkJ0rGRrQWikkPw0bq+csW0GjFXsVaWl4eaOW9M4iavSRY\\nb64BBpZ4XgltcieSdQWp33yrzvhh+0MPzOxkmxyyc/Z7sp1WWbs4Fz5ymuyr+xnP\\n7kYNhopMOBgmIvjX0DZTOC90QaHhtit361GbWSeJQfX9oCYMaQQRg3W+mToUMa/h\\noEERY2QJf1Np8Y+cObrbhwPQWjQOIFK/I0MAPoDUFvtV2/xg+hPoxoasN/oBC6wv\\nBhr1Z9R2UOWEm+2cbz8mZFkCAwEAAQ==\\n-----END PUBLIC KEY-----"' >> webapply/.env.production
-        echo 'REACT_APP_SERVER_ENV=development' >>webapply/.env.production
-        #echo 'REACT_APP_CHAT_API_PATH=https://uatrmt.rakbankonline.ae/RMTCHAT/cometd' >> webapply/.env.production
+        echo 'REACT_APP_SERVER_ENV=production' >>webapply/.env.production
+        #echo 'REACT_APP_CHAT_API_PATH=https://rmt.rakbankonline.ae/CHAT/cometd' >> webapply/.env.production
         echo 'REACT_APP_CHAT_API_PATH=https://quickapply.rakbank.ae/genesys/cometd' >> webapply/.env.production
         echo 'REACT_APP_AD_APPKEY=EUM-AAB-AWG' >>webapply/.env.production
         echo 'REACT_APP_AD_URL=https://rakbankonline.ae/appd/cowebapply' >>webapply/.env.production
@@ -139,7 +139,7 @@ then
         echo 'REACT_APP_WCM_API_PATH=https://rakbank.ae' >> webapply/.env.production
         echo 'REACT_APP_WTM_SCHEDULER_INTERVAL=5000' >> webapply/.env.production
         echo 'REACT_APP_DISABLE_GTM=false' >> webapply/.env.production
-        echo 'REACT_APP_BAU_URL=quickapply.rakbank.ae' >> webapply/.env.production
+        echo 'REACT_APP_BAU_URL=https://quickapply.rakbank.ae' >> webapply/.env.production
         echo 'GENERATE_SOURCEMAP=false' >> webapply/.env.production
 else
     exit 1
